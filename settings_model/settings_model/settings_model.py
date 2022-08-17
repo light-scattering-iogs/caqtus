5303035332428e82d0033171057711bd7e5a8ade
Damien Bloch <bloc.dam@gmail.com>
2022-08-17 20:10:39 +0200

    Make representer/constructor into class methodes

--- settings_model/settings_model/settings_model.py
+++ settings_model/settings_model/settings_model.py
@@ -1,5 +1,4 @@
 import abc
-import typing as th
 
 import pydantic
 import yaml
@@ -8,9 +7,10 @@ import yaml
 class SettingsModel(abc.ABC, pydantic.BaseModel):
     """Allows to store and load experiment configuration with type validation
 
-    All instances of a subclass of this class can be (de)serialized (from) to yaml based on their fields (see pydantic).
-    This is used for persistence and loading of experiment settings. If you need to add some parameters to the
-    experiment manager, it is recommended to create a new subclass of this class.
+    All instances of a subclass of this class can be (de)serialized (from) to yaml based
+    on their fields (see pydantic). This is used for persistence and loading of
+    experiment settings. If you need to add some parameters to the experiment manager,
+    it is recommended to create a new subclass of this class.
     """
 
     class Config:
@@ -18,22 +18,33 @@ class SettingsModel(abc.ABC, pydantic.BaseModel):
 
     def __init_subclass__(cls):
         """Register subclasses for serialization and deserialization to yaml"""
-        yaml.SafeDumper.add_representer(cls, generate_representer(cls))
-        yaml.SafeLoader.add_constructor(f"!{cls.__name__}", generate_constructor(cls))
+        yaml.SafeDumper.add_representer(cls, cls.representer)
+        yaml.SafeLoader.add_constructor(f"!{cls.__name__}", cls.constructor)
 
-
-def generate_representer(class_: th.Type[SettingsModel]):
-    def representer(dumper: yaml.Dumper, settings: SettingsModel):
+    @classmethod
+    def representer(cls, dumper: yaml.Dumper, settings: "SettingsModel"):
         return dumper.represent_mapping(
-            f"!{class_.__name__}",
-            {field: getattr(settings, field) for field in class_.__fields__},
+            f"!{cls.__name__}",
+            {field: getattr(settings, field) for field in cls.__fields__},
         )
 
-    return representer
+    @classmethod
+    def constructor(cls, loader: yaml.Loader, node: yaml.Node):
+        return cls(**loader.construct_mapping(node, deep=True))
 
 
-def generate_constructor(class_: th.Type[SettingsModel]):
-    def constructor(loader: yaml.Loader, node: yaml.Node):
-        return class_(**loader.construct_mapping(node, deep=True))
-
-    return constructor
+# def generate_representer(class_: th.Type[SettingsModel]):
+#     def representer(dumper: yaml.Dumper, settings: SettingsModel):
+#         return dumper.represent_mapping(
+#             f"!{class_.__name__}",
+#             {field: getattr(settings, field) for field in class_.__fields__},
+#         )
+#
+#     return representer
+#
+#
+# def generate_constructor(class_: th.Type[SettingsModel]):
+#     def constructor(loader: yaml.Loader, node: yaml.Node):
+#         return class_(**loader.construct_mapping(node, deep=True))
+#
+#     return constructor
