b7f6c2ee0df074c375bd1c4e861465f6ecf80370
Damien Bloch <bloc.dam@gmail.com>
2022-08-20 21:26:44 +0200

    Create main entry point to run program

--- condetrol/condetrol/__main__.py
+++ condetrol/condetrol/__main__.py
@@ -1,11 +1,27 @@
+import logging
 import sys
 
+import qdarkstyle
 from PyQt5.QtWidgets import QApplication
 
 from experiment_viewer import ExperimentViewer
 
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
+    sys.excepthook = except_hook
     app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
     experiment_viewer = ExperimentViewer()
     experiment_viewer.show()
-    app.exec()
+    try:
+        app.exec()
+    except Exception:
+        logger.error("An exception occurred.", exc_info=True)
