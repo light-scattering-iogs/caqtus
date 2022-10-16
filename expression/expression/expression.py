13fff2e4bad7b585c14ae3d8edfb16da78887617
Damien Bloch <bloc.dam@gmail.com>
2022-10-16 14:42:23 +0200

    Add implicit multiplication in expressions

--- expression/expression/expression.py
+++ expression/expression/expression.py
@@ -1,6 +1,7 @@
 import ast
 from functools import cached_property
 
+import token_utils
 import yaml
 
 from settings_model import YAMLSerializable
@@ -30,6 +31,9 @@ class Expression(YAMLSerializable):
             del self.code
         if hasattr(self, "upstream_variables"):
             del self.upstream_variables
+
+    def __repr__(self):
+        return f"Expression({self.body})"
 
     def evaluate(self, variables: dict[str]):
         """Evaluate an expression on specific values for its variables"""
@@ -64,7 +68,8 @@ class Expression(YAMLSerializable):
     def ast(self) -> ast.Expression:
         """Computes the abstract syntax tree for this expression"""
 
-        return ast.parse(self.body, mode="eval")
+        explicit_expr = add_implicit_multiplication(self.body)
+        return ast.parse(explicit_expr, mode="eval")
 
     @cached_property
     def code(self):
@@ -80,3 +85,46 @@ class Expression(YAMLSerializable):
     @classmethod
     def constructor(cls, loader: yaml.Loader, node: yaml.Node):
         return cls(body=loader.construct_scalar(node))
+
+
+def add_implicit_multiplication(source: str) -> str:
+    """This adds a multiplication symbol where it would be understood as
+    being implicit by the normal way algebraic equations are written but would
+    be a SyntaxError in Python. Thus we have::
+
+        2n  -> 2*n
+        n 2 -> n* 2
+        2(a+b) -> 2*(a+b)
+        (a+b)2 -> (a+b)*2
+        2 3 -> 2* 3
+        m n -> m* n
+        (a+b)c -> (a+b)*c
+
+    The obvious one (in algebra) being left out is something like ``n(...)``
+    which is a function call - and thus valid Python syntax.
+    """
+
+    tokens = token_utils.tokenize(source)
+    if not tokens:
+        return tokens
+
+    prev_token = tokens[0]
+    new_tokens = [prev_token]
+
+    for token in tokens[1:]:
+        if (
+            (
+                prev_token.is_number()
+                and (token.is_identifier() or token.is_number() or token == "(")
+            )
+            or (
+                prev_token.is_identifier()
+                and (token.is_identifier() or token.is_number())
+            )
+            or (prev_token == ")" and (token.is_identifier() or token.is_number()))
+        ):
+            new_tokens.append("*")
+        new_tokens.append(token)
+        prev_token = token
+
+    return token_utils.untokenize(new_tokens)
