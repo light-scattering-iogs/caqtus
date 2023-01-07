ac46c709ae4e261386616603c6d3fa4c0f239631
Damien <damien.bloch@institutoptique.fr>
2023-01-07 13:01:52 +0100

    Add phases optimization to reduce signal peak values

--- devices/tweezers/trap_signal_generator/trap_signal_generator/static_trap_generator.py
+++ devices/tweezers/trap_signal_generator/trap_signal_generator/static_trap_generator.py
@@ -1,20 +1,22 @@
+from collections import defaultdict
 from collections.abc import Sequence
+from itertools import product, chain
+from typing import Iterable
 
 import numpy as np
 from numba import njit, float64, prange
-from pydantic import BaseModel, validator, Field
+from pydantic import validator, Field
+from scipy.optimize import basinhopping
+
+from settings_model import SettingsModel
 
 
-class StaticTrapGenerator(BaseModel):
+class StaticTrapGenerator(SettingsModel):
     frequencies: Sequence[float] = Field(allow_mutation=False)
-    amplitudes: Sequence[float] = Field(allow_mutation=False)
-    phases: Sequence[float] = Field(allow_mutation=False)
+    amplitudes: Sequence[float] = Field()
+    phases: Sequence[float] = Field()
     sampling_rate: float = Field(gt=0, allow_mutation=False)
     number_samples: int = Field(gt=0, allow_mutation=False)
-
-    class Config:
-        validate_assignment = True
-        validate_all = True
 
     @validator("frequencies", pre=True)
     def validate_frequencies(cls, frequencies):
@@ -50,6 +52,15 @@ class StaticTrapGenerator(BaseModel):
     def times(self):
         return np.arange(self.number_samples) / self.sampling_rate
 
+    def optimize_phases(self):
+        """Changes its phases to reduce the peak values of the signal envelope."""
+
+        segment_frequency = self.sampling_rate / self.number_samples
+        optimal_phases = compute_optimized_phases(
+            self.frequencies, self.amplitudes, segment_frequency
+        )
+        self.phases = optimal_phases
+
 
 @njit(parallel=True)
 def compute_signal_numba(
@@ -68,3 +79,43 @@ def compute_signal_numba(
 
         result += amplitude * np.sin(2 * np.pi * t * frequency + phase)
     return result
+
+
+def compute_optimized_phases(
+    frequencies: Sequence[float],
+    amplitudes: Sequence[float],
+    segment_frequency: float,
+    initial_phases: Sequence[float] = None,
+):
+    amplitudes = np.array(amplitudes)
+    frequencies = np.array(frequencies)
+
+    integer_frequencies = np.round(frequencies / segment_frequency).astype(int)
+    indexes = np.array(list(_compute_quadruplets(integer_frequencies)))
+
+    def to_minimize(phases):
+        a = (amplitudes * np.exp(1j * phases))[indexes]
+        return np.real(np.sum(a[:, 0] * np.conj(a[:, 1]) * np.conj(a[:, 2]) * a[:, 3]))
+
+    if initial_phases:
+        initial_phases = np.array(initial_phases)
+    else:
+        initial_phases = np.random.uniform(0, 2 * np.pi, len(frequencies))
+
+    solution = basinhopping(to_minimize, initial_phases)
+    return solution.x
+
+
+def _compute_quadruplets(
+    frequencies: Sequence[int],
+) -> Iterable[tuple[int, int, int, int]]:
+    """Computes all indexes i,j,k,l such that frequencies[i]-frequencies[j] == frequencies[k]-frequencies[l]"""
+
+    diffs = defaultdict(list)
+    for (i, fi), (j, fj) in product(enumerate(frequencies), repeat=2):
+        diffs[fi - fj].append((i, j))
+
+    indexes = chain(
+        *map(lambda y: map(lambda x: x[0] + x[1], product(y, repeat=2)), diffs.values())
+    )
+    return indexes
