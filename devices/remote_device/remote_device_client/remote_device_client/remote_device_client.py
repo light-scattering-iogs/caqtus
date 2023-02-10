5ba548903ce6972e80f7bdb35431de940ab457ae
Damien Bloch <damien.bloch@institutoptique>
2023-02-10 11:10:50 +0100

    Add spincore and ni6738 to remote devices

--- devices/remote_device/remote_device_client/remote_device_client/remote_device_client.py
+++ devices/remote_device/remote_device_client/remote_device_client/remote_device_client.py
@@ -1,3 +1,5 @@
+"""This module defines a client class that can be used to create and manage remote devices"""
+
 from multiprocessing.managers import BaseManager
 
 
@@ -5,4 +7,13 @@ class RemoteDeviceClientManager(BaseManager):
     pass
 
 
-RemoteDeviceClientManager.register("OrcaQuestCamera")
+# Client will only try to ask for the creation of a device type if it is registered
+
+REGISTERED_DEVICE_TYPES: list[str] = [
+    "OrcaQuestCamera",
+    "SpincorePulseBlaster",
+    "NI6738AnalogCard",
+]
+
+for device_type in REGISTERED_DEVICE_TYPES:
+    RemoteDeviceClientManager.register(device_type)
--- devices/remote_device/remote_device_server/remote_device_server/remote_device_server.py
+++ devices/remote_device/remote_device_server/remote_device_server/remote_device_server.py
@@ -1,8 +1,32 @@
 import logging
 import os
 from multiprocessing.managers import BaseManager
+from typing import Type
 
-from orca_quest.runtime import OrcaQuestCamera
+from device import RuntimeDevice
+
+imported_devices: dict[str, Type[RuntimeDevice]] = {}
+
+try:
+    from orca_quest.runtime import OrcaQuestCamera
+
+    imported_devices["OrcaQuestCamera"] = OrcaQuestCamera
+except Exception:
+    pass
+
+try:
+    from spincore_sequencer.runtime import SpincorePulseBlaster
+
+    imported_devices["SpincorePulseBlaster"] = SpincorePulseBlaster
+except Exception:
+    pass
+
+try:
+    from ni6738_analog_card.runtime import NI6738AnalogCard
+
+    imported_devices["NI6738AnalogCard"] = NI6738AnalogCard
+except Exception:
+    pass
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -13,9 +37,10 @@ class RemoteDeviceServerManager(BaseManager):
     pass
 
 
-RemoteDeviceServerManager.register(
-    "OrcaQuestCamera", OrcaQuestCamera, exposed=OrcaQuestCamera.exposed_remote_methods()
-)
+for type_name, device_type in imported_devices.items():
+    RemoteDeviceServerManager.register(
+        type_name, device_type, exposed=device_type.exposed_remote_methods()
+    )
 
 password = os.environ.get("CAQTUS_DEVICE_SERVER_PASSWORD", None)
 if password is None:
@@ -27,4 +52,5 @@ manager = RemoteDeviceServerManager(
     address=("", 65000), authkey=bytes(password, encoding="utf-8")
 )
 server = manager.get_server()
+print(f"Server instantiated at {server.address}")
 server.serve_forever()
