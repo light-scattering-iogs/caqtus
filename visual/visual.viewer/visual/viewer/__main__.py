672ad7da600604560c5bc475f4784f9f2f0754cd
Damien <damien.bloch@institutoptique.fr>
2023-03-07 23:33:46 +0100

    Create visual viewer package

---
+++ visual/visual.viewer/visual/viewer/__main__.py
@@ -0,0 +1,26 @@
+import logging
+import sys
+
+import qdarkstyle
+from PyQt6.QtWidgets import QApplication
+
+from experiment.session import get_standard_experiment_session_maker
+
+
+def except_hook(cls, exception, traceback):
+    sys.__excepthook__(cls, exception, traceback)
+
+
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel("DEBUG")
+
+
+if __name__ == "__main__":
+    sys.excepthook = except_hook
+
+    app = QApplication(sys.argv)
+    app.setStyle("Fusion")
+    app.setStyleSheet(qdarkstyle.load_stylesheet())
+    session_maker = get_standard_experiment_session_maker()
+    app.exec()
