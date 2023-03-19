ab91fe30550599b549aa849f78aa3d2c263a1e67
Damien Bloch <damien.bloch@institutoptique>
2023-03-19 13:35:19 +0100

    Add yaml clipboard for unimplemented devices

--- condetrol/condetrol/experiment_viewer/config_editor/config_settings_editor.py
+++ condetrol/condetrol/experiment_viewer/config_editor/config_settings_editor.py
@@ -3,8 +3,10 @@ from typing import Optional
 
 from PyQt6.QtWidgets import QWidget, QHBoxLayout, QLabel
 
+from device_config import DeviceConfiguration
 from experiment.configuration import ExperimentConfig
 from qabc import QABC
+from yaml_clipboard_mixin import YAMLClipboardMixin
 
 
 class ConfigSettingsEditor(QWidget, QABC):
@@ -41,14 +43,14 @@ class ConfigSettingsEditor(QWidget, QABC):
         return tree_label[8:]
 
 
-class NotImplementedDeviceConfigEditor(ConfigSettingsEditor):
+class NotImplementedDeviceConfigEditor(ConfigSettingsEditor, YAMLClipboardMixin):
     def __init__(
         self,
         experiment_config: ExperimentConfig,
         tree_label: str,
         parent: Optional[QWidget] = None,
     ):
-        super().__init__(experiment_config, tree_label, parent)
+        super().__init__(config=experiment_config, tree_label=tree_label, parent=parent)
 
         self.config = experiment_config
         self.device_name = self.strip_device_prefix(tree_label)
@@ -61,6 +63,19 @@ class NotImplementedDeviceConfigEditor(ConfigSettingsEditor):
             )
         )
         self.setLayout(layout)
+
+    def convert_to_external_use(self):
+        return self.config.get_device_config(self.device_name)
+
+    def update_from_external_source(self, new_config: DeviceConfiguration):
+        if new_config.device_name != self.device_name:
+            raise ValueError(
+                f"Cannot change device name from {self.device_name} to {new_config.device_name}"
+            )
+        old_config = self.config.get_device_config(self.device_name)
+        if not isinstance(new_config, type(old_config)):
+            raise TypeError(f"Expected {type(old_config)} got {type(new_config)}")
+        self.config.set_device_config(self.device_name, new_config)
 
     def get_experiment_config(self) -> ExperimentConfig:
         return self.config
