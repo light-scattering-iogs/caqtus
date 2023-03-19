027d8beb74241aae2148d00377f8cc5b822cdcac
Damien Bloch <damien.bloch@institutoptique>
2023-03-19 13:56:12 +0100

    Allow to copy/paste spincore config to/from clipboard

--- condetrol/condetrol/experiment_viewer/config_editor/spincore_config_editor/spincore_config_editor.py
+++ condetrol/condetrol/experiment_viewer/config_editor/spincore_config_editor/spincore_config_editor.py
@@ -1,16 +1,14 @@
 import logging
 from typing import Optional
 
-from PyQt6.QtWidgets import (
-    QWidget,
-)
+from PyQt6.QtWidgets import QWidget
 
 from experiment.configuration import ExperimentConfig
 from spincore_sequencer.configuration import SpincoreSequencerConfiguration
 from .spincore_editor_ui import Ui_SpincoreEditor
 from ..channel_model import ChannelsModel
 from ..color_delegate import ColorCellDelegate
-from ..config_settings_editor import ConfigSettingsEditor
+from ..config_settings_editor import DeviceConfigEditor
 
 logger = logging.getLogger(__name__)
 logger.setLevel("DEBUG")
@@ -18,7 +16,7 @@ logger.setLevel("DEBUG")
 ns = 1e-9
 
 
-class SpincoreConfigEditor(ConfigSettingsEditor, Ui_SpincoreEditor):
+class SpincoreConfigEditor(DeviceConfigEditor, Ui_SpincoreEditor):
     def __init__(
         self,
         experiment_config: ExperimentConfig,
@@ -27,20 +25,19 @@ class SpincoreConfigEditor(ConfigSettingsEditor, Ui_SpincoreEditor):
     ):
         super().__init__(experiment_config, tree_label, parent)
 
-        self.device_name = self.strip_device_prefix(tree_label)
-        self.experiment_config = experiment_config
-        self.config: SpincoreSequencerConfiguration = (
-            experiment_config.get_device_config(self.device_name)
+        config: SpincoreSequencerConfiguration = (
+            self._experiment_config.get_device_config(self.device_name)
         )
 
         self.setupUi(self)
-        self.setup_ui_from_config(self.config)
+        self.setup_ui_from_config(config)
         self.channels_table_view.setItemDelegateForColumn(1, ColorCellDelegate())
 
     def get_experiment_config(self) -> ExperimentConfig:
-        self.write_ui_to_config(self.config)
-        self.experiment_config.set_device_config(self.device_name, self.config)
-        return self.experiment_config
+        new_config = self._experiment_config.get_device_config(self.device_name)
+        self.write_ui_to_config(new_config)
+        self._experiment_config.set_device_config(self.device_name, new_config)
+        return self._experiment_config
 
     def setup_ui_from_config(self, config: SpincoreSequencerConfiguration):
         self.board_number_spinbox.setValue(config.board_number)
@@ -52,6 +49,10 @@ class SpincoreConfigEditor(ConfigSettingsEditor, Ui_SpincoreEditor):
         config.board_number = self.board_number_spinbox.value()
         config.time_step = self.time_step_spinbox.value() * ns
         return config
+
+    def update_from_external_source(self, new_config: SpincoreSequencerConfiguration):
+        super().update_from_external_source(new_config)
+        self.setup_ui_from_config(new_config)
 
 
 class SpincoreChannelsModel(ChannelsModel):
