ed59008a412ebcc8f2138b64c7459dd1a423014d
Damien Bloch <damien.bloch@institutoptique>
2023-04-18 21:35:21 +0200

    Refactor units and analog quantities

---
+++ common/units/units/units.py
@@ -0,0 +1,52 @@
+from pathlib import Path
+from typing import Type
+
+import pint
+
+ureg = pint.UnitRegistry(
+    Path(__file__).parent / "units_definition.txt",
+    autoconvert_offset_to_baseunit=True,
+    cache_folder=":auto:",
+)
+unit_registry = ureg
+pint.set_application_registry(unit_registry)
+Quantity: Type[pint.quantity.Quantity] = unit_registry.Quantity
+Unit = pint.unit.Unit
+UndefinedUnitError = pint.UndefinedUnitError
+
+DimensionalityError = pint.DimensionalityError
+dimensionless = Quantity(1, "").units
+
+TIME_UNITS = {"s", "ms", "µs", "us", "ns"}
+
+FREQUENCY_UNITS = {
+    "Hz",
+    "kHz",
+    "MHz",
+    "GHz",
+    "THz",
+}
+
+POWER_UNITS = {
+    "W",
+    "mW",
+    "dBm",
+}
+
+DIMENSIONLESS_UNITS = {"dB"}
+
+CURRENT_UNITS = {"A", "mA"}
+
+VOLTAGE_UNITS = {"V", "mV"}
+
+UNITS = (
+    TIME_UNITS
+    | FREQUENCY_UNITS
+    | POWER_UNITS
+    | DIMENSIONLESS_UNITS
+    | CURRENT_UNITS
+    | VOLTAGE_UNITS
+)
+
+
+units = {unit: getattr(ureg, unit) for unit in UNITS}
