d6e105afd971e53517ccb03dd7bb2e301a4fa8a1
Damien Bloch <damien.bloch@institutoptique>
2023-04-18 21:35:29 +0200

    Refactor units and analog quantities

--- common/units/units/__init__.py
+++ common/units/units/__init__.py
@@ -1,53 +1,20 @@
 __version__ = "0.1.0"
 
-from pathlib import Path
-from typing import Type
-
-import pint
-
-ureg = pint.UnitRegistry(
-    Path(__file__).parent / "units_definition.txt",
-    autoconvert_offset_to_baseunit=True,
-    cache_folder=":auto:",
+from .analog_value import AnalogValue, is_analog_value, get_unit, magnitude_in_unit
+from .units import (
+    ureg,
+    unit_registry,
+    Quantity,
+    Unit,
+    UndefinedUnitError,
+    DimensionalityError,
+    dimensionless,
+    TIME_UNITS,
+    FREQUENCY_UNITS,
+    POWER_UNITS,
+    DIMENSIONLESS_UNITS,
+    CURRENT_UNITS,
+    VOLTAGE_UNITS,
+    UNITS,
+    units,
 )
-unit_registry = ureg
-pint.set_application_registry(unit_registry)
-Quantity: Type[pint.quantity.Quantity] = unit_registry.Quantity
-UndefinedUnitError = pint.UndefinedUnitError
-
-DimensionalityError = pint.DimensionalityError
-dimensionless = Quantity(1, "").units
-
-TIME_UNITS = {"s", "ms", "µs", "us", "ns"}
-
-FREQUENCY_UNITS = {
-    "Hz",
-    "kHz",
-    "MHz",
-    "GHz",
-    "THz",
-}
-
-POWER_UNITS = {
-    "W",
-    "mW",
-    "dBm",
-}
-
-DIMENSIONLESS_UNITS = {"dB"}
-
-CURRENT_UNITS = {"A", "mA"}
-
-VOLTAGE_UNITS = {"V", "mV"}
-
-UNITS = (
-    TIME_UNITS
-    | FREQUENCY_UNITS
-    | POWER_UNITS
-    | DIMENSIONLESS_UNITS
-    | CURRENT_UNITS
-    | VOLTAGE_UNITS
-)
-
-
-units = {unit: getattr(ureg, unit) for unit in UNITS}
