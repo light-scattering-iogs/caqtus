74411d88f4afaf95e8b8e70a205bb2dd3e1b5560
Damien <damien.bloch@institutoptique.fr>
2023-04-22 20:18:33 +0200

    Add decorator to validate function arguments

---
+++ common/settings_model/settings_model/validate_arguments.py
@@ -0,0 +1,21 @@
+from inspect import getfullargspec
+from typing import Callable, get_type_hints
+
+from pydantic import parse_obj_as
+
+
+def validate_arguments(function: Callable):
+    argspec = getfullargspec(function)
+    arg_names = argspec.args
+    if arg_names[0] == "self":
+        del arg_names[0]
+    type_hints = get_type_hints(function)
+
+    def wrapped(*args, **kwargs):
+        for index, arg in enumerate(args):
+            name = argspec.args[index]
+            type_hint = type_hints[name]
+            parse_obj_as(type_hint, arg)
+        return wrapped(*args, **kwargs)
+
+    return wrapped
