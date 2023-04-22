eb68623430f8c44c022d9e931dd44a888c19b138
Damien <damien.bloch@institutoptique.fr>
2023-04-22 20:16:35 +0200

    Add function to maximize likelihood to fit parameters to values

---
+++ analyza/analyza/maximum_likelihood.py
@@ -0,0 +1,22 @@
+import numpy as np
+from scipy.optimize import minimize
+from scipy.stats import norm
+
+Parameters = tuple[float]
+Values = np.ndarray
+
+
+def log_likelihood(values, model, parameters) -> float:
+    return float(np.sum(np.log(model(values, *parameters))))
+
+
+def maximize_likelihood(
+    values: Values, model, initial_parameters: Parameters, method: str = "Nelder-Mead"
+) -> Parameters:
+    return minimize(
+        lambda parameters: -log_likelihood(values, model, parameters),
+        x0=np.array(initial_parameters),
+        method=method,
+    ).x
+
+
