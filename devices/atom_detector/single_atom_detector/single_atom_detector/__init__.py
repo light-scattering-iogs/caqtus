013eda0a88ab1176bb7bdac1e79c482135568ce3
Damien Bloch <damien.bloch@institutoptique>
2023-05-15 20:40:43 +0200

    Move atom detector to new package

---
+++ devices/atom_detector/single_atom_detector/single_atom_detector/__init__.py
@@ -0,0 +1,3 @@
+from .single_atom_detector import SingleAtomDetector
+
+__all__ = ["SingleAtomDetector"]
--- devices/tweezers/atom_detector/atom_detector/__init__.py
+++
@@ -1,4 +0,0 @@
-from .atom_detector import AtomDetector
-from .atom_signal_calculator import WeightedAtomSignalCalculator
-
-__all__ = ["AtomDetector", "WeightedAtomSignalCalculator"]
