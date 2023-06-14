bec25f68fd3ae5ff063e12ecee08cb09e882f1fc
damien <damien.bloch@institutoptique.fr>
2023-06-14 23:17:43 +0200

    Create viewer launcher

--- viewer/viewer.launcher/viewer/launcher/launcher.py
+++ viewer/viewer.launcher/viewer/launcher/launcher.py
@@ -4,19 +4,21 @@ from PyQt6.QtWidgets import QApplication
 
 from analyza.loading.importers import ImageImporter
 from device.configuration import DeviceName
-from experiment.session import get_standard_experiment_session
 from sequence.runtime import Sequence
+from viewer.sequence_watcher import SequenceWatcher
 from viewer.single_shot_viewers import ImageViewer
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
 
-    session = get_standard_experiment_session()
-    sequence = Sequence("2023.06_June.14.survival_138_5")
-    with session.activate():
-        shots = sequence.get_shots(session)
-
     viewer = ImageViewer(importer=ImageImporter(DeviceName("Orca Quest"), "picture"))
-    viewer.set_shot(shots[0])
     viewer.show()
+
+    watcher = SequenceWatcher(
+        Sequence("2023.06_June.14.test"),
+        target=lambda shots: viewer.set_shot(shots[-1]),
+        watch_interval=0.1,
+    )
+    watcher.start()
+
     app.exec()
