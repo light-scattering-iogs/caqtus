19b7fe1afc7546da236b00123bc9f01d6ffd3663
damien <damien.bloch@institutoptique.fr>
2023-06-14 22:50:59 +0200

    Fix image type

--- common/types/image_types/image_types/image_type.py
+++ common/types/image_types/image_types/image_type.py
@@ -1,9 +1,11 @@
 from numbers import Real
 from typing import TypeVar
 
-from numpy.typing import NDArray
+import numpy as np
 
 Width = TypeVar("Width", bound=int)
 Height = TypeVar("Height", bound=int)
 
-Image = NDArray[tuple[Width, Height], Real]
+T = TypeVar("T", bound=Real)
+
+Image = np.ndarray[tuple[Width, Height], np.dtype[T]]
