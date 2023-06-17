4dbad9eb8242dbb9ddb5af34e55d9b6287203265
Damien Bloch <damien.bloch@institutoptique>
2023-06-17 20:30:24 +0200

    Move device name in subpackage

--- devices/device/device.configuration/device/configuration/__init__.py
+++ devices/device/device.configuration/device/configuration/__init__.py
@@ -1,5 +1,5 @@
+from device.name import DeviceName
 from .device_config import DeviceConfiguration, DeviceConfigType
-from .device_name import DeviceName
 from .device_parameter import DeviceParameter
 
 __all__ = ["DeviceConfiguration", "DeviceConfigType", "DeviceParameter", "DeviceName"]
---
+++ devices/device/device.name/device/name/__init__.py
@@ -0,0 +1,3 @@
+from .device_name import DeviceName
+
+__all__ = ["DeviceName"]
