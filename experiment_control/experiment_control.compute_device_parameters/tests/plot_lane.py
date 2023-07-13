2f9e50ed606a05747cc069248b098029a20fa7b6
damien <damien.bloch@institutoptique.fr>
2023-07-13 14:06:06 +0200

    Plot camera lane

--- experiment_control/experiment_control.compute_device_parameters/tests/plot_lane.py
+++ experiment_control/experiment_control.compute_device_parameters/tests/plot_lane.py
@@ -1,53 +1,59 @@
 from collections.abc import Iterable
-from numbers import Real
 
 import matplotlib.pyplot as plt
 import numpy as np
 
+from experiment.configuration import ExperimentConfig
 from experiment_control.compute_device_parameters import (
     compile_step_durations,
-    compile_analog_lane,
-    get_step_bounds,
+    compute_shot_parameters, get_step_bounds,
 )
-from sequence.configuration import ShotConfiguration, AnalogLane
+from sequence.configuration import SequenceConfig
 from sequencer.channel import ChannelInstruction, ChannelPattern, Repeat, Concatenate
 from variable.namespace import VariableNamespace
 
 
 def test_compile_analog_lane(
-    shot_config: ShotConfiguration, variables: VariableNamespace
+    experiment_config_2: ExperimentConfig,
+    sequence_config_2: SequenceConfig,
+    variables_2: VariableNamespace,
 ) -> None:
+    shot_config = sequence_config_2.shot_configurations["shot"]
     durations = compile_step_durations(
         step_durations=shot_config.step_durations,
         step_names=shot_config.step_names,
-        variables=variables,
+        variables=variables_2,
     )
 
-    time_step = 50
+    parameters = compute_shot_parameters(
+        experiment_config_2, sequence_config_2.shot_configurations["shot"], variables_2
+    )
+
+    cam_trig = parameters["NI6738 card"]["sequence"][0]
     #
     # lane = shot_config.find_lane("Tweezers power (AOM)")
     # assert isinstance(lane, AnalogLane)
     # instruction = compile_analog_lane(
     #     shot_config.step_names, durations, lane, variables, time_step
     # )
-    plot_instruction(instruction, durations, time_step)
+    plot_instruction(cam_trig, durations, 2.5e-6)
 
 
 def plot_instruction(
     instruction: ChannelInstruction,
-    # step_durations: Iterable[float],
+    step_durations: Iterable[float],
     time_step: float,
 ) -> None:
     values = instruction.flatten().values
     times = np.arange(len(values)) * time_step
     plt.plot(times, values, drawstyle="steps-post")
-    # step_bounds = get_step_bounds(step_durations)
-    # for step_bound in step_bounds:
-    #     plt.axvline(step_bound, ls="--", color="k")
+    step_bounds = get_step_bounds(step_durations)
+    for step_bound in step_bounds:
+        plt.axvline(step_bound, ls="--", color="k", zorder=-1)
     plt.show()
 
-# test_compile_analog_lane()
 
+# test_compile_analog_lane()
 
 
 instruction = Concatenate(
@@ -150,4 +156,3 @@ instruction = Concatenate(
         Repeat(ChannelPattern((False,)), 199764),
     )
 )
-plot_instruction(instruction, 50)
