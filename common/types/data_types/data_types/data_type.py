e17cfeca3ea985067fbf9713ae1263afdaf25c81
Damien Bloch <damien.bloch@institutoptique>
2023-07-26 10:22:15 +0200

    Fix dictionary for Data type

--- common/types/data_types/data_types/data_type.py
+++ common/types/data_types/data_types/data_type.py
@@ -3,8 +3,10 @@ from typing import TypeAlias
 
 import numpy as np
 
+DataKey: TypeAlias = bool | str | Real | tuple["DataKey", ...]
+
 
 Array = np.ndarray
 Data: TypeAlias = (
-    bool | str | Real | Array | dict[str, "Data"] | list["Data"] | tuple["Data"]
+        bool | str | Real | Array | dict[DataKey, "Data"] | list["Data"] | tuple["Data"]
 )
