5d2d5b1ea0832d59b8ad2b67e85fab716b39b5df
Damien <damien.bloch@institutoptique.fr>
2023-07-26 21:20:13 +0200

    Replace pycuda with cuda-python

--- devices/tweezer_arranger/aod_tweezer_arranger.runtime/aod_tweezer_arranger/runtime/signal_generator.py
+++ devices/tweezer_arranger/aod_tweezer_arranger.runtime/aod_tweezer_arranger/runtime/signal_generator.py
@@ -1,9 +1,9 @@
 import math
-from typing import NewType, SupportsFloat, SupportsInt
+from contextlib import ExitStack
+from typing import NewType, SupportsFloat, SupportsInt, Self
 
 import numpy as np
-import pycuda.driver as cuda
-from pycuda.compiler import SourceModule
+from cuda import nvrtc, cuda
 
 from .static_traps_cuda import get_static_traps_cuda_program
 
@@ -13,6 +13,49 @@ NumberSamples = NewType("NumberSamples", int)
 NUMBER_THREADS_PER_BLOCK = 1024
 
 AWGSignalArray = np.ndarray[NumberSamples, np.dtype[np.int16]]
+
+
+def _check_cuda_error(fun):
+    def wrapper(*args, **kwargs):
+        err, *result = fun(*args, **kwargs)
+        if isinstance(err, cuda.CUresult):
+            if err != cuda.CUresult.CUDA_SUCCESS:
+                raise RuntimeError("Cuda Error: {}".format(err))
+        elif isinstance(err, nvrtc.nvrtcResult):
+            if err != nvrtc.nvrtcResult.NVRTC_SUCCESS:
+                raise RuntimeError("Nvrtc Error: {}".format(err))
+        else:
+            raise RuntimeError("Unknown error type: {}".format(err))
+        if len(result) == 0:
+            return None
+        elif len(result) == 1:
+            return result[0]
+        else:
+            return result
+
+    return wrapper
+
+
+nvrtcCreateProgram = _check_cuda_error(nvrtc.nvrtcCreateProgram)
+nvrtcCompileProgram = _check_cuda_error(nvrtc.nvrtcCompileProgram)
+nvrtcGetPTXSize = _check_cuda_error(nvrtc.nvrtcGetPTXSize)
+nvrtcGetPTX = _check_cuda_error(nvrtc.nvrtcGetPTX)
+cuInit = _check_cuda_error(cuda.cuInit)
+cuDeviceGet = _check_cuda_error(cuda.cuDeviceGet)
+cuCtxCreate = _check_cuda_error(cuda.cuCtxCreate)
+cuCtxDestroy = _check_cuda_error(cuda.cuCtxDestroy)
+cuModuleLoadData = _check_cuda_error(cuda.cuModuleLoadData)
+cuModuleUnload = _check_cuda_error(cuda.cuModuleUnload)
+cuModuleGetFunction = _check_cuda_error(cuda.cuModuleGetFunction)
+cuModuleGetGlobal = _check_cuda_error(cuda.cuModuleGetGlobal)
+cuStreamCreate = _check_cuda_error(cuda.cuStreamCreate)
+cuStreamDestroy = _check_cuda_error(cuda.cuStreamDestroy)
+cuMemAlloc = _check_cuda_error(cuda.cuMemAlloc)
+cuMemFree = _check_cuda_error(cuda.cuMemFree)
+cuMemcpyHtoDAsync = _check_cuda_error(cuda.cuMemcpyHtoDAsync)
+cuLaunchKernel = _check_cuda_error(cuda.cuLaunchKernel)
+cuStreamSynchronize = _check_cuda_error(cuda.cuStreamSynchronize)
+cuMemcpyDtoHAsync = _check_cuda_error(cuda.cuMemcpyDtoHAsync)
 
 
 class SignalGenerator:
@@ -28,26 +71,47 @@ class SignalGenerator:
         self._sampling_rate = float(sampling_rate)
         self._time_step = 1 / self._sampling_rate
         self._max_number_tones = int(max_number_tones)
-        cuda.init()
-        from pycuda.tools import make_default_context
+        self._exit_stack = ExitStack()
 
-        self._context = make_default_context()
-        self._device = self._context.get_device()
-        self.setup_cuda()
-
-    def __enter__(self):
+    def __enter__(self) -> Self:
+        self._exit_stack = ExitStack()
+        self._exit_stack.__enter__()
+        self._initialize()
         return self
 
+    def _initialize(self):
+        source = get_static_traps_cuda_program(self._max_number_tones)
+        program = nvrtcCreateProgram(
+            str.encode(source), b"generate_signal.cu", 0, [], []
+        )
+        # compute_61 refer to the architecture of the GPU used (6.1)
+        # should be replaced for other GPUs
+        opts = [b"--fmad=false", b"--gpu-architecture=compute_61"]
+        nvrtcCompileProgram(program, 2, opts)
+        ptx_size = nvrtcGetPTXSize(program)
+        ptx = b" " * ptx_size
+        nvrtcGetPTX(program, ptx)
+        cuda.cuInit(0)
+
+        # Retrieve handle for device 0, should be changed with multiple GPUs
+        cu_device = cuDeviceGet(0)
+
+        self._context = cuCtxCreate(0, cu_device)
+        self._exit_stack.callback(cuCtxDestroy, self._context)
+        ptx = np.char.array(ptx)
+        module = cuModuleLoadData(ptx.ctypes.data)
+        self._exit_stack.callback(cuModuleUnload, module)
+        self._compute_static_traps_signal = cuModuleGetFunction(
+            module, b"compute_static_traps_signal"
+        )
+        self._amplitudes_gpu = cuModuleGetGlobal(module, b"amplitudes")[0]
+        self._frequencies_gpu = cuModuleGetGlobal(module, b"frequencies")[0]
+        self._phases_gpu = cuModuleGetGlobal(module, b"phases")[0]
+        self._stream = cuStreamCreate(0)
+        self._exit_stack.callback(cuStreamDestroy, self._stream)
+
     def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
-
-    def close(self):
-        self._context.pop()
-        self._context = None
-
-        from pycuda.tools import clear_context_caches
-
-        clear_context_caches()
+        self._exit_stack.__exit__(exc_type, exc_value, traceback)
 
     @property
     def sampling_rate(self) -> float:
@@ -56,16 +120,6 @@ class SignalGenerator:
     @property
     def time_step(self) -> float:
         return self._time_step
-
-    def setup_cuda(self):
-        source = get_static_traps_cuda_program(self._max_number_tones)
-        self._module = SourceModule(source)
-        self._compute_static_traps_signal = self._module.get_function(
-            "compute_static_traps_signal"
-        )
-        self._amplitudes_gpu = self._module.get_global("amplitudes")[0]
-        self._frequencies_gpu = self._module.get_global("frequencies")[0]
-        self._phases_gpu = self._module.get_global("phases")[0]
 
     def generate_signal_static_traps(
         self,
@@ -81,24 +135,54 @@ class SignalGenerator:
             )
 
         output = np.zeros(number_samples, dtype=np.int16)
-        amplitudes_f32 = np.array(amplitudes, dtype=np.float32)
-        frequencies_f32 = np.array(frequencies, dtype=np.float32)
-        phases_f32 = np.array(phases, dtype=np.float32)
-        cuda.memcpy_htod(self._amplitudes_gpu, amplitudes_f32)
-        cuda.memcpy_htod(self._frequencies_gpu, frequencies_f32)
-        cuda.memcpy_htod(self._phases_gpu, phases_f32)
+        with ExitStack() as stack:
+            output_gpu = cuMemAlloc(output.nbytes)
+            stack.callback(cuMemFree, output_gpu)
 
-        block = (NUMBER_THREADS_PER_BLOCK, 1, 1)
-        grid = (math.ceil(number_samples / NUMBER_THREADS_PER_BLOCK), 1, 1)
-        output_gpu = cuda.mem_alloc(output.nbytes)
-        self._compute_static_traps_signal(
-            output_gpu,
-            np.uint32(number_samples),
-            np.uint32(number_tones),
-            np.float32(self._time_step),
-            block=block,
-            grid=grid,
-        )
-        cuda.memcpy_dtoh(output, output_gpu)
-        output_gpu.free()
+            amplitudes_f32 = np.array(amplitudes, dtype=np.float32)
+            frequencies_f32 = np.array(frequencies, dtype=np.float32)
+            phases_f32 = np.array(phases, dtype=np.float32)
+            cuMemcpyHtoDAsync(
+                self._amplitudes_gpu,
+                amplitudes_f32.ctypes.data,
+                amplitudes_f32.nbytes,
+                self._stream,
+            )
+            cuMemcpyHtoDAsync(
+                self._frequencies_gpu,
+                frequencies_f32.ctypes.data,
+                frequencies_f32.nbytes,
+                self._stream,
+            )
+            cuMemcpyHtoDAsync(
+                self._phases_gpu,
+                phases_f32.ctypes.data,
+                phases_f32.nbytes,
+                self._stream,
+            )
+            arguments = [
+                np.array([int(output_gpu)], dtype=np.uint64),
+                np.array([number_samples], dtype=np.uint32),
+                np.array([number_tones], dtype=np.uint32),
+                np.array([self._time_step], dtype=np.float32),
+            ]
+            args = np.array([arg.ctypes.data for arg in arguments], dtype=np.uint64)
+            number_blocks = math.ceil(number_samples / NUMBER_THREADS_PER_BLOCK)
+            cuLaunchKernel(
+                self._compute_static_traps_signal,
+                number_blocks,
+                1,
+                1,
+                NUMBER_THREADS_PER_BLOCK,
+                1,
+                1,
+                0,
+                self._stream,
+                args.ctypes.data,
+                0,
+            )
+            cuMemcpyDtoHAsync(
+                output.ctypes.data, output_gpu, output.nbytes, self._stream
+            )
+            cuStreamSynchronize(self._stream)
         return output
