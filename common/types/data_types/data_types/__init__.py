a367db6a0437d837e8293ae6c581899d1ffec9e5
Damien <damien.bloch@institutoptique.fr>
2023-09-20 18:28:56 +0200

    Add DataKey to data_types.__init__

--- common/types/data_types/data_types/__init__.py
+++ common/types/data_types/data_types/__init__.py
@@ -1,5 +1,5 @@
 from .data_label import DataLabel, is_data_label
-from .data_type import Data, Array
+from .data_type import Data, Array, DataKey
 from .is_data import is_data
 
-__all__ = ["Data", "Array", "is_data", "DataLabel", "is_data_label"]
+__all__ = ["Data", "Array", "is_data", "DataLabel", "is_data_label", "DataKey"]
