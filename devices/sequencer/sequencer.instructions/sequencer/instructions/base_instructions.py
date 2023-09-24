e9dd9ee0d8137d4f1c97d6ae133ff52f24234188
Damien Bloch <bloc.dam@gmail.com>
2023-09-24 22:49:51 +0200

    Add docstrings to SequenceInstruction and implement better __iter__ methods

--- devices/sequencer/sequencer.instructions/sequencer/instructions/base_instructions.py
+++ devices/sequencer/sequencer.instructions/sequencer/instructions/base_instructions.py
@@ -1,3 +1,4 @@
+import itertools
 import logging
 from abc import ABC, abstractmethod
 from functools import cached_property
@@ -10,6 +11,88 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 T = TypeVar("T", covariant=True)
+
+
+class SequenceInstruction(ABC, Generic[T]):
+    """Interface defining a compact representation of a sequence of values.
+
+    Instances of this class are a higher level representation of a sequence of values
+    than bare arrays. They are easier to manipulate and can be stored more efficiently.
+    Instead of storing all the values explicitly, they instead store the instructions to
+    generate the sequence.
+
+    For instance Pattern([1, 2, 3]) * 10_000_000 + Pattern([4, 5]) * 10_000_000,
+    represents a sequence of 50 million values, but has a very small memory footprint
+    since we only store the number of repetitions and the values to repeat instead of
+    each value explicitly.
+
+    Instances of this class are immutable. They can be concatenated with the `+`
+    operator and repeated an integer number of times with the `*` operator. They can be
+    subscripted with an integer or a slice.
+    """
+
+    @abstractmethod
+    def __len__(self) -> int:
+        """Return the length of the sequence represented by this instruction."""
+
+        raise NotImplementedError()
+
+    @abstractmethod
+    def __iter__(self) -> Iterator[T]:
+        """Return an iterator over the explicit values represented by this instruction.
+
+        This is mostly useful for debugging purposes as it transforms an implicit
+        representation into an explicit one, thus losing all potential performance
+        benefits.
+        """
+
+        raise NotImplementedError()
+
+    @overload
+    def __getitem__(self, index: int) -> T:
+        ...
+
+    @overload
+    def __getitem__(self, index: slice) -> "SequenceInstruction[T]":
+        ...
+
+    @abstractmethod
+    def __getitem__(self, index):
+        raise NotImplementedError()
+
+    def __add__(self, other) -> "SequenceInstruction[T]":
+        if isinstance(other, SequenceInstruction):
+            if len(other) == 0:
+                return self
+            elif len(self) == 0:
+                return other
+            else:
+                return Add(self, other)
+        else:
+            return NotImplemented
+
+    def __mul__(self, other) -> "SequenceInstruction[T]":
+        if isinstance(other, int):
+            if other == 0:
+                return self[:0]
+            elif other == 1:
+                return self
+            else:
+                if len(self) == 0:
+                    return self
+                else:
+                    return Multiply(other, self)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other) -> "SequenceInstruction[T]":
+        return self.__mul__(other)
+
+    @classmethod
+    def empty_like(
+        cls, instruction: "SequenceInstruction[T]"
+    ) -> "SequenceInstruction[T]":
+        return instruction[:0]
 
 
 @runtime_checkable
@@ -32,57 +115,20 @@ class InternalPattern(Protocol[T]):
         raise NotImplementedError()
 
 
-class Instruction(ABC, Generic[T]):
-    @abstractmethod
-    def __len__(self) -> int:
-        raise NotImplementedError()
-
-    def __iter__(self) -> Iterator[T]:
-        return (self[i] for i in range(len(self)))
-
-    @overload
-    def __getitem__(self, index: int) -> T:
-        ...
-
-    @overload
-    def __getitem__(self, index: slice) -> "Instruction[T]":
-        ...
-
-    @abstractmethod
-    def __getitem__(self, index):
-        raise NotImplementedError()
-
-    def __add__(self, other) -> "Instruction[T]":
-        if isinstance(other, Instruction):
-            if len(other) == 0:
-                return self
-            elif len(self) == 0:
-                return other
-            else:
-                return Add(self, other)
-        else:
-            return NotImplemented
-
-    def __mul__(self, other) -> "Instruction[T]":
-        if isinstance(other, int):
-            if other == 0:
-                return self[:0]
-            elif other == 1:
-                return self
-            else:
-                if len(self) == 0:
-                    return self
-                else:
-                    return Multiply(other, self)
-        else:
-            return NotImplemented
-
-    def __rmul__(self, other) -> "Instruction[T]":
-        return self.__mul__(other)
-
-
 @frozen
-class Pattern(Instruction[T]):
+class Pattern(SequenceInstruction[T]):
+    """Explicit representation of a sequence of values.
+
+    This is the most specific way to represent a sequence of values, by specifying them
+    explicitly. It is also the most verbose and memory expensive way to represent the
+    values.
+
+    Fields:
+        pattern: The sequence of values wrapped by this instruction. This must be a
+        sequence of values implementing the `__len__` and `__getitem__` methods, like
+        for example a list or a numpy array.
+    """
+
     pattern: InternalPattern[T]
 
     def __len__(self) -> int:
@@ -101,9 +147,15 @@ class Pattern(Instruction[T]):
 
 
 @frozen
-class Add(Instruction[T]):
-    left: Instruction[T] = field()
-    right: Instruction[T] = field()
+class Add(SequenceInstruction[T]):
+    """Represent the concatenation of two sequences.
+
+    Instances of this class are pure concatenations of two sequences: the left and right
+    part cannot be empty.
+    """
+
+    left: SequenceInstruction[T] = field()
+    right: SequenceInstruction[T] = field()
 
     # noinspection PyUnresolvedReferences
     @left.validator
@@ -119,6 +171,9 @@ class Add(Instruction[T]):
 
     def __len__(self) -> int:
         return self._length
+
+    def __iter__(self):
+        return itertools.chain(self.left, self.right)
 
     @cached_property
     def _length(self):
@@ -145,9 +200,18 @@ class Add(Instruction[T]):
 
 
 @frozen
-class Multiply(Instruction[T]):
+class Multiply(SequenceInstruction[T]):
+    """Represent the repetition of a sequence of values.
+
+    This allows to repeat a sequence of value a large number of times without having to
+    store the sequence explicitly.
+
+    Instances of this class are pure repetitions of a sequence: the repeated sequence
+    cannot be empty and the number of repetitions must be strictly greater than 1.
+    """
+
     repetitions: int = field()
-    instruction: Instruction[T] = field()
+    instruction: SequenceInstruction[T] = field()
 
     # noinspection PyUnresolvedReferences
     @repetitions.validator
@@ -169,6 +233,11 @@ class Multiply(Instruction[T]):
     @cached_property
     def _length(self):
         return self.repetitions * len(self.instruction)
+
+    def __iter__(self):
+        return itertools.chain.from_iterable(
+            itertools.repeat(self.instruction, self.repetitions)
+        )
 
     def __getitem__(self, index):
         if isinstance(index, int):
@@ -224,7 +293,7 @@ class Multiply(Instruction[T]):
     def __mul__(self, other):
         if isinstance(other, int):
             if other == 0:
-                return empty_like(self)
+                return SequenceInstruction.empty_like(self)
             elif other == 1:
                 return self
             else:
@@ -257,7 +326,3 @@ def _normalize_slice(index: slice, length: int) -> tuple[int, int, int]:
         step = index.step
 
     return start, stop, step
-
-
-def empty_like(instruction: Instruction[T]) -> Instruction[T]:
-    return instruction[:0]
