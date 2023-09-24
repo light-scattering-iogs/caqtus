b9279efbe7e1f546e6e80bcd211a74a76b61347a
Damien Bloch <bloc.dam@gmail.com>
2023-09-24 22:21:32 +0200

    Implement Multiply instruction with test

--- devices/sequencer/sequencer.instructions/sequencer/instructions/base_instructions.py
+++ devices/sequencer/sequencer.instructions/sequencer/instructions/base_instructions.py
@@ -1,5 +1,7 @@
 import logging
-from abc import ABC
+from abc import ABC, abstractmethod
+from functools import cached_property
+from math import floor, ceil
 from typing import TypeVar, Protocol, runtime_checkable, overload, Iterator, Generic
 
 from attr import frozen, field
@@ -31,6 +33,7 @@ class InternalPattern(Protocol[T]):
 
 
 class Instruction(ABC, Generic[T]):
+    @abstractmethod
     def __len__(self) -> int:
         raise NotImplementedError()
 
@@ -45,6 +48,7 @@ class Instruction(ABC, Generic[T]):
     def __getitem__(self, index: slice) -> "Instruction[T]":
         ...
 
+    @abstractmethod
     def __getitem__(self, index):
         raise NotImplementedError()
 
@@ -58,6 +62,23 @@ class Instruction(ABC, Generic[T]):
                 return Add(self, other)
         else:
             return NotImplemented
+
+    def __mul__(self, other) -> "Instruction[T]":
+        if isinstance(other, int):
+            if other == 0:
+                return self[:0]
+            elif other == 1:
+                return self
+            else:
+                if len(self) == 0:
+                    return self
+                else:
+                    return Multiply(other, self)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other) -> "Instruction[T]":
+        return self.__mul__(other)
 
 
 @frozen
@@ -74,6 +95,9 @@ class Pattern(Instruction[T]):
             return Pattern(self.pattern[index])
         else:
             raise TypeError("Index must be int or slice.")
+
+    def __iter__(self):
+        return iter(self.pattern)
 
 
 @frozen
@@ -94,35 +118,23 @@ class Add(Instruction[T]):
             raise ValueError("Right instruction is empty.")
 
     def __len__(self) -> int:
+        return self._length
+
+    @cached_property
+    def _length(self):
         return len(self.left) + len(self.right)
 
     def __getitem__(self, index):
         if isinstance(index, int):
-            while index < 0:
-                index += len(self)
+            index = _normalize_integer_index(index, len(self))
             if index < len(self.left):
                 return self.left[index]
             else:
                 return self.right[index - len(self.left)]
         elif isinstance(index, slice):
-            if index.start is None:
-                start = 0
-            else:
-                start = index.start
-            if index.stop is None:
-                stop = len(self)
-            else:
-                stop = index.stop
-            if index.step is None:
-                step = 1
-            else:
-                step = index.step
+            start, stop, step = _normalize_slice(index, len(self))
             if step != 1:
                 raise ValueError("Step must be 1.")
-            while start < 0:
-                start += len(self)
-            while stop < 0:
-                stop += len(self)
             if start < len(self.left):
                 if stop <= len(self.left):
                     return self.left[start:stop]
@@ -130,3 +142,122 @@ class Add(Instruction[T]):
                     return self.left[start:] + self.right[: stop - len(self.left)]
             else:
                 return self.right[start - len(self.left) : stop - len(self.left)]
+
+
+@frozen
+class Multiply(Instruction[T]):
+    repetitions: int = field()
+    instruction: Instruction[T] = field()
+
+    # noinspection PyUnresolvedReferences
+    @repetitions.validator
+    def _validate_repetitions(self, _, value):
+        if value <= 1:
+            raise ValueError(
+                f"Repetitions must be strictly greater than 1, got {value}."
+            )
+
+    # noinspection PyUnresolvedReferences
+    @instruction.validator
+    def _validate_instruction(self, _, value):
+        if len(value) == 0:
+            raise ValueError("Instruction is empty.")
+
+    def __len__(self):
+        return self._length
+
+    @cached_property
+    def _length(self):
+        return self.repetitions * len(self.instruction)
+
+    def __getitem__(self, index):
+        if isinstance(index, int):
+            index = _normalize_integer_index(index, len(self))
+            return self.instruction[index % len(self.instruction)]
+        elif isinstance(index, slice):
+            start, stop, step = _normalize_slice(index, len(self))
+            if step != 1:
+                raise ValueError("Step must be 1.")
+            if len(self.instruction) == 1:
+                return self.instruction * (stop - start)
+
+            before_instruction = floor(start / len(self.instruction))
+            before_instruction_start = before_instruction * len(self.instruction)
+            next_instruction = floor(stop / len(self.instruction))
+            next_instruction_start = next_instruction * len(self.instruction)
+
+            if before_instruction == next_instruction:
+                return self.instruction[
+                    start - before_instruction_start : stop - before_instruction_start
+                ]
+            else:
+                first_whole_instruction = ceil(start / len(self.instruction))
+                first_whole_instruction_start = first_whole_instruction * len(
+                    self.instruction
+                )
+                last_whole_instruction = floor(stop / len(self.instruction))
+                last_whole_instruction_stop = last_whole_instruction * len(
+                    self.instruction
+                )
+
+                whole_repetitions = last_whole_instruction - first_whole_instruction
+
+                if whole_repetitions < 0:
+                    whole_repetitions = 0
+
+                before = self.instruction[
+                    start
+                    - before_instruction_start : first_whole_instruction_start
+                    - before_instruction_start
+                ]
+                middle = self.instruction * whole_repetitions
+
+                after = self.instruction[
+                    last_whole_instruction_stop
+                    - next_instruction_start : stop
+                    - next_instruction_start
+                ]
+                return before + middle + after
+        else:
+            raise TypeError("Index must be int or slice.")
+
+    def __mul__(self, other):
+        if isinstance(other, int):
+            if other == 0:
+                return empty_like(self)
+            elif other == 1:
+                return self
+            else:
+                return Multiply(self.repetitions * other, self.instruction)
+        else:
+            return NotImplemented
+
+    def __rmul__(self, other):
+        return self.__mul__(other)
+
+
+def _normalize_integer_index(index: int, length: int) -> int:
+    if index < 0:
+        index += length
+    return index
+
+
+def _normalize_slice(index: slice, length: int) -> tuple[int, int, int]:
+    if index.start is None:
+        start = 0
+    else:
+        start = _normalize_integer_index(index.start, length)
+    if index.stop is None:
+        stop = length
+    else:
+        stop = _normalize_integer_index(index.stop, length)
+    if index.step is None:
+        step = 1
+    else:
+        step = index.step
+
+    return start, stop, step
+
+
+def empty_like(instruction: Instruction[T]) -> Instruction[T]:
+    return instruction[:0]
---
+++ devices/sequencer/sequencer.instructions/tests/test_operations.py
@@ -0,0 +1,71 @@
+import numpy as np
+from hypothesis import given
+from hypothesis.strategies import composite, booleans, integers
+
+from sequencer.instructions.base_instructions import Pattern
+
+
+@composite
+def pattern(draw):
+    length = draw(integers(min_value=0, max_value=10))
+    return Pattern(np.arange(length))
+
+
+@composite
+def add(draw, depth: int):
+    shortest_length = draw(integers(min_value=0, max_value=depth))
+    if draw(booleans()):
+        return draw(fixed_depth_instruction(depth=depth - 1)) + draw(
+            fixed_depth_instruction(depth=shortest_length)
+        )
+    else:
+        return draw(fixed_depth_instruction(depth=shortest_length)) + draw(
+            fixed_depth_instruction(depth=depth - 1)
+        )
+
+
+@composite
+def multiply(draw, depth: int):
+    return draw(integers(min_value=0, max_value=10)) * draw(
+        fixed_depth_instruction(depth=depth - 1)
+    )
+
+
+@composite
+def fixed_depth_instruction(draw, depth: int):
+    if depth == 0:
+        return draw(pattern())
+    else:
+        if draw(booleans()):
+            return draw(multiply(depth=depth))
+        else:
+            return draw(add(depth=depth))
+
+
+@composite
+def instruction(draw, max_depth: int):
+    return draw(
+        fixed_depth_instruction(depth=draw(integers(min_value=0, max_value=max_depth)))
+    )
+
+
+@given(instruction(max_depth=3), instruction(max_depth=3))
+def test_addition(left, right):
+    s = left + right
+    assert list(s) == list(left) + list(right)
+
+
+@composite
+def instruction_and_slice(draw, max_depth: int):
+    instr = draw(instruction(max_depth=max_depth))
+    start = draw(integers(min_value=0, max_value=len(instr)))
+    stop = draw(integers(min_value=start, max_value=len(instr)))
+    return instr, start, stop
+
+
+@given(
+    instruction_and_slice(max_depth=6),
+)
+def test_slice(args):
+    instr, start, stop = args
+    assert list(instr[start:stop]) == list(instr)[start:stop]
