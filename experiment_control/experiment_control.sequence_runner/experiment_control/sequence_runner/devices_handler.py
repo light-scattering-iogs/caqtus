ce71ad741f236f3123d2716bc63d33986a2046ac
Damien Bloch <damien.bloch@institutoptique>
2023-10-03 18:02:41 +0200

    Use asyncio to program devices and start shot

--- experiment_control/experiment_control.sequence_runner/experiment_control/sequence_runner/devices_handler.py
+++ experiment_control/experiment_control.sequence_runner/experiment_control/sequence_runner/devices_handler.py
@@ -1,6 +1,5 @@
 import asyncio
 import logging
-from concurrent.futures import ThreadPoolExecutor
 from contextlib import AbstractContextManager, ExitStack
 from threading import Lock
 from typing import (
@@ -19,7 +18,6 @@ from experiment_control.sequence_runner.device_context_manager import (
 )
 from sequencer.configuration import SequencerConfiguration
 from sequencer.runtime import Sequencer
-from .task_group import TaskGroup
 
 if TYPE_CHECKING:
     from camera.runtime import Camera
@@ -71,16 +69,28 @@ class DevicesHandler(AbstractContextManager):
     def update_device_parameters(
         self, device_parameters: Mapping[DeviceName, dict[DeviceParameter, Any]]
     ):
-        with ThreadPoolExecutor() as thread_pool, TaskGroup(thread_pool) as g:
+        asyncio.run(self._update_device_parameters(device_parameters))
+
+    async def _update_device_parameters(
+        self, device_parameters: Mapping[DeviceName, dict[DeviceParameter, Any]]
+    ):
+        async with asyncio.TaskGroup() as g:
             for device_name, parameters in device_parameters.items():
-                g.add_task(update_device, self._devices[device_name], parameters)
+                g.create_task(
+                    asyncio.to_thread(
+                        update_device, self._devices[device_name], parameters
+                    )
+                )
 
     def start_shot(self):
-        with ThreadPoolExecutor() as thread_pool, TaskGroup(thread_pool) as g:
-            for tweezer_arrangers in self.tweezer_arrangers.values():
-                g.add_task(tweezer_arrangers.start_sequence)
+        asyncio.run(self._start_shot())
+
+    async def _start_shot(self):
+        async with asyncio.TaskGroup() as g:
+            for tweezer_arranger in self.tweezer_arrangers.values():
+                g.create_task(asyncio.to_thread(tweezer_arranger.start_sequence))
             for camera in self.cameras.values():
-                g.add_task(camera.start_acquisition)
+                g.create_task(asyncio.to_thread(camera.start_acquisition))
         # we need the sequencers to be correctly triggered, so we start them in their priority order
         for sequencer in self.sequencers.values():
             sequencer.start_sequence()
