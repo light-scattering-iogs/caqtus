f545f1e337a9aa066324f6a3249aa5ea6d88412d
Damien <damien.bloch@institutoptique.fr>
2023-10-31 23:49:43 +0100

    Register ROI subclasses for serialization

--- common/roi/roi/__init__.py
+++ common/roi/roi/__init__.py
@@ -1,5 +1,10 @@
+from util import serialization
 from .arbitrary_roi import ArbitraryROI
 from .rectangular_roi import RectangularROI
 from .roi import ROI
+
+serialization.include_subclasses(
+    ROI, union_strategy=serialization.include_type(tag_name="roi_type")
+)
 
 __all__ = ["ArbitraryROI", "RectangularROI", "ROI"]
--- common/roi/tests/test_roi_serialization.py
+++ common/roi/tests/test_roi_serialization.py
@@ -1,8 +1,9 @@
-from roi import RectangularROI
-
+from roi import ROI, RectangularROI
 from settings_model import YAMLSerializable
+from util import serialization
 
 
 def test_roi_serialization():
     roi = RectangularROI(original_image_size=(10, 20), x=1, y=2, width=3, height=4)
-    assert(YAMLSerializable.load(YAMLSerializable.dump(roi)) == roi)
+    assert YAMLSerializable.load(YAMLSerializable.dump(roi)) == roi
+    assert serialization.structure(serialization.unstructure(roi), ROI) == roi
