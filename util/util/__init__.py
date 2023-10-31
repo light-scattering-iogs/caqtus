b21e32d2fc702ca34b24bd632de8777364bc4be2
Damien <damien.bloch@institutoptique.fr>
2023-10-31 22:45:12 +0100

    Fix imports

--- util/util/__init__.py
+++ util/util/__init__.py
@@ -1,6 +1,5 @@
 import attrs as attrs
 
-import inspect_function as inspect_function
 from ._log_exception import log_exception
 from ._run_on_change_only import run_on_change_method
 from .duration_timer import DurationTimer, DurationTimerLog
