28ea0866195f4383e6f5c0702e1a7ebf3bc9e297
Damien <damien.bloch@institutoptique.fr>
2023-11-01 00:32:13 +0100

    Remove discrimination tag when serializing ROI

--- common/roi/roi/__init__.py
+++ common/roi/roi/__init__.py
@@ -3,8 +3,6 @@ from .arbitrary_roi import ArbitraryROI
 from .rectangular_roi import RectangularROI
 from .roi import ROI
 
-serialization.include_subclasses(
-    ROI, union_strategy=serialization.include_type(tag_name="roi_type")
-)
+serialization.include_subclasses(ROI)
 
 __all__ = ["ArbitraryROI", "RectangularROI", "ROI"]
