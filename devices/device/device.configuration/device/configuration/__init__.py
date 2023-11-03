d1a7e942b1ac0c2abc9611ea1998fec2b7938145
Damien Bloch <bloc.dam@gmail.com>
2023-11-03 18:59:27 +0100

    Create attrs copy of DeviceConfiguration

--- devices/device/device.configuration/device/configuration/__init__.py
+++ devices/device/device.configuration/device/configuration/__init__.py
@@ -1,5 +1,15 @@
 from device.name import DeviceName
-from .device_config import DeviceConfiguration, DeviceConfigType
+from .device_config import (
+    DeviceConfiguration,
+    DeviceConfigurationAttrs,
+    DeviceConfigType,
+)
 from .device_parameter import DeviceParameter
 
-__all__ = ["DeviceConfiguration", "DeviceConfigType", "DeviceParameter", "DeviceName"]
+__all__ = [
+    "DeviceConfiguration",
+    "DeviceConfigurationAttrs",
+    "DeviceConfigType",
+    "DeviceParameter",
+    "DeviceName",
+]
