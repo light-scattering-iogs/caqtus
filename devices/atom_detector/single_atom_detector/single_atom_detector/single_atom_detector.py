ba02d89bf390413e508405fccbcb8c8eb11857bf
Damien Bloch <bloc.dam@gmail.com>
2023-11-04 15:21:02 +0100

    SingleAtomDetector is no longer an instance of YAMLSerializable

--- devices/atom_detector/single_atom_detector/single_atom_detector/single_atom_detector.py
+++ devices/atom_detector/single_atom_detector/single_atom_detector/single_atom_detector.py
@@ -1,61 +1,32 @@
-from numbers import Number
-from typing import Self
-
 import numpy as np
-import yaml
 
 from settings_model import YAMLSerializable
+from util import attrs
 from .atom_signal_calculator import WeightedAtomSignalCalculator
 
 
-class SingleAtomDetector(YAMLSerializable):
+@attrs.define(slots=False)
+class SingleAtomDetector:
     """Look at an image and determine whether one specific atom is present."""
 
-    def __init__(
-        self, atom_signal_calculator: WeightedAtomSignalCalculator, threshold: float
-    ):
-        if not isinstance(atom_signal_calculator, WeightedAtomSignalCalculator):
-            raise TypeError(
-                f"atom_signal_calculator must be an AtomSignalCalculator, not {type(atom_signal_calculator)}"
-            )
-        if not isinstance(threshold, Number):
-            raise TypeError(f"threshold must be a number, not {type(threshold)}")
-        self._atom_signal_calculator = atom_signal_calculator
-        self._threshold = float(threshold)
+    atom_signal_calculator: WeightedAtomSignalCalculator = attrs.field(
+        validator=attrs.validators.instance_of(WeightedAtomSignalCalculator),
+        on_setattr=attrs.setters.validate,
+    )
+    threshold: float = attrs.field(
+        converter=float,
+        on_setattr=attrs.setters.convert,
+    )
 
     def is_atom_present(self, image: np.ndarray) -> bool:
         """Determine whether the associated atom is present in the image"""
 
-        return self.compute_signal(image) > self._threshold
+        return self.compute_signal(image) > self.threshold
 
     def compute_signal(self, image: np.ndarray) -> float:
         """Compute the signal for the associated atom in the image."""
 
-        return self._atom_signal_calculator.compute_signal(image)
+        return self.atom_signal_calculator.compute_signal(image)
 
-    @classmethod
-    def representer(cls, dumper: yaml.Dumper, settings: Self):
-        """Return a yaml representation of the object"""
 
-        return dumper.represent_mapping(
-            f"!{cls.__name__}",
-            {
-                "atom_signal_calculator": settings._atom_signal_calculator,
-                "threshold": settings._threshold,
-            },
-        )
-
-    @classmethod
-    def constructor(cls, loader: yaml.Loader, node: yaml.Node):
-        """Construct an object from a yaml node"""
-
-        kwargs = loader.construct_mapping(node, deep=True)
-        return cls(**kwargs)
-
-    def __eq__(self, other):
-        if not isinstance(other, SingleAtomDetector):
-            return False
-        return (
-            self._atom_signal_calculator == other._atom_signal_calculator
-            and self._threshold == other._threshold
-        )
+YAMLSerializable.register_attrs_class(SingleAtomDetector)
