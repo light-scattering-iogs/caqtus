79ee58a4f94b1738f3c4d864fba7d9a3da053168
Damien Bloch <bloc.dam@gmail.com>
2023-11-06 11:19:58 +0100

    Formatting

--- devices/elliptec_ell14/elliptec_ell14.configuration/elliptec_ell14/configuration/ell14_configuration.py
+++ devices/elliptec_ell14/elliptec_ell14.configuration/elliptec_ell14/configuration/ell14_configuration.py
@@ -15,14 +15,17 @@ class ElliptecELL14RotationStageConfiguration(DeviceConfiguration):
     """Holds static configuration to control an ELL14 rotation stage device
 
     Attributes:
-        serial_port: The serial port to use to communicate with the device. e.g. "COM9"
-        device_id: The ID of the device. This is what is referred as the address in the thorlabs Ello software.
-            If the device is used in multi-port mode, a single serial port can control multiple devices with different
-            device IDs. However, this is not supported at the moment and only one device can be instantiated for a given
-            serial port.
-        position: The position of the stage in degrees. This can be an expression that depends on other variables.
-            When these variables change, the new position will be recalculated in consequence and the stage will move to
-            the new position.
+        serial_port: The serial port to use to communicate with the device.
+            e.g. "COM9"
+        device_id: The ID of the device. This is what is referred as the
+            address in the thorlabs Ello software. If the device is used in multi-port
+            mode, a single serial port can control multiple devices with different
+            device IDs. However, this is not supported at the moment and only one device
+            can be instantiated for a given serial port.
+        position: The position of the stage in degrees. This can be an expression that
+            depends on other variables. When these variables change, the new position
+            will be recalculated in consequence and the stage will move to the new
+            position.
     """
 
     serial_port: str
