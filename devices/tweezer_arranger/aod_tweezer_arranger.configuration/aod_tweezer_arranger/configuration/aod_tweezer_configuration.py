1c90bf2cba8a98c9973fd5b35a6eb9a6c8400c32
Damien Bloch <bloc.dam@gmail.com>
2023-11-08 18:55:01 +0100

    AODTweezerConfiguration is no longer a subclass of SettingsModel

--- devices/tweezer_arranger/aod_tweezer_arranger.configuration/aod_tweezer_arranger/configuration/aod_tweezer_configuration.py
+++ devices/tweezer_arranger/aod_tweezer_arranger.configuration/aod_tweezer_arranger/configuration/aod_tweezer_configuration.py
@@ -1,10 +1,16 @@
-from pydantic import validator, Field
+from typing import Iterable, SupportsFloat
 
-from settings_model import SettingsModel
+from settings_model import YAMLSerializable
 from tweezer_arranger.configuration import TweezerConfiguration2D, TweezerLabel
+from util import attrs
 
 
-class AODTweezerConfiguration(SettingsModel, TweezerConfiguration2D):
+def to_tuple_of_float(values: Iterable[SupportsFloat]) -> tuple[float, ...]:
+    return tuple(float(v) for v in values)
+
+
+@attrs.define(slots=False)
+class AODTweezerConfiguration(TweezerConfiguration2D):
     """Contains the information to generate a grid of trap with an AOD/AWG.
 
     Fields:
@@ -21,85 +27,83 @@ class AODTweezerConfiguration(SettingsModel, TweezerConfiguration2D):
         number_samples / sampling_rate, the waveform is repeated.
     """
 
-    frequencies_x: tuple[float, ...]
-    phases_x: tuple[float, ...]
-    amplitudes_x: tuple[float, ...]
-    scale_x: float = Field(ge=0.0)
-    frequencies_y: tuple[float, ...]
-    phases_y: tuple[float, ...]
-    amplitudes_y: tuple[float, ...]
-    scale_y: float = Field(ge=0.0)
+    frequencies_x: tuple[float, ...] = attrs.field(
+        converter=to_tuple_of_float, on_setattr=attrs.setters.convert
+    )
+    phases_x: tuple[float, ...] = attrs.field(
+        converter=to_tuple_of_float, on_setattr=attrs.setters.convert
+    )
+    amplitudes_x: tuple[float, ...] = attrs.field(
+        converter=to_tuple_of_float, on_setattr=attrs.setters.convert
+    )
+    scale_x: float = attrs.field(
+        converter=float,
+        validator=attrs.validators.ge(0.0),
+        on_setattr=attrs.setters.pipe(attrs.setters.convert, attrs.setters.validate),
+    )
+    frequencies_y: tuple[float, ...] = attrs.field(
+        converter=to_tuple_of_float, on_setattr=attrs.setters.convert
+    )
+    phases_y: tuple[float, ...] = attrs.field(
+        converter=to_tuple_of_float, on_setattr=attrs.setters.convert
+    )
+    amplitudes_y: tuple[float, ...] = attrs.field(
+        converter=to_tuple_of_float, on_setattr=attrs.setters.convert
+    )
+    scale_y: float = attrs.field(
+        converter=float,
+        validator=attrs.validators.ge(0.0),
+        on_setattr=attrs.setters.pipe(attrs.setters.convert, attrs.setters.validate),
+    )
 
-    sampling_rate: float = Field(ge=0.0)
-    number_samples: int = Field(ge=1)
+    sampling_rate: float = attrs.field(
+        converter=float,
+        validator=attrs.validators.ge(0.0),
+        on_setattr=attrs.setters.pipe(attrs.setters.convert, attrs.setters.validate),
+    )
+    number_samples: int = attrs.field(
+        converter=int,
+        validator=attrs.validators.ge(1),
+        on_setattr=attrs.setters.pipe(attrs.setters.convert, attrs.setters.validate),
+    )
 
-    @validator("frequencies_x")
-    def validate_frequencies_x(cls, frequencies_x):
+    @frequencies_x.validator  # type: ignore
+    def validate_frequencies_x(self, _, frequencies_x):
         if not all(f >= 0 for f in frequencies_x):
             raise ValueError("Frequencies must be positive.")
-        return tuple(float(f) for f in frequencies_x)
 
-    @validator("frequencies_y")
-    def validate_frequencies_y(cls, frequencies_y):
+    @frequencies_y.validator  # type: ignore
+    def validate_frequencies_y(self, _, frequencies_y):
         if not all(f >= 0 for f in frequencies_y):
             raise ValueError("Frequencies must be positive.")
-        return tuple(float(f) for f in frequencies_y)
 
-    @validator("phases_x")
-    def validate_phases_x(cls, phases_x, values):
-        if not len(phases_x) == len(values["frequencies_x"]):
+    @phases_x.validator  # type: ignore
+    def validate_phases_x(self, _, phases_x):
+        if not len(phases_x) == len(self.frequencies_x):
             raise ValueError(
                 "The number of phases must be equal to the number of frequencies."
             )
-        return tuple(float(p) for p in phases_x)
 
-    @validator("phases_y")
-    def validate_phases_y(cls, phases_y, values):
-        if not len(phases_y) == len(values["frequencies_y"]):
+    @phases_y.validator  # type: ignore
+    def validate_phases_y(self, _, phases_y):
+        if not len(phases_y) == len(self.frequencies_y):
             raise ValueError(
                 "The number of phases must be equal to the number of frequencies."
             )
-        return tuple(float(p) for p in phases_y)
 
-    @validator("amplitudes_x")
-    def validate_amplitude_x(cls, amplitude_x, values):
-        if not len(amplitude_x) == len(values["frequencies_x"]):
+    @amplitudes_x.validator  # type: ignore
+    def validate_amplitude_x(self, _, amplitude_x):
+        if not len(amplitude_x) == len(self.frequencies_x):
             raise ValueError(
                 "The number of amplitudes must be equal to the number of frequencies."
             )
-        return tuple(float(a) for a in amplitude_x)
 
-    @validator("amplitudes_y")
-    def validate_amplitude_y(cls, amplitude_y, values):
-        if not len(amplitude_y) == len(values["frequencies_y"]):
+    @amplitudes_y.validator  # type: ignore
+    def validate_amplitude_y(self, _, amplitude_y):
+        if not len(amplitude_y) == len(self.frequencies_y):
             raise ValueError(
                 "The number of amplitudes must be equal to the number of frequencies."
             )
-        return tuple(float(a) for a in amplitude_y)
-
-    @validator("scale_x")
-    def validate_scale_x(cls, scale_x):
-        if not scale_x >= 0:
-            raise ValueError("The scale must be positive.")
-        return float(scale_x)
-
-    @validator("scale_y")
-    def validate_scale_y(cls, scale_y):
-        if not scale_y >= 0:
-            raise ValueError("The scale must be positive.")
-        return float(scale_y)
-
-    @validator("sampling_rate")
-    def validate_sampling_rate(cls, sampling_rate):
-        if not sampling_rate >= 0:
-            raise ValueError("The sampling rate must be positive.")
-        return float(sampling_rate)
-
-    @validator("number_samples")
-    def validate_number_samples(cls, number_samples):
-        if not number_samples >= 1:
-            raise ValueError("The number of samples must be greater than 1.")
-        return int(number_samples)
 
     @property
     def number_tweezers(self) -> int:
@@ -126,3 +130,6 @@ class AODTweezerConfiguration(SettingsModel, TweezerConfiguration2D):
     @property
     def position_units(self) -> str:
         return "MHz"
+
+
+YAMLSerializable.register_attrs_class(AODTweezerConfiguration)
