0875c4d75dac16d07f50e8f26560ba3419921319
Damien Bloch <damien.bloch@institutoptique>
2023-11-12 16:35:03 +0100

    Fix serialization of SecretStr

--- common/experiment/experiment.configuration/experiment/configuration/device_server_config/device_server_config.py
+++ common/experiment/experiment.configuration/experiment/configuration/device_server_config/device_server_config.py
@@ -4,12 +4,21 @@ from settings_model import YAMLSerializable
 from util import attrs, serialization
 
 
+def secret_str_converter(value: str | SecretStr) -> SecretStr:
+    if isinstance(value, SecretStr):
+        return value
+    elif isinstance(value, str):
+        return SecretStr(value)
+    else:
+        raise TypeError(f"Expected str or SecretStr, got {value} of type {type(value)}")
+
+
 @attrs.define
 class DeviceServerConfiguration:
     address: str = attrs.field(converter=str, on_setattr=attrs.setters.convert)
     port: int = attrs.field(converter=int, on_setattr=attrs.setters.convert)
     authkey: SecretStr = attrs.field(
-        converter=SecretStr, on_setattr=attrs.setters.convert
+        converter=secret_str_converter, on_setattr=attrs.setters.convert
     )
 
 
@@ -23,7 +32,7 @@ def secret_str_unstructure(secret_str: SecretStr) -> str:
 serialization.register_unstructure_hook(SecretStr, secret_str_unstructure)
 
 
-def secret_str_structure(secret_str) -> SecretStr:
+def secret_str_structure(secret_str, _) -> SecretStr:
     return SecretStr(secret_str)
 
 
