048dd0a32a54e2368929e5559a208f9c7e4cded3
Damien Bloch <damien.bloch@institutoptique>
2023-11-18 18:01:30 +0100

    Add function to discover devices installed in the current environment

--- devices/device/device.runtime/device/runtime/__init__.py
+++ devices/device/device.runtime/device/runtime/__init__.py
@@ -1,4 +1,5 @@
+from ._installed_devices_discovery import discover_installed_devices
 from .device import Device
 from .runtime_device import RuntimeDevice
 
-__all__ = ["Device", "RuntimeDevice",]
+__all__ = ["Device", "RuntimeDevice", "discover_installed_devices"]
