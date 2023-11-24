75d9d412601d1e44bdc910e3bff8b16204e35258
Damien <damien.bloch@institutoptique.fr>
2023-11-24 19:11:15 +0100

    Export data types in graphplot data_loading

--- ui/graphplot/graphplot/data_loading/__init__.py
+++ ui/graphplot/graphplot/data_loading/__init__.py
@@ -1,6 +1,6 @@
 """Objects in this module are used to define how data are pulled from shots."""
 
-from ._data_importer import DataImporter
+from ._data_importer import DataImporter, ShotData, RowData
 from ._data_loader_selector import DataLoaderSelector
 
-__all__ = ["DataLoaderSelector", "DataImporter"]
+__all__ = ["DataLoaderSelector", "DataImporter", "ShotData", "RowData"]
