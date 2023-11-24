73334f0d756c290cbe43b3906b065e07441366ea
Damien <damien.bloch@institutoptique.fr>
2023-11-24 23:37:50 +0100

    Change sequence analyzer to have one thread per monitored sequence

--- ui/graphplot/graphplot/sequence_analyzer.py
+++ ui/graphplot/graphplot/sequence_analyzer.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import collections.abc
+import concurrent.futures
+import contextlib
 import datetime
 import itertools
 import threading
@@ -13,35 +15,49 @@ from experiment.session import ExperimentSessionMaker, ExperimentSession
 from sequence.runtime import Sequence, Shot
 from sequence.runtime.shot import ShotNotFoundError
 from util import attrs
-from util.concurrent import BackgroundScheduler
+from util.concurrent import TaskGroup
 from util.itertools import batched
 from .data_loading import DataImporter
 
 
 class SequenceAnalyzer:
     def __init__(self, session_maker: ExperimentSessionMaker) -> None:
-        self._sequences: dict[Sequence, _SequenceInfo] = {}
+        self._monitored_sequences: dict[Sequence, _SequenceInfo] = {}
         self._session_maker = session_maker
-        self._background_scheduler = BackgroundScheduler(max_workers=1)
         self._must_interrupt = threading.Event()
+        self._number_loaded_shots = 0
+        self._number_shots_to_load = 0
+
+        self._thread_pool = concurrent.futures.ThreadPoolExecutor()
+        self._task_group = TaskGroup(self._thread_pool)
+        self._exit_stack = contextlib.ExitStack()
+        self._lock = threading.RLock()
 
     def __enter__(self) -> Self:
-        self._background_scheduler.__enter__()
-        self._background_scheduler.schedule_task(self._update_sequences, 0.5)
+        self._exit_stack.__enter__()
+        self._exit_stack.enter_context(self._thread_pool)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
-        # We signal that the background task must be stopped fast. Otherwise, we will have to have that all the data
-        # have been loaded from the sequence before we can exit.
-        self._must_interrupt.set()
-        return self._background_scheduler.__exit__(exc_type, exc_val, exc_tb)
+        with self._lock:
+            exceptions = []
+            for sequence in self.sequences:
+
+                try:
+                    self.stop_monitoring_sequence(sequence)
+                except Exception as error:
+                    exceptions.append(error)
+            result = self._exit_stack.__exit__(exc_type, exc_val, exc_tb)
+            if exceptions:
+                raise ExceptionGroup(
+                    "Errors occurred while shutting down sequence analyzer", exceptions
+                )
+            return result
 
     @property
     def sequences(self) -> list[Sequence]:
-        return list(self._sequences)
-
-    def is_running(self) -> bool:
-        return self._background_scheduler.is_running()
+        with self._lock:
+            return list(self._monitored_sequences)
 
     def monitor_sequence(self, sequence: Sequence, data_importer: DataImporter) -> None:
         """Starts to load and analyze data a sequence.
@@ -54,22 +70,45 @@ class SequenceAnalyzer:
                 used as the name of the columns to construct a dataframe.
         """
 
-        with self._session_maker() as session:
-            stats = sequence.get_stats(session)
-        sequence_infos = _SequenceInfo(
-            start_date=stats.start_date,
-            shot_importer=data_importer,
-            dataframe=None,
-            lock=threading.Lock(),
-        )
-        if sequence in self._sequences:
-            with self._get_sequence_lock(sequence):
-                self._sequences[sequence] = sequence_infos
-        else:
-            self._sequences[sequence] = sequence_infos
+        with self._lock:
+            self.stop_monitoring_sequence(sequence)
+
+            with self._session_maker() as session:
+                stats = sequence.get_stats(session)
+
+            sequence_infos = _SequenceInfo(
+                start_date=stats.start_date,
+                shot_importer=data_importer,
+                dataframe=None,
+                number_shots_to_load=stats.number_completed_shots,
+                must_interrupt=threading.Event(),
+                future=None
+            )
+            self._monitored_sequences[sequence] = sequence_infos
+            sequence_infos.future = self._thread_pool.submit(self._load_sequence_data, sequence)
+
+    def stop_monitoring_sequence(self, sequence: Sequence) -> None:
+        """Stops monitoring a sequence.
+
+        If the sequence is not currently monitored, this method return immediately. Otherwise, it will signal the
+        background thread monitoring the sequence to stop and block until this thread finishes.
+
+        Raises:
+            The error that happened in the background thread should one have happened.
+        """
+
+        with self._lock:
+            if sequence in self._monitored_sequences:
+                self._monitored_sequences[sequence].must_interrupt.set()
+                self._monitored_sequences[sequence].future.result()
+                del self._monitored_sequences[sequence]
 
     def get_sequence_dataframes(self) -> dict[Sequence, Optional[polars.DataFrame]]:
-        return {sequence: info.dataframe for sequence, info in self._sequences.items()}
+        with self._lock:
+            return {
+                sequence: info.dataframe
+                for sequence, info in self._monitored_sequences.items()
+            }
 
     def get_dataframe(self) -> Optional[polars.DataFrame]:
         dataframes_to_concatenate = [
@@ -82,33 +121,25 @@ class SequenceAnalyzer:
         else:
             return None
 
-    def _update_sequences(self) -> None:
-        for sequence in list(self._sequences):
-            if self._must_interrupt.is_set():
-                break
-            self._update_sequence(sequence)
-
-    def _update_sequence(self, sequence: Sequence) -> None:
-        with self._session_maker() as session:
-            with self._get_sequence_lock(sequence):
+    def _load_sequence_data(self, sequence: Sequence) -> None:
+        while not self._monitored_sequences[sequence].must_interrupt.is_set():
+            with self._session_maker() as session:
                 self._check_sequence_change(sequence, session)
                 self._import_new_shots(sequence, session)
-
-    def _get_sequence_lock(self, sequence: Sequence) -> threading.Lock:
-        return self._sequences[sequence].lock
+            self._monitored_sequences[sequence].must_interrupt.wait(0.5)
 
     def _check_sequence_change(self, sequence: Sequence, session: ExperimentSession):
         start_date = sequence.get_stats(session).start_date
-        previous_start_date = self._sequences[sequence].start_date
+        previous_start_date = self._monitored_sequences[sequence].start_date
         if previous_start_date != start_date:
             self._reset_data_loading(sequence, session)
 
     def _reset_data_loading(
         self, sequence: Sequence, session: ExperimentSession
     ) -> None:
-        new_start_date = sequence.get_stats(session).start_date
-        self._sequences[sequence].start_date = new_start_date
-        self._sequences[sequence].dataframe = None
+        stats = sequence.get_stats(session)
+        self._monitored_sequences[sequence].start_date = stats.start_date
+        self._monitored_sequences[sequence].dataframe = None
 
     def _import_new_shots(self, sequence: Sequence, session: ExperimentSession) -> None:
         new_shots = self._get_not_yet_imported_shots(sequence, session)
@@ -116,8 +147,8 @@ class SequenceAnalyzer:
         # Here we process new shots in batch. This has two purposes: the dataframe will slowly increase if we load a
         # sequence with many shots, and it won't just happen once all shots are loaded.
         # We also check if we must exit fast between each batch, in which case we finish this task as soon as possible.
-        for shots in batched(new_shots, 20):
-            if self._must_interrupt.is_set():
+        for shots in batched(new_shots, 10):
+            if self._monitored_sequences[sequence].must_interrupt.is_set():
                 break
             try:
                 self._append_shots_to_dataframe(sequence, shots, session)
@@ -134,7 +165,7 @@ class SequenceAnalyzer:
         session: ExperimentSession,
     ):
         if shots:
-            shot_importer = self._sequences[sequence].shot_importer
+            shot_importer = self._monitored_sequences[sequence].shot_importer
             rows = itertools.chain.from_iterable(
                 shot_importer(shot, session) for shot in shots
             )
@@ -142,27 +173,30 @@ class SequenceAnalyzer:
         else:
             dataframe_to_append = None
 
-        if (old_dataframe := self._sequences[sequence].dataframe) is not None:
+        if (old_dataframe := self._monitored_sequences[sequence].dataframe) is not None:
             if dataframe_to_append is not None:
-                self._sequences[sequence].dataframe = polars.concat(  # type: ignore
+                self._monitored_sequences[sequence].dataframe = polars.concat(  # type: ignore
                     [old_dataframe, dataframe_to_append]
                 )
         else:
-            self._sequences[sequence].dataframe = dataframe_to_append
+            self._monitored_sequences[sequence].dataframe = dataframe_to_append
 
     def _get_not_yet_imported_shots(
         self, sequence: Sequence, session: ExperimentSession
     ) -> list[Shot]:
         shots = sequence.get_shots(session)
-        return shots[self._sequences[sequence].number_imported_shots :]
+        return shots[self._monitored_sequences[sequence].number_imported_shots :]
 
 
 @attrs.define
 class _SequenceInfo:
+    number_shots_to_load: int
     start_date: Optional[datetime.datetime]
     shot_importer: ShotImporter
     dataframe: Optional[polars.DataFrame]
-    lock: threading.Lock
+
+    must_interrupt: threading.Event
+    future: concurrent.futures.Future
 
     @property
     def number_imported_shots(self) -> int:
