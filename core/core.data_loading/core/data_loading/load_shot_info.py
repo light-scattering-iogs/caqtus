b382089b10c17e831e9fd122ee796900f1fdd175
Damien <damien.bloch@institutoptique.fr>
2023-11-25 23:00:32 +0100

    Add function to load time at which a shot was executed

--- core/core.data_loading/core/data_loading/load_shot_info.py
+++ core/core.data_loading/core/data_loading/load_shot_info.py
@@ -22,14 +22,28 @@ def get_shot_info_importer(lazy: bool = False) -> DataImporter | LazyDataImporte
     # We can't concatenate two LazyFrames horizontally, so we load them eagerly
     params_importer = get_parameters_importer(False)
     id_importer = get_shot_id_importer(False)
+    time_importer = get_shot_time_importer()
 
     def importer(shot: Shot, session: ExperimentSession):
         id_ = id_importer(shot, session)
         params = params_importer(shot, session)
-        dataframe = polars.concat([id_, params], how="horizontal")
+        shot_time = time_importer(shot, session)
+        dataframe = polars.concat([id_, shot_time, params], how="horizontal")
         if lazy:
             return dataframe.lazy()
         else:
             return dataframe
 
+    return importer
+
+
+def get_shot_time_importer() -> DataImporter:
+    def importer(shot: Shot, session: ExperimentSession):
+        start_time = polars.Series(
+            "start time", [shot.get_start_time(session)], dtype=polars.Datetime
+        )
+        stop_time = polars.Series(
+            "end time", [shot.get_end_time(session)], dtype=polars.Datetime
+        )
+        return polars.DataFrame([start_time, stop_time])
     return importer
