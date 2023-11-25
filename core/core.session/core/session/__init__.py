2e5fdb9ffa867462b3ac43da373860ae6f42acf4
Damien <damien.bloch@institutoptique.fr>
2023-11-25 13:01:56 +0100

    Add core.session package

---
+++ core/core.session/core/session/__init__.py
@@ -0,0 +1,4 @@
+from experiment.session import ExperimentSession, ExperimentSessionMaker
+from sequence.runtime import Shot, Sequence
+
+__all__ = ["ExperimentSession", "ExperimentSessionMaker", "Sequence", "Shot"]
