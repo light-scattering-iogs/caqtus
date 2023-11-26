36beb5f3292a3236fd3d4c90d04054687fac4bd5
Damien <damien.bloch@institutoptique.fr>
2023-11-26 17:31:36 +0100

    Compute stats for error bars in new thread

--- ui/graphplot/graphplot/visualization/visualizers/errorbar_visualizer.py
+++ ui/graphplot/graphplot/visualization/visualizers/errorbar_visualizer.py
@@ -3,10 +3,11 @@ from __future__ import annotations
 from collections.abc import Sequence
 from typing import Optional, Literal, TypeAlias
 
+import attrs
 import numpy as np
 import polars
 import pyqtgraph
-from PyQt6 import QtGui
+from PyQt6 import QtGui, QtCore
 from PyQt6.QtCore import pyqtSignal
 from PyQt6.QtWidgets import QWidget
 from pyqtgraph import PlotWidget
@@ -50,30 +51,106 @@ class ErrorBarViewCreator(QWidget, ViewCreator, Ui_ErrorBarVisualizerCreator):
 WhichAxis: TypeAlias = Literal["left", "right", "top", "bottom"]
 
 
-class ErrorBarView(PlotWidget, DataView):
-    data_updated = pyqtSignal()
+@attrs.define
+class Stats:
+    x_values: np.ndarray
+    y_values: np.ndarray
+    error_values: np.ndarray
+    x_unit: Optional[Unit]
+    y_unit: Optional[Unit]
 
+
+class ComputeStatsThread(QtCore.QThread):
+    stats_computed = pyqtSignal(Stats)
+
+    def __init__(
+        self,
+        parent: ErrorBarView,
+        dataframe: Optional[polars.DataFrame],
+        x_var: str,
+        y_var: str,
+        hue: Optional[str] = None,
+    ):
+        super().__init__(parent=parent)
+        self._parent = parent
+        self._dataframe = dataframe
+        self._x_var = x_var
+        self._y_var = y_var
+        self._hue = hue
+
+    def run(self) -> None:
+        if self._dataframe is not None and len(self._dataframe) > 0:
+            stats = self.process_data()
+        else:
+            stats = Stats(
+                x_values=np.array([]),
+                y_values=np.array([]),
+                error_values=np.array([]),
+                x_unit=None,
+                y_unit=None,
+            )
+        self._parent._stats = stats
+
+    def process_data(self) -> Stats:
+        x_var = self._x_var
+        y_var = self._y_var
+
+        x_magnitudes, x_unit = convert_to_single_unit(self._dataframe[x_var])
+        y_magnitudes, y_unit = convert_to_single_unit(self._dataframe[y_var])
+
+        data = polars.DataFrame([x_magnitudes, y_magnitudes])
+
+        mean = polars.col(y_var).mean()
+        sem = polars.col(y_var).std() / polars.Expr.sqrt(polars.col(y_var).count())
+        dataframe_stats = (
+            data.lazy()
+            .group_by(x_var)
+            .agg(mean.alias(f"{y_var}.mean"), sem.alias(f"{y_var}.sem"))
+            .sort(by=x_var)
+            .collect()
+        )
+
+        stats = Stats(
+            x_values=dataframe_stats[x_var].to_numpy(),
+            y_values=dataframe_stats[f"{y_var}.mean"].to_numpy(),
+            error_values=dataframe_stats[f"{y_var}.sem"].to_numpy(),
+            x_unit=x_unit,
+            y_unit=y_unit,
+        )
+        return stats
+
+
+class ErrorBarView(PlotWidget, DataView):
     def __init__(self, x: str, y: str, *args, **kwargs):
         PlotWidget.__init__(self, *args, background=(0, 0, 0, 0), **kwargs)
 
         self._x_var = x
         self._y_var = y
-        self._x_unit: Optional[Unit] = None
-        self._y_unit: Optional[Unit] = None
 
-        self._x_values = np.array([])
-        self._y_values = np.array([])
-        self._error_values = np.array([])
+        self._stats = Stats(
+            x_values=np.array([]),
+            y_values=np.array([]),
+            error_values=np.array([]),
+            x_unit=None,
+            y_unit=None,
+        )
 
         self._error_bar_plotter = FilledErrorBarPlotter()
+        self._compute_stats_thread: Optional[ComputeStatsThread] = None
 
         self._setup_ui()
 
     def _setup_ui(self):
         for item in self._error_bar_plotter.get_items():
             self.addItem(item)
+        stats = Stats(
+            x_values=np.array([]),
+            y_values=np.array([]),
+            error_values=np.array([]),
+            x_unit=None,
+            y_unit=None,
+        )
         self.update_plot()
-        self.data_updated.connect(self.update_plot)  # type:ignore
 
     def set_axis_tick_font(self, axis: WhichAxis, font: QtGui.QFont) -> None:
         self.getAxis(axis).setStyle(tickFont=font)
@@ -84,74 +161,31 @@ class ErrorBarView(PlotWidget, DataView):
     def set_axis_pen(self, axis: WhichAxis, pen: pyqtgraph.mkPen) -> None:
         self.plotItem.getAxis(axis).setPen(pen)
 
-    def update_plot(self):
-        self.update_axis_labels()
+    def update_plot(self) -> None:
+        stats = self._stats
+        self.update_axis_labels(stats)
         self._error_bar_plotter.set_data(
-            x=self._x_values,
-            y=self._y_values,
-            error=self._error_values,
+            x=stats.x_values,
+            y=stats.y_values,
+            error=stats.error_values,
         )
 
-    def update_axis_labels(self):
-        self.setLabel("left", format_label(self._y_var, self._y_unit))
-        self.setLabel("bottom", format_label(self._x_var, self._x_unit))
+    def update_axis_labels(self, stats: Stats):
+        self.setLabel("left", format_label(self._y_var, stats.y_unit))
+        self.setLabel("bottom", format_label(self._x_var, stats.x_unit))
 
     def update_data(self, dataframe: Optional[polars.DataFrame]) -> None:
-        if dataframe is not None and len(dataframe) > 0:
-            self.process_data(dataframe)
-        else:
-            self._x_values = np.array([])
-            self._y_values = np.array([])
-            self._error_values = np.array([])
-
-        # We don't want to update the plot directly here because if this is not running in the same thread the plot
-        # items leave in, that would lead to a crash. Instead, we emit the signal and do the plot update in the slot.
-        self.data_updated.emit()  # type: ignore
-
-    def process_data(self, dataframe: polars.DataFrame) -> None:
-        x_var = self._x_var
-        y_var = self._y_var
-
-        x_magnitudes, self._x_unit = convert_to_single_unit(dataframe[x_var])
-        y_magnitudes, self._y_unit = convert_to_single_unit(dataframe[y_var])
-
-        data = polars.DataFrame([x_magnitudes, y_magnitudes])
-
-        mean = polars.col(y_var).mean()
-        sem = polars.col(y_var).std() / polars.Expr.sqrt(polars.col(y_var).count())
-        stats = (
-            data.lazy()
-            .group_by(x_var)
-            .agg(mean.alias(f"{y_var}.mean"), sem.alias(f"{y_var}.sem"))
-            .sort(by=x_var)
-            .collect()
+        if self._compute_stats_thread is not None:
+            if self._compute_stats_thread.isRunning():
+                return
+        self._compute_stats_thread = ComputeStatsThread(
+            self,
+            dataframe,
+            self._x_var,
+            self._y_var,
         )
-
-        self._x_values = stats[x_var].to_numpy()
-        self._y_values = stats[f"{y_var}.mean"].to_numpy()
-        self._error_values = stats[f"{y_var}.sem"].to_numpy()
-
-    def plot_with_hue(self, dataframe: polars.DataFrame) -> None:
-        x_var = self._x_var
-        y_var = self._y_var
-        hue = self.hue
-        mean = polars.col(y_var).mean()
-        sem = polars.col(y_var).std() / polars.Expr.sqrt(polars.col(y_var).count())
-        stats = (
-            dataframe.lazy()
-            .group_by(x_var, hue)
-            .agg(mean.alias(f"{y_var}.mean"), sem.alias(f"{y_var}.sem"))
-            .sort(hue, x_var)
-            .collect()
-        )
-        for hue_value, group in stats.group_by(hue, maintain_order=True):
-            self.plot_curve(
-                group[x_var].to_numpy(),
-                group[f"{y_var}.mean"].to_numpy(),
-                group[f"{y_var}.sem"].to_numpy(),
-                label=hue_value,
-            )
-        self._axis.legend(title=hue)
+        self._compute_stats_thread.finished.connect(self.update_plot)  # type: ignore
+        self._compute_stats_thread.start()
 
 
 def format_label(label: str, unit: Optional[Unit]) -> str:
@@ -182,8 +216,9 @@ class FilledErrorBarPlotter:
         self._top_curve = pyqtgraph.PlotCurveItem(x=np.array([]), y=np.array([]))
         self._bottom_curve = pyqtgraph.PlotCurveItem(x=np.array([]), y=np.array([]))
         self._middle_curve = pyqtgraph.PlotCurveItem(
-            x=np.array([], dtype=float), y=np.array([], dtype=float),
-            pen=pyqtgraph.mkPen("w", width=2)
+            x=np.array([], dtype=float),
+            y=np.array([], dtype=float),
+            pen=pyqtgraph.mkPen("w", width=2),
         )
         self._fill = pyqtgraph.FillBetweenItem(
             self._top_curve, self._bottom_curve, brush=0.2
