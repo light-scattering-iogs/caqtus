e77a6bfd90d4ed511119f833664e161bd38daee4
Damien <damien.bloch@institutoptique.fr>
2023-11-27 11:27:35 +0100

    Export get_standard_experiment_session in core.session

--- core/core.session/core/session/__init__.py
+++ core/core.session/core/session/__init__.py
@@ -2,6 +2,7 @@ from experiment.session import (
     ExperimentSession,
     ExperimentSessionMaker,
     get_standard_experiment_session_maker,
+    get_standard_experiment_session,
 )
 from sequence.runtime import Shot, Sequence
 
@@ -9,6 +10,7 @@ __all__ = [
     "ExperimentSession",
     "ExperimentSessionMaker",
     "get_standard_experiment_session_maker",
+    "get_standard_experiment_session",
     "Sequence",
     "Shot",
 ]
