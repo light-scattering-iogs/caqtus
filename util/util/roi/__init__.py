6b01c749dea9dc5ae41dec5fbfea58bfe6cbe0a9
Damien <damien.bloch@institutoptique.fr>
2023-12-19 11:36:16 +0100

    Add rotated rectangular roi to util.roi

--- util/util/roi/__init__.py
+++ util/util/roi/__init__.py
@@ -2,7 +2,15 @@ from util import serialization
 from .arbitrary_roi import ArbitraryROI
 from .rectangular_roi import RectangularROI
 from .roi import ROI, Width, Height
+from .rotated_rectangular_roi import RotatedRectangularROI
 
 serialization.include_subclasses(ROI)
 
-__all__ = ["ArbitraryROI", "RectangularROI", "ROI", "Width", "Height"]
+__all__ = [
+    "ArbitraryROI",
+    "RectangularROI",
+    "RotatedRectangularROI",
+    "ROI",
+    "Width",
+    "Height",
+]
