603410abe26e097aeeed6bd22e521b516aa0a057
damien <damien.bloch@institutoptique.fr>
2023-12-28 16:22:56 +0100

    Clean docstring for ExperimentSession

--- core/core.session/core/session/experiment_session.py
+++ core/core.session/core/session/experiment_session.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 from contextlib import AbstractContextManager
 from typing import Protocol
@@ -15,23 +17,42 @@ class ExperimentSessionNotActiveError(RuntimeError):
 
 
 class ExperimentSession(
-    AbstractContextManager["ExperimentSession"],
+    AbstractContextManager,
     Protocol,
 ):
-    """Interface that define an experiment session.
+    """Provides a connection to access the permanent storage of the experiment.
 
-    Implementations of this class manage access to the permanent storage of the experiment.
+    An :py:class:`ExperimentSession` object allows to read and write configurations
+    and data of the experiment.
+    Every function and method that read or write data do so through an experiment
+    session object.
 
-    An ExperimentSession is a context manager and must be used in a `with` statement. If an error occurs inside the
-    `with` block, the session will be rolled back to the state it was in before the `with` block was entered in order to
-    prevent leaving the data in an inconsistent state.
+    An experiment session object must be activated before it can be used.
+    This is done by using the `with` statement on the session, inside which the session
+    is active.
+    If an error occurs inside the `with` block of the session, the data will be
+    rolled back to the state it was in before the `with` block was entered in order to
+    prevent leaving the storage in an inconsistent state.
+    Data is only committed to the permanent storage when the `with` block is exited and
+    will only be visible to other sessions after that point.
+    For this reason, it is recommended to keep the `with` block as short as possible.
+
+    A given session is not meant to be used concurrently.
+    It can't be pickled and must not be passed to other processes.
+    It is also not thread safe.
+    It is not meant to be used by several coroutines at the same time, even if they
+    belong to the same thread.
+
+    It is possible to create multiple sessions connecting to the same storage using an
+    :py:class:`core.session.ExperimentSessionMaker`.
 
     Attributes:
-        sequence_hierarchy: Gives access to all sequences stored in the session.
-        shot_collection: Gives access to the data acquired while running shots on the experiment.
         experiment_configs: Gives access to the history of the device configurations.
+        sequence_hierarchy: Gives access to all sequences stored in the session.
+        shot_collection: Gives access to the data acquired while running shots on the
+        experiment.
     """
 
+    experiment_configs: ExperimentConfigCollection
     sequence_hierarchy: SequenceHierarchy
     shot_collection: ShotCollection
-    experiment_configs: ExperimentConfigCollection
