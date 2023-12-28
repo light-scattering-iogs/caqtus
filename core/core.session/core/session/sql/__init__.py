e5106c567631cb329cdee79aaae95241cb224695
damien <damien.bloch@institutoptique.fr>
2023-12-28 18:29:59 +0100

    Add fixture to test experiment session

--- core/core.session/core/session/sql/__init__.py
+++ core/core.session/core/session/sql/__init__.py
@@ -1,4 +1,5 @@
 from ._create_tables import create_tables
 from ._session_maker import SQLExperimentSessionMaker
+from ._experiment_session import SQLExperimentSession
 
-__all__ = ["create_tables", "SQLExperimentSessionMaker"]
+__all__ = ["create_tables", "SQLExperimentSessionMaker", "SQLExperimentSession"]
--- core/core.session/tests/test_sql_session/test_experiment_config_collection.py
+++ core/core.session/tests/test_sql_session/test_experiment_config_collection.py
@@ -1,10 +1,15 @@
+import pytest
 import sqlalchemy
 
-from core.session.sql import SQLExperimentSessionMaker, create_tables
-from core.session import ExperimentConfig
+from core.session import ExperimentConfig, ExperimentSession
+from core.session.sql import (
+    SQLExperimentSessionMaker,
+    create_tables,
+)
 
 
-def test_set_current():
+@pytest.fixture(scope="function")
+def empty_session() -> ExperimentSession:
     url = "sqlite:///:memory:"
     engine = sqlalchemy.create_engine(url)
 
@@ -12,8 +17,12 @@ def test_set_current():
 
     session_maker = SQLExperimentSessionMaker(engine)
 
+    return session_maker()
+
+
+def test_set_current(empty_session: ExperimentSession):
     experiment_config = ExperimentConfig()
 
-    with session_maker() as session:
+    with empty_session as session:
         session.experiment_configs.set_current_config(experiment_config)
         assert session.experiment_configs.get_current_config() == experiment_config
