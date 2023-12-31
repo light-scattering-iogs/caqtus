078625e0c523dfa28485eea2e049c070f93a01ba
damien <damien.bloch@institutoptique.fr>
2023-12-31 10:45:41 +0100

    Move data types into core.types

--- core/core.types/core/types/__init__.py
+++ core/core.types/core/types/__init__.py
@@ -1,4 +1,3 @@
-from data_types import Data, DataLabel, is_data_label, is_data
 from image_types import ImageLabel, Image, is_image
 from parameter_types import (
     Parameter,
@@ -14,10 +13,6 @@ __all__ = [
     "is_parameter",
     "is_analog_value",
     "is_quantity",
-    "DataLabel",
-    "is_data_label",
-    "Data",
-    "is_data",
     "ImageLabel",
     "Image",
     "is_image",
