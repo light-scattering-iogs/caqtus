543307527bd7b1aac32802e7cac5afd383ec1829
damien <damien.bloch@institutoptique.fr>
2024-01-03 16:42:33 +0100

    Add step insertion to step iteration editor

--- ui/condetrol/condetrol/sequence_iteration_editors/steps_iteration_editor/steps_iteration_editor.py
+++ ui/condetrol/condetrol/sequence_iteration_editors/steps_iteration_editor/steps_iteration_editor.py
@@ -1,10 +1,47 @@
+import functools
 from typing import Optional
 
-from PyQt6.QtWidgets import QWidget, QTreeView
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QKeySequence, QShortcut, QAction, QFont
+from PyQt6.QtWidgets import QWidget, QTreeView, QAbstractItemView, QMenu
 
-from core.session.sequence.iteration_configuration import StepsConfiguration
-from ..sequence_iteration_editor import SequenceIterationEditor
+from core.session.sequence.iteration_configuration import (
+    StepsConfiguration,
+    VariableDeclaration,
+    ExecuteShot,
+    LinspaceLoop,
+    ArangeLoop,
+)
+from core.types.expression import Expression
+from core.types.variable_name import DottedVariableName
 from .steps_model import StepsModel
+from ..sequence_iteration_editor import SequenceIterationEditor
+
+
+def create_variable_declaration():
+    return VariableDeclaration(
+        variable=DottedVariableName("new_variable"), value=Expression("...")
+    )
+
+
+def create_linspace_loop():
+    return LinspaceLoop(
+        variable=DottedVariableName("new_variable"),
+        start=Expression("..."),
+        stop=Expression("..."),
+        num=10,
+        sub_steps=[],
+    )
+
+
+def create_arange_loop():
+    return ArangeLoop(
+        variable=DottedVariableName("new_variable"),
+        start=Expression("..."),
+        stop=Expression("..."),
+        step=Expression("..."),
+        sub_steps=[],
+    )
 
 
 class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration]):
@@ -17,6 +54,23 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         self.expandAll()
         self.header().hide()
 
+        self.setDragEnabled(True)
+        self.setAcceptDrops(True)
+        self.setDropIndicatorShown(True)
+        self.setDragDropMode(QAbstractItemView.DragDropMode.DragDrop)
+        self.setDefaultDropAction(Qt.DropAction.MoveAction)
+        # self.setDragDropOverwriteMode(False)
+
+        self.delete_shortcut = QShortcut(QKeySequence("Delete"), self)
+        self.delete_shortcut.activated.connect(self.delete_selected)
+
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
+
+        font = QFont()
+        font.setPixelSize(15)
+        self.setFont(font)
+
     def get_iteration(self) -> StepsConfiguration:
         return self.iteration
 
@@ -25,3 +79,44 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
 
     def set_read_only(self, read_only: bool):
         self.read_only = read_only
+
+    def delete_selected(self):
+        selected = self.selectedIndexes()
+        if selected:
+            self._model.removeRow(selected[0].row(), selected[0].parent())
+
+    def show_context_menu(self, position):
+        index = self.indexAt(position)
+        if not index.isValid():
+            return
+        menu = QMenu(self)
+
+        add_menu = QMenu()
+        add_menu.setTitle("Insert above...")
+        menu.addMenu(add_menu)
+
+        create_variable_action = QAction("variable")
+        add_menu.addAction(create_variable_action)
+        new_variable = create_variable_declaration()
+        create_variable_action.triggered.connect(
+            functools.partial(self._model.insert_above, new_variable, index)
+        )
+        create_shot_action = QAction("Shot")
+        add_menu.addAction(create_shot_action)
+        new_shot = ExecuteShot()
+        create_shot_action.triggered.connect(
+            functools.partial(self._model.insert_above, new_shot, index)
+        )
+        create_linspace_action = QAction("Linspace loop")
+        add_menu.addAction(create_linspace_action)
+        new_linspace = create_linspace_loop()
+        create_linspace_action.triggered.connect(
+            functools.partial(self._model.insert_above, new_linspace, index)
+        )
+        create_arange_action = QAction("Arange loop")
+        add_menu.addAction(create_arange_action)
+        new_arange = create_arange_loop()
+        create_arange_action.triggered.connect(
+            functools.partial(self._model.insert_above, new_arange, index)
+        )
+        menu.exec(self.mapToGlobal(position))
