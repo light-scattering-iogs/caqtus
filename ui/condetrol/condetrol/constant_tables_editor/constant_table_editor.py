dede8916d50b59b4220229c2bcec854a4b2d1edf
damien <damien.bloch@institutoptique.fr>
2024-01-04 12:54:25 +0100

    Add possibility to add variable in constant table editor

--- ui/condetrol/condetrol/constant_tables_editor/constant_table_editor.py
+++ ui/condetrol/condetrol/constant_tables_editor/constant_table_editor.py
@@ -1,15 +1,39 @@
-from PyQt6.QtWidgets import QListView
+from PyQt6.QtCore import Qt
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QListView, QMenu
 
 from core.session import ConstantTable
+from .constant_table_model import ConstantTableModel
 
 
 class ConstantTableEditor(QListView):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.table: ConstantTable = []
+        self.model = ConstantTableModel(self)
+        self.setModel(self.model)
+
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
 
     def set_table(self, table: ConstantTable):
-        self.table = table
+        self.model.set_table(table)
 
     def get_table(self) -> ConstantTable:
-        return self.table
+        return self.model.get_table()
+
+    def show_context_menu(self, position):
+        index = self.indexAt(position)
+        menu = QMenu(self)
+        if not index.isValid():
+            add_action = QAction("Add")
+            add_action.triggered.connect(
+                lambda: self.model.insertRow(self.model.rowCount())
+            )
+            menu.addAction(add_action)
+        else:
+            add_above_action = QAction("Insert above")
+            add_above_action.triggered.connect(
+                lambda: self.model.insertRow(index.row())
+            )
+            menu.addAction(add_above_action)
+        menu.exec(self.mapToGlobal(position))
---
+++ ui/condetrol/condetrol/constant_tables_editor/constant_table_model.py
@@ -0,0 +1,49 @@
+import copy
+from typing import Optional
+
+from PyQt6.QtCore import QAbstractListModel, QObject, Qt, QModelIndex
+
+from core.session import ConstantTable
+from core.session.sequence.iteration_configuration import VariableDeclaration
+from core.types.expression import Expression
+from core.types.variable_name import DottedVariableName
+
+
+class ConstantTableModel(QAbstractListModel):
+    def __init__(self, parent=Optional[QObject]):
+        super().__init__(parent)
+        self.table = []
+
+    def rowCount(self, parent=None, *args, **kwargs):
+        return len(self.table)
+
+    def data(self, index, role=Qt.ItemDataRole.DisplayRole):
+        if role == Qt.ItemDataRole.DisplayRole:
+            return str(self.table[index.row()])
+        elif role == Qt.ItemDataRole.EditRole:
+            return self.table[index.row()]
+
+    def flags(self, index: QModelIndex) -> Qt.ItemFlag:
+        flags = (
+            Qt.ItemFlag.ItemIsSelectable
+            | Qt.ItemFlag.ItemIsDragEnabled
+            | Qt.ItemFlag.ItemIsDropEnabled
+            | Qt.ItemFlag.ItemIsEnabled
+        )
+        return flags
+
+    def set_table(self, table: ConstantTable):
+        self.beginResetModel()
+        self.table = copy.deepcopy(table)
+        self.endResetModel()
+
+    def get_table(self) -> ConstantTable:
+        return copy.deepcopy(self.table)
+
+    def insertRow(self, row, parent=QModelIndex()):
+        self.beginInsertRows(parent, row, row)
+        declaration = VariableDeclaration(
+            variable=DottedVariableName("new_variable"), value=Expression("...")
+        )
+        self.table.insert(row, declaration)
+        self.endInsertRows()
