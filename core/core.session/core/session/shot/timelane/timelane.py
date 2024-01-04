dcafa349591a6ff21e1b26639cdcb34b1f2a3cae
damien <damien.bloch@institutoptique.fr>
2024-01-04 17:46:05 +0100

    Add basic time lane with no merged cells

---
+++ core/core.session/core/session/shot/timelane/timelane.py
@@ -0,0 +1,45 @@
+import abc
+from collections.abc import MutableSequence
+from typing import TypeVar
+
+import attrs
+
+T = TypeVar("T")
+
+
+@attrs.define
+class TimeLane(MutableSequence[T]):
+    values: list[T] = attrs.field(factory=list)
+
+    def __len__(self):
+        return len(self.values)
+
+    def __getitem__(self, item):
+        if isinstance(item, int):
+            return self.get_index(item)
+        else:
+            raise TypeError(f"Invalid type for index: {type(item)}")
+
+    def get_index(self, index: int) -> T:
+        return self.values[index]
+
+    def __setitem__(self, key, value):
+        if isinstance(key, int):
+            self.set_index(key, value)
+        else:
+            raise TypeError(f"Invalid type for index: {type(key)}")
+
+    def set_index(self, index: int, value: T):
+        self.values[index] = value
+
+    def __delitem__(self, key):
+        if isinstance(key, int):
+            self.delete_index(key)
+        else:
+            raise TypeError(f"Invalid type for index: {type(key)}")
+
+    def delete_index(self, index: int):
+        del self.values[index]
+
+    def insert(self, index: int, value: T):
+        self.values.insert(index, value)
