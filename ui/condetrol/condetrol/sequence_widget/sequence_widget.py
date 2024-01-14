ba51d987bd5bbb64a089611d2f2f6a395e8be61d
damien <damien.bloch@institutoptique.fr>
2024-01-14 17:58:01 +0100

    Detect external change to time lanes in sequence widget

--- ui/condetrol/condetrol/sequence_widget/sequence_widget.py
+++ ui/condetrol/condetrol/sequence_widget/sequence_widget.py
@@ -14,6 +14,7 @@ from core.session.sequence_collection import (
     SequenceStats,
     SequenceNotEditableError,
 )
+from core.session.shot import TimeLanes
 from waiting_widget import run_with_wip_widget
 from .sequence_widget_ui import Ui_SequenceWidget
 from ..sequence_iteration_editors import create_default_editor
@@ -48,6 +49,7 @@ class SequenceWidget(QWidget, Ui_SequenceWidget):
         self.tabWidget.addTab(self.iteration_editor, "Iteration")
         self.time_lanes_editor = TimeLanesEditor(self)
         self.time_lanes_editor.set_time_lanes(time_lanes)
+        self.time_lanes = time_lanes
         self.time_lanes_editor.time_lanes_changed.connect(self.on_time_lanes_changed)
         self.tabWidget.addTab(self.time_lanes_editor, "Shot")
 
@@ -63,6 +65,9 @@ class SequenceWidget(QWidget, Ui_SequenceWidget):
         self.clear_button.clicked.connect(self.clear_sequence)
         self.state_watcher_thread.sequence_not_found.connect(self.deleteLater)
         self.state_watcher_thread.stats_changed.connect(self.apply_stats)
+        self.state_watcher_thread.time_lanes_changed.connect(
+            self.time_lanes_editor.set_time_lanes
+        )
 
     def clear_sequence(self):
         def clear():
@@ -96,6 +101,8 @@ class SequenceWidget(QWidget, Ui_SequenceWidget):
                     self.sequence_path
                 )
                 self.time_lanes_editor.set_time_lanes(time_lanes)
+            finally:
+                self.time_lanes = time_lanes
 
     def apply_stats(self, stats: SequenceStats):
         self.apply_state(stats.state)
@@ -123,6 +130,7 @@ class SequenceWidget(QWidget, Ui_SequenceWidget):
     class StateWatcherThread(QThread):
         stats_changed = pyqtSignal(SequenceStats)
         sequence_not_found = pyqtSignal()
+        time_lanes_changed = pyqtSignal(TimeLanes)
 
         def __init__(self, sequence_widget: SequenceWidget):
             super().__init__(sequence_widget)
@@ -146,6 +154,11 @@ class SequenceWidget(QWidget, Ui_SequenceWidget):
                         if stats != self.stats:
                             self.stats = stats
                             self.stats_changed.emit(stats)
+                        time_lanes = session.sequence_collection.get_time_lanes(
+                            self.sequence_widget.sequence_path
+                        )
+                        if time_lanes != self.sequence_widget.time_lanes:
+                            self.time_lanes_changed.emit(time_lanes)
                     except (PathNotFoundError, PathIsNotSequenceError):
                         self.sequence_not_found.emit()
                         self.quit()
