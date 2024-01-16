23dc5b5262a50bf2cee787af49104452849224c2
damien <damien.bloch@institutoptique.fr>
2024-01-16 21:19:49 +0100

    Add tables for data in sql experiment session

--- core/core.session/core/session/sql/_shot_tables.py
+++ core/core.session/core/session/sql/_shot_tables.py
@@ -1,7 +1,7 @@
 import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import UniqueConstraint, ForeignKey, JSON
+from sqlalchemy import UniqueConstraint, ForeignKey, JSON, LargeBinary
 from sqlalchemy.orm import mapped_column, Mapped, relationship
 
 from ._table_base import Base
@@ -30,8 +30,22 @@ class SQLShot(Base):
         cascade="all, delete",
         passive_deletes=True,
     )
+    array_data: Mapped[list["SQLShotArray"]] = relationship(
+        back_populates="shot",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
+    structured_data: Mapped[list["SQLStructuredShotData"]] = relationship(
+        back_populates="shot",
+        cascade="all, delete",
+        passive_deletes=True,
+    )
 
 
+# Shot parameters are stored as a single JSON row per shot, which is a dictionary
+# mapping parameter names to values.
+# Since we usually load all parameters at once, there is not much need to have a row for
+# each single parameter.
 class SQLShotParameter(Base):
     __tablename__ = "shot.parameters"
 
@@ -43,3 +57,41 @@ class SQLShotParameter(Base):
     )
     shot: Mapped[SQLShot] = relationship(back_populates="parameters")
     content = mapped_column(JSON)
+
+
+# Unlike parameters, data are stored as many rows per shot. This is because we might not
+# need to load all data for each shot every time.
+# Data are separated in two sub-tables: one for structured nested data that contains
+# few elements, and one for large arrays of bytes.
+class SQLStructuredShotData(Base):
+    __tablename__ = "shot.data.structured"
+
+    __table_args__ = (
+        UniqueConstraint("shot_id", "label", name="structured_identifier"),
+    )
+
+    id_: Mapped[int] = mapped_column(name="id", primary_key=True)
+    shot_id: Mapped[int] = mapped_column(
+        ForeignKey("shots.id", ondelete="CASCADE"),
+        index=True,
+    )
+    shot: Mapped[SQLShot] = relationship(back_populates="structured_data")
+    label: Mapped[str] = mapped_column()
+    content = mapped_column(JSON)
+
+
+class SQLShotArray(Base):
+    __tablename__ = "shot.data.arrays"
+
+    __table_args__ = (UniqueConstraint("shot_id", "label", name="array_identifier"),)
+
+    id_: Mapped[int] = mapped_column(name="id", primary_key=True)
+    shot_id: Mapped[int] = mapped_column(
+        ForeignKey("shots.id", ondelete="CASCADE"),
+        index=True,
+    )
+    shot: Mapped[SQLShot] = relationship(back_populates="arrays")
+    label: Mapped[str] = mapped_column()
+    dtype: Mapped[str] = mapped_column()
+    shape = mapped_column(JSON)
+    bytes_ = mapped_column(LargeBinary, name="bytes")  # stored in C-order
