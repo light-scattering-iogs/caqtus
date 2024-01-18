85a49e8f264ffd2e0db15fee95680f03dce3b8a0
Damien Bloch <damien.bloch@institutoptique>
2024-01-18 20:30:58 +0100

    Export Device and RuntimeDevice in core.device

--- core/core.device/core/device/__init__.py
+++ core/core.device/core/device/__init__.py
@@ -1,5 +1,6 @@
 from .name import DeviceName
 from .configuration import DeviceConfigurationAttrs
 from .parameter import DeviceParameter
+from .runtime import Device, RuntimeDevice
 
-__all__ = ["DeviceName", "DeviceConfigurationAttrs", "DeviceParameter"]
+__all__ = ["DeviceName", "DeviceConfigurationAttrs", "DeviceParameter", "Device", "RuntimeDevice"]
