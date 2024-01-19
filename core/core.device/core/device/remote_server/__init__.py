5e6f274f056970403e9ee96d51bab3d0aec97b8f
Damien Bloch <damien.bloch@institutoptique>
2024-01-19 10:27:02 +0100

    Add RemoteDeviceServer to core.device

--- core/core.device/core/device/remote_server/__init__.py
+++ core/core.device/core/device/remote_server/__init__.py
@@ -1,3 +1,4 @@
 from ._device_server_configuration import DeviceServerConfiguration
+from ._device_server import RemoteDeviceServer
 
-__all__ = ["DeviceServerConfiguration"]
+__all__ = ["DeviceServerConfiguration", "RemoteDeviceServer"]
