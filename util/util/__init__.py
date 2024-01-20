763ea8411073bca43e1737a206923b12db85394f
Damien Bloch <damien.bloch@institutoptique>
2024-01-20 12:31:02 +0100

    Add context manager to add exception note

--- util/util/__init__.py
+++ util/util/__init__.py
@@ -1,5 +1,6 @@
 import attrs as attrs
 
+from ._add_exc_note import add_exc_note
 from ._log_exception import log_exception
 from ._run_on_change_only import run_on_change_method
 from .duration_timer import DurationTimer, DurationTimerLog
@@ -10,4 +11,5 @@ __all__ = [
     "DurationTimer",
     "DurationTimerLog",
     "attrs",
+    "add_exc_note",
 ]
