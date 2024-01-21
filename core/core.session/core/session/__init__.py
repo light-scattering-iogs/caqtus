f45d31221eb67f9928fc948c0d6cabde140f0c73
Damien Bloch <damien.bloch@institutoptique>
2024-01-21 21:26:33 +0100

    Export Sequence and Shot in core.session

--- core/core.session/core/session/__init__.py
+++ core/core.session/core/session/__init__.py
@@ -3,6 +3,7 @@ from .data_type import DataType
 from .experiment_session import ExperimentSession
 from .path import BoundSequencePath, PureSequencePath
 from .path_hierarchy import PathHierarchy
+from .sequence import Sequence, Shot
 from .session_maker import (
     ExperimentSessionMaker,
     get_standard_experiment_session_maker,
@@ -19,4 +20,6 @@ __all__ = [
     "DataType",
     "PathHierarchy",
     "ConstantTable",
+    "Sequence",
+    "Shot",
 ]
