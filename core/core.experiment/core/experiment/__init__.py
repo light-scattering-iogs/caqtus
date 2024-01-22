b5938703a05ef67ba5f9285d5815b162c9b26f45
Damien Bloch <damien.bloch@institutoptique>
2024-01-22 22:02:34 +0100

    Export ExperimentManager and Procedure in core.experiment

--- core/core.experiment/core/experiment/__init__.py
+++ core/core.experiment/core/experiment/__init__.py
@@ -1,4 +1,11 @@
 from .sequence_runner import SequenceInterruptedException
 from .shot_runner import ShotRunner, ShotRunnerFactory
+from .manager import ExperimentManager, Procedure
 
-__all__ = ["ShotRunner", "ShotRunnerFactory", "SequenceInterruptedException"]
+__all__ = [
+    "ShotRunner",
+    "ShotRunnerFactory",
+    "SequenceInterruptedException",
+    "ExperimentManager",
+    "Procedure",
+]
