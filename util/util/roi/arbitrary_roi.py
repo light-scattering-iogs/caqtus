59b36e96727b4829c7bc806445b0f27b54a28a6a
Damien Bloch <damien.bloch@institutoptique>
2024-01-28 12:49:58 +0100

    Fix serialization for arbitrary roi

--- util/util/roi/arbitrary_roi.py
+++ util/util/roi/arbitrary_roi.py
@@ -4,21 +4,24 @@ from typing import Self
 import attrs
 import numpy as np
 
-from .roi import ROI
+from .roi import ROI, Width, Height
 
 
-@attrs.define(slots=False)
+@attrs.define
 class ArbitraryROI(ROI):
     """Arbitrary region of interest inside an image.
 
-    This ROI is defined by specifying the indices of the pixels in the original image that are part of the region of
-    interest.
+    This ROI is defined by specifying the indices of the pixels in the original image
+    that are part of the region of interest.
 
-    While this ROI is the most generic one, it becomes very inefficient when the number of pixels in the region of
-    interest is large, because it stores the coordinates of all pixels. Whenever possible, use a more specific ROI.
+    While this ROI is the most generic one, it becomes very inefficient when the number
+    of pixels in the region of interest is large, because it stores the coordinates of
+    all pixels.
+    Whenever possible, use a more specific ROI.
 
     Attributes:
-        indices: The indices of the pixels in the original image that are part of the region of interest.
+        indices: The indices of the pixels in the original image that are part of the
+        region of interest.
     """
 
     indices: tuple[tuple[int, int], ...] = attrs.field(
@@ -27,27 +30,28 @@ class ArbitraryROI(ROI):
     )
 
     def get_indices(self) -> tuple[Iterable[int], Iterable[int]]:
-        return np.array(self.indices).T
+        indices = np.array(self.indices).T
+        return indices[0], indices[1]
 
     @classmethod
     def from_mask(cls, mask: np.ndarray) -> Self:
         """Create a region of interest from a mask
 
         Args:
-            mask: A boolean array with the same shape as the original image. True values indicate that the pixel is part
-            of the region of interest.
+            mask: A boolean array with the same shape as the original image.
+            True values indicate that the pixel is part of the region of interest.
         """
 
         shape = mask.shape
         if len(shape) != 2:
             raise ValueError("mask must be 2D")
-        shape = shape[0], shape[1]
+        shape = Width(shape[0]), Height(shape[1])
         indices = np.argwhere(mask).tolist()
         return cls(
             original_image_size=shape, indices=tuple(tuple(index) for index in indices)
         )
 
-    @indices.validator
+    @indices.validator  # type: ignore
     def validate_indices(self, _, indices):
         for index in indices:
             if not isinstance(index, tuple):
