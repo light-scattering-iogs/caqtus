d5e1f945e185b9c092b80049cd658e5935fda976
Damien Bloch <damien.bloch@institutoptique>
2024-01-29 17:35:48 +0100

    Fix bug when checking is data

--- core/core.types/core/types/data/is_data.py
+++ core/core.types/core/types/data/is_data.py
@@ -10,7 +10,7 @@ def is_data(data: Any) -> TypeGuard[Data]:
 
     if isinstance(data, (bool, int, str, float, np.ndarray)):
         return True
-    elif isinstance(data, list):
+    elif isinstance(data, (list, tuple)):
         return all(is_data(x) for x in data)
     elif isinstance(data, dict):
         return all(isinstance(k, str) and is_data(v) for k, v in data.items())
