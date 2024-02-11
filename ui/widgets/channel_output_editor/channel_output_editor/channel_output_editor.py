6884eb7ba987bf707b717b7f8c6f38fe4addce00
damien <damien.bloch@institutoptique.fr>
2024-02-11 14:15:08 +0100

    Add connection link when user drags a line between two connection points

--- ui/widgets/channel_output_editor/channel_output_editor/channel_output_editor.py
+++ ui/widgets/channel_output_editor/channel_output_editor/channel_output_editor.py
@@ -13,7 +13,12 @@ from PySide6.QtWidgets import (
 )
 
 from .calibrated_analog_mapping_item import AnalogMappingBlock
-from .item_container import ChannelOutputBlock, TimeLaneBlock, ConnectionPoint
+from .item_container import (
+    ChannelOutputBlock,
+    TimeLaneBlock,
+    ConnectionPoint,
+    ConnectionLink,
+)
 
 
 class ChannelOutputEditor(QGraphicsView):
@@ -28,8 +33,34 @@ class ChannelOutputScene(QGraphicsScene):
         self.channel_output = ChannelOutputBlock(channel_label)
         self.addItem(self.channel_output)
 
-        # A line that is drawn when the user is linking two connections
-        self.line: Optional[QGraphicsLineItem] = None
+        # This object is used to store the line and the initial connection point when
+        # the user starts dragging a line to connect two blocks.
+        self._line_and_initial_connection: Optional[
+            tuple[QGraphicsLineItem, ConnectionPoint]
+        ] = None
+
+    def _is_user_dragging_line(self) -> bool:
+        return self._line_and_initial_connection is not None
+
+    def _clear_user_dragging_line(self) -> None:
+        assert self._line_and_initial_connection is not None
+        line_item = self._line_and_initial_connection[0]
+        self.removeItem(line_item)
+        self._line_and_initial_connection = None
+
+    def get_line_item(self) -> Optional[QGraphicsLineItem]:
+        return (
+            self._line_and_initial_connection[0]
+            if self._line_and_initial_connection
+            else None
+        )
+
+    def get_initial_connection_point(self) -> Optional[ConnectionPoint]:
+        return (
+            self._line_and_initial_connection[1]
+            if self._line_and_initial_connection
+            else None
+        )
 
     def mousePressEvent(self, event: QGraphicsSceneMouseEvent) -> None:
         if event.button() == Qt.MouseButton.RightButton:
@@ -49,27 +80,47 @@ class ChannelOutputScene(QGraphicsScene):
             if len(items_at_click) != 0:
                 highest_item = items_at_click[0]
                 if isinstance(highest_item, ConnectionPoint):
-                    self.line = QGraphicsLineItem(
-                        QLineF(
-                            highest_item.link_position(), highest_item.link_position()
-                        )
+                    line = QLineF(
+                        highest_item.link_position(), highest_item.link_position()
                     )
-                    self.line.setPen(QPen(Qt.GlobalColor.white, 1))
-                    self.addItem(self.line)
+                    line_item = QGraphicsLineItem(line)
+                    line_item.setPen(QPen(Qt.GlobalColor.white, 1))
+                    self._line_and_initial_connection = (line_item, highest_item)
+                    self.addItem(line_item)
                     return
         super().mousePressEvent(event)
 
     def mouseMoveEvent(self, event: QGraphicsSceneMouseEvent) -> None:
-        if self.line is not None:
-            new_line = QLineF(self.line.line().p1(), event.scenePos())
-            self.line.setLine(new_line)
+        if self._is_user_dragging_line():
+            line_item = self.get_line_item()
+            assert line_item is not None
+            new_line = QLineF(line_item.line().p1(), event.scenePos())
+            line_item.setLine(new_line)
         else:
             super().mouseMoveEvent(event)
 
     def mouseReleaseEvent(self, event: QGraphicsSceneMouseEvent) -> None:
-        if self.line is not None:
-            self.removeItem(self.line)
-            self.line = None
+        if self._is_user_dragging_line():
+            line_item = self.get_line_item()
+            assert line_item is not None
+            items_at_release = self.items(line_item.line().p2())
+            if len(items_at_release):
+                # The dragged line is often the first item in the list of items at the
+                # release point, so we remove it.
+                if items_at_release[0] is self.get_line_item():
+                    items_at_release = items_at_release[1:]
+            initial_connection = self.get_initial_connection_point()
+            assert initial_connection is not None
+            self._clear_user_dragging_line()
+
+            if len(items_at_release):
+                highest_item = items_at_release[0]
+                if isinstance(highest_item, ConnectionPoint):
+                    link = ConnectionLink(initial_connection, highest_item)
+                    self.addItem(link)
+                    initial_connection.link = link
+                    highest_item.link = link
+                    return
         else:
             super().mouseReleaseEvent(event)
 
