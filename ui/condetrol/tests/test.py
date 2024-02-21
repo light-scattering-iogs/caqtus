ef44b13f278b1b9c9cfc120f3aa480c1bfd14330
Damien Bloch <bloc.dam@gmail.com>
2024-02-21 20:22:59 +0100

    Test hiding preview widget in QColumnView

--- ui/condetrol/tests/test.py
+++ ui/condetrol/tests/test.py
@@ -1,6 +1,13 @@
 import sys
 
-from PySide6.QtWidgets import QApplication, QColumnView, QLabel, QListView
+from PySide6.QtWidgets import (
+    QApplication,
+    QColumnView,
+    QLabel,
+    QListView,
+    QWidget,
+    QSizePolicy,
+)
 
 from condetrol.parameter_tables_editor import ParameterNamespaceModel
 from core.types.expression import Expression
@@ -8,14 +15,26 @@ from core.types.variable_name import DottedVariableName
 
 
 class View(QColumnView):
-    def createColumn(self, rootIndex):
-        view = super().createColumn(rootIndex)
-        view.setHidden(True)
-        return view
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # stupid fix to make the preview widget not show
+        # see: https://bugreports.qt.io/browse/QTBUG-1826
+
+        self.w = QWidget()
+        self.w.setMaximumSize(0, 0)
+        self.w.setSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        self.setPreviewWidget(self.w)
+        self.updatePreviewWidget.connect(self.on_update_preview_widget)
+
+    def on_update_preview_widget(self, index):
+        self.w.parentWidget().parentWidget().setMinimumWidth(0)
+        self.w.parentWidget().parentWidget().setMaximumWidth(0)
 
 
 app = QApplication(sys.argv)
 view = View()
+
 model = ParameterNamespaceModel(view)
 # view.setColumnWidths([0])
 view.setModel(model)
