4a17eb1f59c4c76af63076c52f4e490ddc262565
Damien Bloch <damien.bloch@institutoptique>
2024-03-03 10:32:22 +0100

    Refactor sequence progress delegate to instantiate single progress bar option

--- ui/widgets/sequence_hierarchy/sequence_hierarchy/delegate.py
+++ ui/widgets/sequence_hierarchy/sequence_hierarchy/delegate.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from PySide6.QtCore import QModelIndex, Qt
+from PySide6.QtCore import QModelIndex, Qt, QObject
 from PySide6.QtGui import (
     QColor,
     QPalette,
@@ -19,59 +19,71 @@ from core.session.sequence_collection import SequenceStats
 
 
 class ProgressDelegate(QStyledItemDelegate):
+    def __init__(self, parent: Optional[QObject] = None):
+        super().__init__(parent)
+        self._progress_bar_option = QStyleOptionProgressBar()
+        self._progress_bar_option.textVisible = True
+        self._progress_bar_option.minimum = 0
+
     def paint(
         self, painter: QPainter, option: QStyleOptionViewItem, index: QModelIndex
     ) -> None:
-        model = index.model()
-        sequence_stats: Optional[SequenceStats] = model.data(index, Qt.ItemDataRole.DisplayRole)
+        sequence_stats = index.data(Qt.ItemDataRole.DisplayRole)
+        assert sequence_stats is None or isinstance(sequence_stats, SequenceStats)
         if sequence_stats:
-            opt = QStyleOptionProgressBar()
-            opt.rect = option.rect
-            opt.minimum = 0
-            opt.maximum = 100
-            opt.textVisible = True
+            self._progress_bar_option.rect = option.rect
+            self._progress_bar_option.palette = option.palette
+            self._progress_bar_option.maximum = 100
             state = sequence_stats.state
             if state == State.DRAFT:
-                opt.progress = 0
-                opt.text = "draft"
+                self._progress_bar_option.progress = 0
+                self._progress_bar_option.text = "draft"
             elif state == State.PREPARING:
-                opt.progress = 0
-                opt.text = "preparing"
+                self._progress_bar_option.progress = 0
+                self._progress_bar_option.text = "preparing"
             else:
                 total = sequence_stats.expected_number_shots
-                if total:
-                    opt.progress = sequence_stats.number_completed_shots
-                    opt.maximum = total
+                if total is not None:
+                    self._progress_bar_option.progress = (
+                        sequence_stats.number_completed_shots
+                    )
+                    self._progress_bar_option.maximum = total
                 else:
                     if state == State.RUNNING:  # filled bar with sliding reflects
-                        opt.progress = 0
-                        opt.maximum = 0
+                        self._progress_bar_option.progress = 0
+                        self._progress_bar_option.maximum = 0
                     else:  # filled bar
-                        opt.progress = 1
-                        opt.maximum = 1
+                        self._progress_bar_option.progress = 1
+                        self._progress_bar_option.maximum = 1
 
                 if state == State.RUNNING:
-                    opt.text = "running"
+                    self._progress_bar_option.text = "running"
                 elif state == State.INTERRUPTED:
-                    opt.text = "interrupted"
-                    opt.palette.setColor(
+                    self._progress_bar_option.text = "interrupted"
+                    self._progress_bar_option.palette.setColor(
                         QPalette.ColorRole.Highlight, QColor(166, 138, 13)
                     )
-                    opt.palette.setColor(QPalette.ColorRole.Text, QColor(92, 79, 23))
+                    self._progress_bar_option.palette.setColor(
+                        QPalette.ColorRole.Text, QColor(92, 79, 23)
+                    )
                 elif state == State.FINISHED:
-                    opt.text = f"finished"
-                    opt.palette.setColor(
+                    self._progress_bar_option.text = f"finished"
+                    self._progress_bar_option.palette.setColor(
                         QPalette.ColorRole.Highlight, QColor(98, 151, 85)
                     )
-                    opt.palette.setColor(QPalette.ColorRole.Text, QColor(255, 255, 255))
+                    self._progress_bar_option.palette.setColor(
+                        QPalette.ColorRole.Text, QColor(255, 255, 255)
+                    )
                 elif state == State.CRASHED:
-                    opt.text = "crashed"
-                    opt.palette.setColor(QPalette.ColorRole.Text, QColor(119, 46, 44))
-                    opt.palette.setColor(
+                    self._progress_bar_option.text = "crashed"
+                    self._progress_bar_option.palette.setColor(
+                        QPalette.ColorRole.Text, QColor(119, 46, 44)
+                    )
+                    self._progress_bar_option.palette.setColor(
                         QPalette.ColorRole.Highlight, QColor(240, 82, 79)
                     )
             QApplication.style().drawControl(
-                QStyle.ControlElement.CE_ProgressBar, opt, painter
+                QStyle.ControlElement.CE_ProgressBar, self._progress_bar_option, painter
             )
         else:
             super().paint(painter, option, index)
