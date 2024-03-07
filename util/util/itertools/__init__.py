804e4982630e16405ee9d7848e74ed2583283e24
Damien <damien.bloch@institutoptique.fr>
2024-03-07 21:47:50 +0100

    Add iterate_in_order function to util.itertools

--- util/util/itertools/__init__.py
+++ util/util/itertools/__init__.py
@@ -1,5 +1,6 @@
 from ._batched import batched
 from ._first import first
+from ._iterate_in_order import iterate_in_order
 from ._pairwise import pairwise
 
-__all__ = ["batched", "first", "pairwise"]
+__all__ = ["batched", "first", "pairwise", "iterate_in_order"]
