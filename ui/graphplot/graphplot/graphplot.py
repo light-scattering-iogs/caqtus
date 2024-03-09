b9ed791b0448733752ed362a2b08fd395c2ae53d
damien <damien.bloch@institutoptique.fr>
2024-03-09 15:06:23 +0100

    Add graphplot main window

--- ui/graphplot/graphplot/graphplot.py
+++ ui/graphplot/graphplot/graphplot.py
@@ -1,8 +1,11 @@
 import asyncio
 import logging
+from typing import Self
 
 import PySide6.QtAsyncio as QtAsyncio
-from PySide6.QtWidgets import QApplication, QMainWindow
+from PySide6.QtWidgets import QApplication, QMainWindow, QSplitter, QLabel
+
+from core.session import ExperimentSessionMaker
 
 logger = logging.getLogger(__name__)
 
@@ -18,10 +21,28 @@ async def wrap(coro):
 
 
 class GraphPlot:
-    def __init__(self, *args):
+    def __init__(self, session_maker: ExperimentSessionMaker, *args) -> None:
         self.app = QApplication(*args)
-        self.main = QMainWindow()
+        self.app.setApplicationName("GraphPlot")
+        self.main_window = GraphPlotMainWindow()
+        self.session_maker = session_maker
 
-    def run(self):
-        self.main.show()
-        QtAsyncio.run()
+    def run(self) -> None:
+        with self.main_window:
+            self.main_window.show()
+            QtAsyncio.run()
+
+
+class GraphPlotMainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.splitter = QSplitter(self)
+        self.setCentralWidget(self.splitter)
+        self.splitter.addWidget(QLabel("Hello"))
+        self.splitter.addWidget(QLabel("World"))
+
+    def __enter__(self) -> Self:
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
+        return False
