6ff3c50326d0a2f072d3de2b436123f02da954d0
Damien <damien.bloch@institutoptique.fr>
2024-03-09 15:27:38 +0100

    Add path view and icon to graphplot

--- ui/graphplot/graphplot/graphplot.py
+++ ui/graphplot/graphplot/graphplot.py
@@ -3,9 +3,11 @@ import logging
 from typing import Self
 
 import PySide6.QtAsyncio as QtAsyncio
+import qtawesome
 from PySide6.QtWidgets import QApplication, QMainWindow, QSplitter, QLabel
 
 from core.session import ExperimentSessionMaker
+from sequence_hierarchy import PathHierarchyView
 
 logger = logging.getLogger(__name__)
 
@@ -22,10 +24,16 @@ async def wrap(coro):
 
 class GraphPlot:
     def __init__(self, session_maker: ExperimentSessionMaker, *args) -> None:
+        """
+        Args:
+            session_maker: A callable used to create sessions from which the application can retrieve data.
+        """
+
         self.app = QApplication(*args)
         self.app.setApplicationName("GraphPlot")
-        self.main_window = GraphPlotMainWindow()
-        self.session_maker = session_maker
+        self.app.setStyle("Fusion")
+        self.app.setWindowIcon(qtawesome.icon("mdi6.chart-line", size=64))
+        self.main_window = GraphPlotMainWindow(session_maker)
 
     def run(self) -> None:
         with self.main_window:
@@ -34,15 +42,25 @@ class GraphPlot:
 
 
 class GraphPlotMainWindow(QMainWindow):
-    def __init__(self, *args, **kwargs) -> None:
+    """The main window for the GraphPlot application.
+
+    On the left, it displays a tree view of the experiment session's sequences.
+    On the right, there is a widget to define how to import data from the sequences.
+    In the middle, there is a view of the data loaded from the sequences.
+    """
+
+    def __init__(self, session_maker: ExperimentSessionMaker, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.splitter = QSplitter(self)
         self.setCentralWidget(self.splitter)
-        self.splitter.addWidget(QLabel("Hello"))
+        self.session_maker = session_maker
+        self.path_view = PathHierarchyView(self.session_maker, self)
+        self.splitter.addWidget(self.path_view)
         self.splitter.addWidget(QLabel("World"))
 
     def __enter__(self) -> Self:
+        self.path_view.__enter__()
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb) -> bool:
-        return False
+        return self.path_view.__exit__(exc_type, exc_val, exc_tb)
