64852e57edf9a943b9b827e7941e6b108013043c
Damien Bloch <damien.bloch@institutoptique>
2024-03-17 22:41:13 +0100

    Remove picture_names and exposures from camera initialization arguments

--- core/core.compilation/core/compilation/initialization/_compute_camera_initialization_parameters.py
+++ core/core.compilation/core/compilation/initialization/_compute_camera_initialization_parameters.py
@@ -1,10 +1,9 @@
 from collections.abc import Mapping
-from typing import assert_never, Any
+from typing import Any
 
 from core.device import DeviceName, DeviceParameter
 from core.device.camera import CameraConfiguration
 from core.session.shot import CameraTimeLane
-from core.session.shot.timelane import TakePicture
 
 
 def get_cameras_initialization_parameters(
@@ -21,23 +20,6 @@ def get_cameras_initialization_parameters(
             )
         camera_config = camera_configs[camera_name]
         init_kwargs = camera_config.get_device_init_args()
-        picture_names = get_picture_names(camera_lane)
-        init_kwargs[DeviceParameter("picture_names")] = get_picture_names(camera_lane)
-        init_kwargs[DeviceParameter("exposures")] = [
-            camera_config.get_default_exposure()
-        ] * len(picture_names)
         result[camera_name] = init_kwargs
 
-    return result
-
-
-def get_picture_names(lane: CameraTimeLane) -> list[str]:
-    result = []
-    for block_value in lane.values():
-        if isinstance(block_value, TakePicture):
-            result.append(block_value.picture_name)
-        elif block_value is None:
-            pass
-        else:
-            assert_never(block_value)
     return result
