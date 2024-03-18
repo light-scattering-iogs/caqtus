1545ce2632185bd541809e49ea2f0e1571743e94
Damien Bloch <damien.bloch@institutoptique>
2024-03-18 21:53:59 +0100

    Attempt to reproduce device proxy bug

--- core/core.device/tests/test_remote_device_manager.py
+++ core/core.device/tests/test_remote_device_manager.py
@@ -1,8 +1,15 @@
+import contextlib
 import os
-from typing import Self
+from typing import Self, Iterable
 
 from core.device import Device, DeviceName
-from core.device.remote_server import RemoteDeviceManager, DeviceProxy
+from core.device.camera import Camera
+from core.device.remote_server import RemoteDeviceManager, DeviceProxy, CameraProxy, \
+    SequencerProxy
+from core.device.sequencer import Sequencer
+from core.device.sequencer.instructions import SequencerInstruction
+from core.types.image import Image
+from util.roi import RectangularROI
 
 
 class DeviceTest(Device):
@@ -30,14 +37,72 @@ class DeviceTest(Device):
         raise Exception("This object cannot be pickled")
 
 
+class TestCamera(Camera):
+    sensor_width = 100
+    sensor_height = 100
+
+    @contextlib.contextmanager
+    def acquire(
+        self, exposures: list[float]
+    ) -> contextlib.AbstractContextManager[Iterable[Image]]:
+        yield self.yield_images(exposures)
+
+    def yield_images(self, exposures: list[float]) -> Iterable[Image]:
+        for _ in range(len(exposures)):
+            yield None
+
+    def update_parameters(self, timeout: float) -> None:
+        pass
+
+
+class TestSequencer(Sequencer):
+    def update_parameters(self, *_, sequence: SequencerInstruction, **kwargs) -> None:
+        pass
+
+    def start_sequence(self) -> None:
+        pass
+
+    def has_sequence_finished(self) -> bool:
+        pass
+
+
+class T1(DeviceTest):
+    def method_1(self):
+        return "method 1"
+
+
+class T2(DeviceTest):
+    def method_2(self):
+        return "method 2"
+
+
+class T1Proxy(DeviceProxy):
+    _exposed_ = DeviceProxy._exposed_ + ("method_1",)
+
+    def method_1(self):
+        return self._callmethod("method_1")
+
+
+class T2Proxy(DeviceProxy):
+    _exposed_ = DeviceProxy._exposed_ + ("method_2",)
+
+    def method_2(self):
+        return self._callmethod("method_2")
+
+
 class Manager(RemoteDeviceManager):
     pass
 
 
 Manager.register_device(DeviceTest, DeviceProxy)
+Manager.register_device(T1, T1Proxy)
+Manager.register_device(T2, T2Proxy)
+Manager.register_device(TestCamera, CameraProxy)
+Manager.register_device(TestSequencer, SequencerProxy)
+Manager.register_device(TestSequencer, SequencerProxy)
 
 
-def test():
+def test_0():
     print(os.getpid())
     with Manager() as m:
         device = m.DeviceTest("test_device")
@@ -45,3 +110,26 @@ def test():
         assert isinstance(device, DeviceProxy)
         with device as d:
             assert d.get_name() == "test_device"
+
+
+def test_1():
+    with Manager() as m:
+        dev1 = m.T1("test_device")
+        dev2 = m.T2("test_device")
+
+        assert dev1.method_1() == "method 1"
+        assert dev2.method_2() == "method 2"
+
+
+def test_2():
+    with Manager() as m:
+        cam = m.TestCamera(
+            timeout=1,
+            external_trigger=False,
+            roi=RectangularROI((100, 100), 0, 100, 0, 100),
+        )
+        with cam:
+            with cam.acquire([0.1, 0.2, 0.3]) as images:
+                for image in images:
+                    pass
+
