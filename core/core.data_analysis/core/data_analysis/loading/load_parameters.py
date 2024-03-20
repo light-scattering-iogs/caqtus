9304bb8364bbc30aad34a4cc5ef5ee92606e752d
Damien <damien.bloch@institutoptique.fr>
2024-03-20 23:31:40 +0100

    Add possibility to load only sequence specific parameters

--- core/core.data_analysis/core/data_analysis/loading/load_parameters.py
+++ core/core.data_analysis/core/data_analysis/loading/load_parameters.py
@@ -3,10 +3,11 @@ from typing import Literal
 
 import attrs
 import polars
-from core.session import ExperimentSession, Shot
+from core.session import ExperimentSession, Shot, Sequence
 from core.types.parameter import is_analog_value, is_quantity
 
 from .combinable_importers import CombinableLoader
+from .sequence_cache import cache_per_sequence
 
 
 @attrs.define
@@ -14,19 +15,25 @@ class LoadShotParameters(CombinableLoader):
     """Loads the parameters of a shot.
 
     When it is evaluated on a shot, it returns a polars dataframe with a single row and
-    with several columns named after each parameter defined for the shot and containing
-    their values.
+    with several columns named after each parameter requested.
 
     If some parameters are quantity with units, the dtype of the associated column will
     be a quantity dtype with two fields, magnitude and units.
 
     Attributes:
         which: the parameters to load from a shot.
-        If "all", all parameters are loaded.
-
+        If it is "sequence", only the parameters defined at the sequence level are loaded.
+        If it is "globals", only the values of the global parameters at the time the sequence was launched are loaded.
+        Note that the values of the globals parameters will be constant for all shot of a given sequence, unless they
+        are overwritten by the sequence iteration.
+        If "all", both sequence specific and global parameters are loaded.
+        If it is an iterable of strings, only the parameters with the given names are loaded.
     """
 
-    which: Literal["all"] | Iterable[str] = "all"
+    which: Literal["sequence", "all"] | Iterable[str] = "all"
+
+    def __attrs_post_init__(self):
+        self._get_local_parameters = cache_per_sequence(get_local_parameters)
 
     def __call__(self, shot: Shot, session: ExperimentSession) -> polars.DataFrame:
         parameters = {
@@ -36,6 +43,11 @@ class LoadShotParameters(CombinableLoader):
 
         if self.which == "all":
             pass
+        elif self.which == "locals":
+            local_parameters = self._get_local_parameters(shot.sequence, session)
+            parameters = {name: parameters[name] for name in local_parameters}
+        elif self.which == "globals":
+            raise NotImplementedError
         else:
             parameters = {name: parameters[name] for name in self.which}
 
@@ -59,3 +71,7 @@ class LoadShotParameters(CombinableLoader):
         series.sort(key=lambda s: s.name)
         dataframe = polars.DataFrame(series)
         return dataframe
+
+
+def get_local_parameters(sequence: Sequence, session: ExperimentSession) -> list[str]:
+    return [str(name) for name in sequence.get_local_parameters(session)]
