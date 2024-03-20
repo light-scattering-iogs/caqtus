c2f19f2c38691506bbd68c768b1667f3aee90f9e
Damien <damien.bloch@institutoptique.fr>
2024-03-20 22:26:42 +0100

    Add possibility to select which parameters to load from a shot

--- core/core.data_analysis/core/data_analysis/loading/load_parameters.py
+++ core/core.data_analysis/core/data_analysis/loading/load_parameters.py
@@ -1,3 +1,7 @@
+from collections.abc import Iterable
+from typing import Literal
+
+import attrs
 import polars
 from core.session import ExperimentSession, Shot
 from core.types.parameter import is_analog_value, is_quantity
@@ -5,6 +9,7 @@ from core.types.parameter import is_analog_value, is_quantity
 from .combinable_importers import CombinableLoader
 
 
+@attrs.define
 class LoadShotParameters(CombinableLoader):
     """Loads the parameters of a shot.
 
@@ -14,13 +19,25 @@ class LoadShotParameters(CombinableLoader):
 
     If some parameters are quantity with units, the dtype of the associated column will
     be a quantity dtype with two fields, magnitude and units.
+
+    Attributes:
+        which: the parameters to load from a shot.
+        If "all", all parameters are loaded.
+
     """
+
+    which: Literal["all"] | Iterable[str] = "all"
 
     def __call__(self, shot: Shot, session: ExperimentSession) -> polars.DataFrame:
         parameters = {
             str(parameter_name): value
             for parameter_name, value in shot.get_parameters(session).items()
         }
+
+        if self.which == "all":
+            pass
+        else:
+            parameters = {name: parameters[name] for name in self.which}
 
         series: list[polars.Series] = []
 
