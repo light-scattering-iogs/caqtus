d465ce2b426deb3f6559e00d96604c7cf2c1b4ed
damien <damien.bloch@institutoptique.fr>
2024-03-23 13:44:57 +0100

    Fix import in caqtus.utils.roi

--- caqtus.utils/caqtus/utils/roi/__init__.py
+++ caqtus.utils/caqtus/utils/roi/__init__.py
@@ -1,4 +1,4 @@
-from util import serialization
+from caqtus.utils import serialization
 from .arbitrary_roi import ArbitraryROI
 from .rectangular_roi import RectangularROI
 from .roi import ROI, Width, Height
