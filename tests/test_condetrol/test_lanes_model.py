d4db92aab478a6d1771c36cc8364173928c6909f
Dear <lechadoq@gmail.com>
2024-03-24 14:47:10 +0100

    Fix test for inserting time lane in TimeLanesModel

--- tests/test_condetrol/test_lanes_model.py
+++ tests/test_condetrol/test_lanes_model.py
@@ -1,11 +1,25 @@
-from caqtus.gui.condetrol.timelanes_editor import TimeLanesModel, default_lane_model_factory
+import pytest
+
+from caqtus.gui.condetrol.timelanes_editor import TimeLanesModel, \
+    default_lane_model_factory
 from caqtus.session.shot import DigitalTimeLane
 
 
 def test_0():
     model = TimeLanesModel(default_lane_model_factory)
 
-    lane = DigitalTimeLane([])
+    lane = DigitalTimeLane([True, False])
+    with pytest.raises(ValueError):
+        model.insert_timelane(0, "lane", lane)
+
+
+def test_1():
+    model = TimeLanesModel(default_lane_model_factory)
+
+    model.insertColumn(0)
+    model.insertColumn(0)
+
+    lane = DigitalTimeLane([True, False])
     model.insert_timelane(0, "lane", lane)
 
     assert model.get_timelanes().lanes["lane"] == lane
