c692336bb07d51abe7b30835d111d6d45be37968
Damien Bloch <damien.bloch@institutoptique>
2024-03-24 19:29:52 +0100

    Export TimeLanesPlugin

--- caqtus/gui/condetrol/__init__.py
+++ caqtus/gui/condetrol/__init__.py
@@ -1,3 +1,4 @@
 from .main_window import CondetrolMainWindow
+from .timelanes_editor import TimeLanesPlugin, default_time_lanes_plugin
 
-__all__ = ["CondetrolMainWindow"]
+__all__ = ["CondetrolMainWindow", "TimeLanesPlugin", "default_time_lanes_plugin"]
