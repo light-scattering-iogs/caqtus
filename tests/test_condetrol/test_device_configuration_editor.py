8006a057685e88ba2a37dfa8c1d8cdd1308a7a9c
damien <damien.bloch@institutoptique.fr>
2024-03-30 13:51:47 +0100

    Add test to device configuration switch in DeviceConfigurationsView

--- tests/test_condetrol/test_device_configuration_editor.py
+++ tests/test_condetrol/test_device_configuration_editor.py
@@ -29,3 +29,24 @@ def test_edit(qtbot: QtBot):
     new_configs = view.get_device_configurations()
     assert new_configs[DeviceName("Device 1")].remote_server == "new"
     assert new_configs[DeviceName("Device 2")].remote_server == "default"
+
+
+def test_edit_1(qtbot: QtBot):
+    device_configurations = {
+        DeviceName("Device 1"): MockDeviceConfiguration(remote_server="default"),
+        DeviceName("Device 2"): MockDeviceConfiguration(remote_server="default"),
+    }
+    view = DeviceConfigurationsView(default_device_editor_factory, parent=None)
+    view.set_device_configurations(device_configurations)
+    qtbot.addWidget(view)
+
+    first_index = view.model().index(0, 0)
+    view.setCurrentIndex(first_index)
+    editor = view.previewWidget()
+    assert isinstance(editor, DefaultDeviceConfigurationEditor)
+    assert editor.remote_server_line_edit.text() == "default"
+    editor.remote_server_line_edit.setText("new")
+    view.setCurrentIndex(view.model().index(1, 0))
+    new_configs = view.get_device_configurations()
+    assert new_configs[DeviceName("Device 1")].remote_server == "new"
+    assert new_configs[DeviceName("Device 2")].remote_server == "default"
