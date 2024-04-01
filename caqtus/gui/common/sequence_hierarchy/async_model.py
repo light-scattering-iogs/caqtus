11d1184c498154afec7ff16bb1be4014a0deb5be
damien <damien.bloch@institutoptique.fr>
2024-04-01 13:47:04 +0200

    Add progressive population of AsyncPathHierarchyModel

--- caqtus/gui/common/sequence_hierarchy/async_model.py
+++ caqtus/gui/common/sequence_hierarchy/async_model.py
@@ -1,9 +1,16 @@
 from typing import Optional
 
 from PySide6.QtCore import QObject, QAbstractItemModel, QModelIndex, Qt
-from PySide6.QtGui import QStandardItemModel
+from PySide6.QtGui import QStandardItemModel, QStandardItem
 
-from caqtus.session import ExperimentSessionMaker
+from caqtus.session import ExperimentSessionMaker, PureSequencePath
+from caqtus.session._return_or_raise import unwrap
+from caqtus.session.path_hierarchy import PathNotFoundError
+from caqtus.session.sequence_collection import PathIsSequenceError
+
+FULL_PATH_ROLE = Qt.UserRole + 1
+HAS_FETCHED_CHILDREN = Qt.UserRole + 2
+PATH_IS_SEQUENCE_ROLE = Qt.UserRole + 3
 
 
 class AsyncPathHierarchyModel(QAbstractItemModel):
@@ -14,15 +21,96 @@ class AsyncPathHierarchyModel(QAbstractItemModel):
         self.session_maker = session_maker
 
         self.tree = QStandardItemModel(self)
+        self.tree.invisibleRootItem().setData(False, HAS_FETCHED_CHILDREN)
+        self.tree.invisibleRootItem().setData(PureSequencePath.root(), FULL_PATH_ROLE)
+        self.tree.invisibleRootItem().setData(False, PATH_IS_SEQUENCE_ROLE)
 
     def index(self, row, column, parent=QModelIndex()):
-        return self.tree.index(row, column, parent)
+        if not self.hasIndex(row, column, parent):
+            return QModelIndex()
+        parent_item = (
+            parent.internalPointer()
+            if parent.isValid()
+            else self.tree.invisibleRootItem()
+        )
+        child_item = parent_item.child(row)
+        return (
+            self.createIndex(row, column, child_item) if child_item else QModelIndex()
+        )
 
     def parent(self, index=QModelIndex()):
-        return self.tree.parent(index)
+        if not index.isValid():
+            return QModelIndex()
+        child_item = index.internalPointer()
+        parent_item = child_item.parent()
+        if parent_item is None:
+            return QModelIndex()
+        return (
+            self.createIndex(parent_item.row(), index.column(), parent_item)
+            if parent_item is not self.tree.invisibleRootItem()
+            else QModelIndex()
+        )
+
+    def _get_item(self, index) -> QStandardItem:
+        result = (
+            index.internalPointer()
+            if index.isValid()
+            else self.tree.invisibleRootItem()
+        )
+        assert isinstance(result, QStandardItem)
+        return result
 
     def rowCount(self, parent=QModelIndex()):
-        return self.tree.rowCount(parent)
+        parent_item = self._get_item(parent)
+        if parent_item.data(PATH_IS_SEQUENCE_ROLE):
+            return 0
+        if parent_item.data(HAS_FETCHED_CHILDREN):
+            return parent_item.rowCount()
+        else:
+            return 0
+
+    def hasChildren(self, parent=QModelIndex()) -> bool:
+        parent_item = self._get_item(parent)
+        if parent_item.data(PATH_IS_SEQUENCE_ROLE):
+            return False
+        if parent_item.data(HAS_FETCHED_CHILDREN):
+            return parent_item.rowCount() > 0
+        else:
+            return True
+
+    def canFetchMore(self, parent) -> bool:
+        parent_item = self._get_item(parent)
+        if parent_item.data(PATH_IS_SEQUENCE_ROLE):
+            return False
+        return not parent_item.data(HAS_FETCHED_CHILDREN)
+
+    def fetchMore(self, parent):
+        parent_item = self._get_item(parent)
+        if parent_item.data(PATH_IS_SEQUENCE_ROLE):
+            return
+        if parent_item.data(HAS_FETCHED_CHILDREN):
+            return
+
+        parent_path = parent_item.data(FULL_PATH_ROLE)
+        with self.session_maker() as session:
+            children_result = session.paths.get_children(parent_path)
+        try:
+            children = unwrap(children_result)
+        except PathIsSequenceError:
+            parent_item.setData(True, PATH_IS_SEQUENCE_ROLE)
+        except PathNotFoundError:
+            pass
+        else:
+            self.beginInsertRows(parent, 0, len(children) - 1)
+            for child_path in children:
+                child_item = QStandardItem()
+                child_item.setData(child_path.name, Qt.DisplayRole)
+                child_item.setData(child_path, FULL_PATH_ROLE)
+                child_item.setData(False, HAS_FETCHED_CHILDREN)
+                child_item.setData(False, PATH_IS_SEQUENCE_ROLE)
+                parent_item.appendRow(child_item)
+                parent_item.setData(True, HAS_FETCHED_CHILDREN)
+            self.endInsertRows()
 
     def columnCount(self, parent=QModelIndex()):
         return 1
@@ -30,7 +118,5 @@ class AsyncPathHierarchyModel(QAbstractItemModel):
     def data(self, index, role=Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
-
-        item = self.tree.itemFromIndex(index)
-
-        return None
+        item = self._get_item(index)
+        return item.data(role)
--- tests/test_sequence_hierarchy_widget/test.py
+++ tests/test_sequence_hierarchy_widget/test.py
@@ -1,8 +1,39 @@
-from caqtus.gui.common.sequence_hierarchy import AsyncPathHierarchyModel
-from .session_maker import session_maker
+from PySide6.QtCore import Qt
 from pytestqt.modeltest import ModelTester
+
+from caqtus.gui.common.sequence_hierarchy import AsyncPathHierarchyModel
+from caqtus.session import PureSequencePath
+from .session_maker import session_maker
 
 
 def test_0(session_maker, qtmodeltester: ModelTester):
     model = AsyncPathHierarchyModel(session_maker)
+    with session_maker() as session:
+        session.paths.create_path(PureSequencePath(r"\test"))
     qtmodeltester.check(model)
+
+
+def test_1(session_maker, qtmodeltester: ModelTester):
+    model = AsyncPathHierarchyModel(session_maker)
+    with session_maker() as session:
+        session.paths.create_path(PureSequencePath(r"\test\test2"))
+    qtmodeltester.check(model)
+    assert model.rowCount() == 1
+    child = model.index(0, 0)
+    assert model.rowCount(child) == 1
+    assert model.data(child, Qt.ItemDataRole.DisplayRole) == "test"
+    child = model.index(0, 0, child)
+    assert model.data(child, Qt.ItemDataRole.DisplayRole) == "test2"
+
+
+# def test_2(session_maker, qtbot: QtBot):
+#     with session_maker() as session:
+#         for i in range(1000):
+#             session.paths.create_path(PureSequencePath(rf"\test\a\test{i}"))
+#     model = AsyncPathHierarchyModel(session_maker)
+#     view = QTreeView()
+#     qtbot.addWidget(view)
+#     view.setModel(model)
+#     view.show()
+#
+#     qtbot.stop()
