2853ec4ee29850be272f31cdb1bf5b4505b792c4
damien <damien.bloch@institutoptique.fr>
2024-04-01 18:05:02 +0200

    Add header menu to AsyncPathHierarchyView

--- caqtus/gui/common/sequence_hierarchy/view.py
+++ caqtus/gui/common/sequence_hierarchy/view.py
@@ -29,13 +29,38 @@ class AsyncPathHierarchyView(QTreeView):
         self.header().setContextMenuPolicy(
             QtCore.Qt.ContextMenuPolicy.CustomContextMenu
         )
-        # self.header().customContextMenuRequested.connect(self.show_header_menu)
-        # self.doubleClicked.connect(self.on_double_click)
+        self.header().customContextMenuRequested.connect(self.show_header_menu)
+        self.doubleClicked.connect(self.on_double_click)
         # self._model.dataChanged.connect(self.update)
         self.sortByColumn(4, QtCore.Qt.SortOrder.AscendingOrder)
         self.hideColumn(4)
         self.setItemDelegateForColumn(1, ProgressDelegate(self))
         self.setUniformRowHeights(True)
+
+    def show_header_menu(self, pos):
+        menu = QMenu(self)
+        visibility_menu = menu.addMenu("Visible")
+        # The first column is the name and should not be hidden.
+        for column in range(1, self.model().columnCount()):
+            action = QAction(
+                self.model().headerData(column, QtCore.Qt.Orientation.Horizontal), self
+            )
+            action.setCheckable(True)
+            action.setChecked(not self.isColumnHidden(column))
+            action.triggered.connect(functools.partial(self.toggle_visibility, column))
+            visibility_menu.addAction(action)
+        menu.exec(self.mapToGlobal(pos))
+
+    def toggle_visibility(self, column: int):
+        column_hidden = self.isColumnHidden(column)
+        self.setColumnHidden(column, not column_hidden)
+
+    def on_double_click(self, index: QModelIndex):
+        path = self._model.get_path(self._proxy_model.mapToSource(index))
+        with self.session_maker() as session:
+            is_sequence = unwrap(session.sequences.is_sequence(path))
+        if is_sequence:
+            self.sequence_double_clicked.emit(path)
 
 
 class PathHierarchyView(QTreeView):
