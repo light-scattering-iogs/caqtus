c7216af9a858d9ea447bbbbfb04f45a3d3b5cb8d
damien <damien.bloch@institutoptique.fr>
2024-04-04 22:01:31 +0200

    Add get_lane argument to ShotContext

--- caqtus/device/camera/_configuration.py
+++ caqtus/device/camera/_configuration.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import TypeVar, TypedDict, Mapping, Any
+from typing import TypeVar, TypedDict
 
 import attrs
 
@@ -49,13 +49,11 @@ class CameraConfiguration(DeviceConfiguration, ABC):
     def compile_device_shot_parameters(
         self,
         device_name: DeviceName,
-        sequence_context: SequenceContext,
         shot_context: ShotContext,
-        already_compiled: Mapping[DeviceName, Mapping[str, Any]],
     ) -> CameraUpdateParams:
-        lane = sequence_context.get_lane(device_name)
+        lane = shot_context.get_lane(device_name)
         if not isinstance(lane, CameraTimeLane):
-            raise ValueError(f"Expected a camera lane for device {device_name}")
+            raise TypeError(f"Expected a camera lane for device {device_name}")
         step_durations = shot_context.get_step_durations()
         exposures = []
         for value, (start, stop) in zip(lane.values(), lane.bounds()):
@@ -64,7 +62,7 @@ class CameraConfiguration(DeviceConfiguration, ABC):
                 exposures.append(exposure)
         return CameraUpdateParams(
             # Add a bit of extra time to the timeout, in case the shot takes a bit of
-            # tim to start
+            # time to actually start.
             timeout=shot_context.get_shot_duration() + 1,
             exposures=exposures,
         )
