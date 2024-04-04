54a9c4d1ed3835d1b635061437cd016a5c7e9a01
damien <damien.bloch@institutoptique.fr>
2024-04-04 22:18:36 +0200

    Implement get_lane for ShotContext

--- caqtus/shot_compilation/compilation_contexts.py
+++ caqtus/shot_compilation/compilation_contexts.py
@@ -1,4 +1,4 @@
-from collections.abc import Mapping
+from collections.abc import Mapping, Iterable
 from typing import Any
 
 import attrs
@@ -6,10 +6,33 @@ import attrs
 from caqtus.device import DeviceName, DeviceConfiguration
 from caqtus.session.shot import TimeLanes, TimeLane
 from caqtus.types.variable_name import DottedVariableName
+from .lane_compilers.timing import get_step_bounds
+from ..types.expression import Expression
+from ..types.parameter import is_quantity, magnitude_in_unit
 
 
 @attrs.define(slots=False)
 class ShotContext:
+    """Contains information about a shot being compiled."""
+
+    _time_lanes: TimeLanes
+    _variables: Mapping[DottedVariableName, Any]
+
+    _step_durations: tuple[float, ...] = attrs.field(init=False)
+    _step_bounds: tuple[float, ...] = attrs.field(init=False)
+    _was_lane_used: dict[str, bool] = attrs.field(init=False)
+
+    def __attrs_post_init__(self):
+        self._step_durations = tuple(
+            evaluate_step_durations(
+                self._time_lanes.step_names,
+                self._time_lanes.step_durations,
+                self._variables,
+            )
+        )
+        self._step_bounds = tuple(get_step_bounds(self._step_durations))
+        self._was_lane_used = {name: False for name in self._time_lanes.lanes}
+
     def get_lane(self, name: str) -> TimeLane:
         """Returns the lane with the given name for the shot.
 
@@ -17,13 +40,19 @@ class ShotContext:
             KeyError: If no lane with the given name is present for the shot.
         """
 
-        raise NotImplementedError
+        result = self._time_lanes.lanes[name]
+        self._was_lane_used[name] = True
+        return result
 
-    def get_step_durations(self) -> list[float]:
-        raise NotImplementedError
+    def get_step_durations(self) -> tuple[float, ...]:
+        """Returns the durations of each step in seconds."""
+
+        return self._step_durations
 
     def get_shot_duration(self) -> float:
-        raise NotImplementedError
+        """Returns the total duration of the shot in seconds."""
+
+        return self._step_bounds[-1]
 
     def get_variables(self) -> Mapping[DottedVariableName, Any]:
         raise NotImplementedError
@@ -45,3 +74,39 @@ class SequenceContext:
             context.
         """
         raise NotImplementedError
+
+
+def evaluate_step_durations(
+    step_names: Iterable[str],
+    step_durations: Iterable[Expression],
+    variables: Mapping[DottedVariableName, Any],
+) -> list[float]:
+    result = []
+
+    for name, duration in zip(step_names, step_durations):
+        try:
+            evaluated = duration.evaluate(variables)
+        except Exception as e:
+            raise ValueError(
+                f"Couldn't evaluate duration <{duration}> of step <{name}>"
+            ) from e
+
+        if not is_quantity(evaluated):
+            raise TypeError(
+                f"Duration <{duration}> of step <{name}> is not a quantity "
+                f"({evaluated})"
+            )
+
+        try:
+            seconds = magnitude_in_unit(evaluated, "s")
+        except Exception as error:
+            raise ValueError(
+                f"Duration <{duration}> of step <{name}> can't be converted to seconds "
+                f"({evaluated})"
+            ) from error
+        if seconds < 0:
+            raise ValueError(
+                f"Duration <{duration}> of step <{name}> is negative ({seconds})"
+            )
+        result.append(float(seconds))
+    return result
