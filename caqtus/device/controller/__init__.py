c32cf72847123ae4e7f42cc47f0a931eb5968975
damien <damien.bloch@institutoptique.fr>
2024-04-06 20:26:06 +0200

    Add async utilities for device controller

--- caqtus/device/controller/__init__.py
+++ caqtus/device/controller/__init__.py
@@ -1,3 +1,15 @@
-from ._controller import DeviceController
+from ._controller import (
+    DeviceController,
+    iterate_async,
+    run_in_thread,
+    async_context,
+    sleep,
+)
 
-__all__ = ["DeviceController"]
+__all__ = [
+    "DeviceController",
+    "iterate_async",
+    "run_in_thread",
+    "async_context",
+    "sleep",
+]
--- caqtus/device/controller/_controller.py
+++ caqtus/device/controller/_controller.py
@@ -1,13 +1,16 @@
 import abc
+import contextlib
 import functools
-from collections.abc import Callable
+from collections.abc import Callable, AsyncGenerator, Iterable, AsyncIterator
 from typing import Generic, TypeVar, ParamSpec
 
 import anyio
 import anyio.to_thread
 
 from caqtus.shot_event_dispatcher import ShotEventDispatcher
+from .. import DeviceName
 from ..runtime import Device
+from ...types.data import DataLabel, Data
 
 DeviceType = TypeVar("DeviceType", bound=Device)
 
@@ -22,12 +25,23 @@ class DeviceController(Generic[DeviceType, ShotParametersType], abc.ABC):
 
     def __init__(
         self,
-        device: DeviceType,
+        device_name: DeviceName,
         shot_event_dispatcher: ShotEventDispatcher,
     ):
-        self.device = device
-        self.device_name = device.get_name()
+        #
+        self.device_name = device_name
         self.event_dispatcher = shot_event_dispatcher
+
+    @abc.abstractmethod
+    async def run_shot(
+        self, device: DeviceType, shot_parameters: ShotParametersType
+    ) -> None:
+        """Runs a shot on the device.
+
+        This method must call :meth:`signal_ready` exactly once.
+        """
+
+        self.signal_ready()
 
     def signal_ready(self) -> None:
         """Indicates that the device has been programed and is ready to run the shot."""
@@ -39,14 +53,43 @@ class DeviceController(Generic[DeviceType, ShotParametersType], abc.ABC):
 
         await self.event_dispatcher.wait_all_devices_ready()
 
-    async def run_in_thread(
-        self, func: Callable[_P, _T], *args: _P.args, **kwargs: _P.kwargs
-    ) -> _T:
-        return await anyio.to_thread.run_sync(functools.partial(func, *args, **kwargs))
+    def signal_data_acquired(self, label: DataLabel, data: Data) -> None:
+        """Signals that data has been acquired from the device."""
 
-    async def sleep(self, seconds: float) -> None:
-        await anyio.sleep(seconds)
+        self.event_dispatcher.signal_data_acquired(label, data)
 
-    @abc.abstractmethod
-    async def run_shot(self, shot_parameters: ShotParametersType) -> None:
-        self.signal_ready()
+    async def wait_data_acquired(self, label: DataLabel) -> Data:
+        """Waits until the data with the given label has been acquired."""
+
+        return await self.event_dispatcher.wait_data_acquired(label)
+
+
+async def run_in_thread(
+    func: Callable[_P, _T], *args: _P.args, **kwargs: _P.kwargs
+) -> _T:
+    return await anyio.to_thread.run_sync(functools.partial(func, *args, **kwargs))
+
+
+async def sleep(seconds: float) -> None:
+    await anyio.sleep(seconds)
+
+
+@contextlib.asynccontextmanager
+async def async_context(
+    cm: contextlib.AbstractContextManager[_T],
+) -> AsyncGenerator[_T, None]:
+    entered = await run_in_thread(cm.__enter__)
+    try:
+        yield entered
+    except BaseException as exc:
+        cm.__exit__(type(exc), exc, exc.__traceback__)
+        raise
+    else:
+        await run_in_thread(cm.__exit__, None, None, None)
+
+
+async def iterate_async(iterable: Iterable[_T]) -> AsyncIterator[_T]:
+    iterator = iter(iterable)
+    done = object()
+    while (value := await run_in_thread(next, iterator, done)) is not done:
+        yield value
