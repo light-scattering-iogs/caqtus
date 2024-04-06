0782490e51f36ce6dfad2dfde8f56d39df2f7718
damien <damien.bloch@institutoptique.fr>
2024-04-06 21:09:35 +0200

    Simplify run_shot for camera controller

--- caqtus/device/camera/_controller.py
+++ caqtus/device/camera/_controller.py
@@ -1,3 +1,6 @@
+import contextlib
+from collections.abc import AsyncIterable, AsyncGenerator
+
 from caqtus.device.controller import (
     DeviceController,
     run_in_thread,
@@ -5,6 +8,7 @@ from caqtus.device.controller import (
     iterate_async,
 )
 from caqtus.types.data import DataLabel
+from caqtus.types.image import Image
 from ._runtime import Camera
 
 
@@ -17,13 +21,33 @@ class CameraController(DeviceController[Camera]):
         picture_names: list[str],
         exposures: list[float],
     ) -> None:
+        await self.set_timeout(camera, timeout)
+        async with self.acquire_pictures(camera, picture_names, exposures) as pictures:
+            async for picture in pictures:
+                pass
+
+    @staticmethod
+    async def set_timeout(camera: Camera, timeout: float) -> None:
         await run_in_thread(camera.update_parameters, timeout=timeout)
 
+    @contextlib.asynccontextmanager
+    async def acquire_pictures(
+        self, camera: Camera, picture_names: list[str], exposures: list[float]
+    ) -> AsyncGenerator[AsyncIterable[tuple[str, Image]], None]:
+        if len(picture_names) != len(exposures):
+            raise ValueError(
+                f"Number of picture names ({len(picture_names)}) must be equal to the "
+                f"number of exposures ({len(exposures)})"
+            )
         async with async_context(camera.acquire(exposures)) as pictures:
             self.signal_ready()
-            async for name, picture in iterate_async(
-                zip(picture_names, pictures, strict=True)
-            ):
-                self.signal_data_acquired(
-                    DataLabel(rf"{self.device_name}\{name}"), picture
-                )
+            yield self.emit_signals(
+                iterate_async(zip(picture_names, pictures, strict=True))
+            )
+
+    async def emit_signals(
+        self, named_pictures: AsyncIterable[tuple[str, Image]]
+    ) -> AsyncGenerator[Image, None]:
+        async for name, picture in named_pictures:
+            self.signal_data_acquired(DataLabel(rf"{self.device_name}\{name}"), picture)
+            yield picture
