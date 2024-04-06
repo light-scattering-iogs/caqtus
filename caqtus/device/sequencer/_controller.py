c3c4f68b6917b9db161b1d7500a522f94a0562cf
damien <damien.bloch@institutoptique.fr>
2024-04-06 20:55:15 +0200

    Simplify run_shot for sequencer

--- caqtus/device/sequencer/_controller.py
+++ caqtus/device/sequencer/_controller.py
@@ -11,7 +11,15 @@ class SequencerController(DeviceController[Sequencer]):
     async def run_shot(
         self, sequencer: Sequencer, /, sequence: SequencerInstruction
     ) -> None:
+        await self.program(sequencer, sequence)
+        await self.start(sequencer)
+        await self.wait_until_finished(sequencer)
+
+    @staticmethod
+    async def program(sequencer: Sequencer, sequence: SequencerInstruction) -> None:
         await run_in_thread(sequencer.update_parameters, sequence=sequence)
+
+    async def start(self, sequencer: Sequencer) -> None:
         if isinstance(sequencer.get_trigger(), SoftwareTrigger):
             self.signal_ready()
             await self.wait_all_devices_ready()
@@ -19,6 +27,11 @@ class SequencerController(DeviceController[Sequencer]):
         else:
             await run_in_thread(sequencer.start_sequence)
             self.signal_ready()
+
+    @staticmethod
+    async def wait_until_finished(sequencer: Sequencer) -> None:
+        # We shield the task because we don't want the sequence to be stopped in the
+        # middle with possibly dangerous values on the sequencer channels.
         with anyio.CancelScope(shield=True):
             while not await run_in_thread(sequencer.has_sequence_finished):
                 await sleep(0)
