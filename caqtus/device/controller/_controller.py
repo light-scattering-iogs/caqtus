eb9498bd73ad978dba57711402f1d0bb3cb7696d
damien <damien.bloch@institutoptique.fr>
2024-04-06 16:48:50 +0200

    Add signal_ready to DeviceController

--- caqtus/device/controller/_controller.py
+++ caqtus/device/controller/_controller.py
@@ -1,9 +1,23 @@
 import abc
 
+from caqtus.device import DeviceName
 from caqtus.shot_event_dispatcher import ShotEventDispatcher
 
 
 class DeviceController(abc.ABC):
+    """Controls a device during a shot."""
+
+    def __init__(
+        self, device_name: DeviceName, shot_event_dispatcher: ShotEventDispatcher
+    ):
+        self._shot_event_dispatcher = shot_event_dispatcher
+        self._device_name = device_name
+
+    def signal_ready(self) -> None:
+        """Indicates that the device has been programed and is ready to run the shot."""
+
+        self._shot_event_dispatcher.signal_device_ready(self._device_name)
+
     @abc.abstractmethod
-    async def run_shot(self, shot_event_dispatcher: ShotEventDispatcher) -> None:
-        raise NotImplementedError
+    async def run_shot(self) -> None:
+        self.signal_ready()
