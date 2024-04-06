1aca558fdab27d247f867d3acfcad06ac4e832ef
damien <damien.bloch@institutoptique.fr>
2024-04-06 17:46:52 +0200

    Add ability to acquire data and signal acquisition in ShotEventDispatcher

--- caqtus/shot_event_dispatcher/_shot_event_dispatcher.py
+++ caqtus/shot_event_dispatcher/_shot_event_dispatcher.py
@@ -1,13 +1,19 @@
+import collections
 from collections.abc import Set
 
 import anyio
 
 from caqtus.device import DeviceName
+from caqtus.types.data import DataLabel, Data
 
 
 class ShotEventDispatcher:
     def __init__(self, devices_in_use: Set[DeviceName]):
         self._device_ready = {device: anyio.Event() for device in devices_in_use}
+        self._acquisition_events: dict[
+            DataLabel, anyio.Event
+        ] = collections.defaultdict(anyio.Event)
+        self._acquired_data: dict[DataLabel, Data] = {}
 
     def signal_device_ready(self, device: DeviceName) -> None:
         if device not in self._device_ready:
@@ -25,3 +31,17 @@ class ShotEventDispatcher:
         async with anyio.create_task_group() as tg:
             for event in self._device_ready.values():
                 tg.start_soon(event.wait)
+
+    async def wait_data_acquired(self, label: DataLabel) -> Data:
+        if label in self._acquired_data:
+            return self._acquired_data
+        else:
+            await self._acquisition_events[label].wait()
+
+    def signal_data_acquired(self, label: DataLabel, data: Data) -> None:
+        if label in self._acquired_data:
+            raise KeyError(f"There is already data acquired for label {label}")
+        else:
+            self._acquired_data[label] = data
+            if label in self._acquisition_events:
+                self._acquisition_events[label].set()
