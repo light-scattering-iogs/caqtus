525cc59bf31972b3bc2f2e1b539fff12a18c9c9c
damien <damien.bloch@institutoptique.fr>
2024-04-07 17:33:03 +0200

    Fix failing digital lane compilation

--- caqtus/shot_compilation/lane_compilers/__init__.py
+++ caqtus/shot_compilation/lane_compilers/__init__.py
@@ -1,7 +1,5 @@
 from .evaluate_step_durations import evaluate_step_durations
 
 __all__ = [
-    "DigitalLaneCompiler",
-    "AnalogLaneCompiler",
     "evaluate_step_durations",
 ]
--- tests/test_shot_compilation/test_digital_lane_compilation.py
+++ tests/test_shot_compilation/test_digital_lane_compilation.py
@@ -1,34 +1,52 @@
-from caqtus.shot_compilation import VariableNamespace
-from caqtus.shot_compilation.lane_compilers import DigitalLaneCompiler
+from caqtus.device.sequencer.configuration._compile_digital_lane import (
+    compile_digital_lane,
+)
 from caqtus.device.sequencer.instructions import Pattern
-from caqtus.session.shot import DigitalTimeLane
+from caqtus.session.shot import DigitalTimeLane, TimeLanes
+from caqtus.shot_compilation import VariableNamespace, ShotContext
 from caqtus.types.expression import Expression
 from caqtus.types.units import Quantity
+from caqtus.types.variable_name import DottedVariableName
 
 
 def test_0():
-    lane = DigitalTimeLane([True, False])
-    lane_compiler = DigitalLaneCompiler(
-        lane, ["a", "b"], [Expression("1 s"), Expression("1 s")]
+    shot_context = ShotContext(
+        device_configurations={},
+        time_lanes=TimeLanes(
+            step_names=["a", "b"], step_durations=[Expression("1 s"), Expression("1 s")]
+        ),
+        variables={},
     )
-    result = lane_compiler.compile(VariableNamespace(), 1)
+    lane = DigitalTimeLane([True, False])
+    result = compile_digital_lane(lane, 1, shot_context)
     assert result == Pattern([True]) * 1_000_000_000 + Pattern([False]) * 1_000_000_000
 
 
 def test_1():
-    lane = DigitalTimeLane([Expression("a"), Expression("b")])
-    lane_compiler = DigitalLaneCompiler(
-        lane, ["a", "b"], [Expression("1 s"), Expression("1 s")]
+    shot_context = ShotContext(
+        device_configurations={},
+        time_lanes=TimeLanes(
+            step_names=["a", "b"], step_durations=[Expression("1 s"), Expression("1 s")]
+        ),
+        variables={DottedVariableName("a"): True, DottedVariableName("b"): False},
     )
-    result = lane_compiler.compile(VariableNamespace({"a": True, "b": False}), 1)
+    lane = DigitalTimeLane([Expression("a"), Expression("b")])
+    result = compile_digital_lane(lane, 1, shot_context)
 
     assert result == Pattern([True]) * 1_000_000_000 + Pattern([False]) * 1_000_000_000
 
 
 def test_2():
+    shot_context = ShotContext(
+        device_configurations={},
+        time_lanes=TimeLanes(
+            step_names=["a", "b", "c"],
+            step_durations=[Expression("1 s")] * 3,
+        ),
+        variables={},
+    )
     lane = DigitalTimeLane([True] * 2 + [False])
-    lane_compiler = DigitalLaneCompiler(lane, ["a", "b", "c"], [Expression("1 s")] * 3)
-    result = lane_compiler.compile(VariableNamespace(), 1)
+    result = compile_digital_lane(lane, 1, shot_context)
     assert (
         result == Pattern([True]) * 2_000_000_000 + Pattern([False]) * 1_000_000_000
     ), str(result)
@@ -81,6 +99,13 @@ def test_3():
             "exposure": Quantity(30, "millisecond"),
         }
     )
-    lane_compiler = DigitalLaneCompiler(lane, lane_names, lane_durations)
-    result = lane_compiler.compile(variables, 1)
+    shot_context = ShotContext(
+        device_configurations={},
+        time_lanes=TimeLanes(
+            step_names=lane_names,
+            step_durations=lane_durations,
+        ),
+        variables=variables.dict(),
+    )
+    result = compile_digital_lane(lane, 1, shot_context)
     assert len(result) == 310_000_001
