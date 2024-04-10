051b4f7f713a858f21451ddcaeb4c80f68da04e0
damien <damien.bloch@institutoptique.fr>
2024-04-10 21:43:48 +0200

    Add failing test for stacking clock pulses

--- tests/test_device/test_sequencer/test_adaptative_clock.py
+++ tests/test_device/test_sequencer/test_adaptative_clock.py
@@ -1,5 +1,7 @@
+import pytest
+
 from caqtus.device.sequencer.compilation import get_adaptive_clock
-from caqtus.device.sequencer.instructions import Pattern
+from caqtus.device.sequencer.instructions import Pattern, stack_instructions, with_name
 
 
 def test_0():
@@ -10,3 +12,13 @@ def test_0():
     clock = get_adaptive_clock(target, pulse)
 
     assert clock == pulse * len(target)
+
+
+@pytest.mark.xfail
+def test_1():
+    instr1 = Pattern([0]) * 8
+
+    instr2 = (Pattern([True]) * 2 + Pattern([False]) * 2) * 2
+
+    stacked = stack_instructions([with_name(instr1, "a"), with_name(instr2, "b")])
+    assert stacked["b"] == instr2
