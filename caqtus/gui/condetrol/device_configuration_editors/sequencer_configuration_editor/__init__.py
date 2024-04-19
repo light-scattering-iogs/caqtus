3bf848dc5d791a400299bbaa4fc9f7a248f1d2ed
damien <damien.bloch@institutoptique.fr>
2024-04-19 13:58:38 +0200

    Add sequence channels widget

--- caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/__init__.py
+++ caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/__init__.py
@@ -1,9 +1,13 @@
+"""This module provides widgets to edit a sequencer configuration."""
+
 from .channel_view import SequencerChannelView
 from .channels_model import SequencerChannelsModel
 from .sequencer_configuration_editor import SequencerConfigurationEditor
+from .channels_widget import SequencerChannelWidget
 
 __all__ = [
     "SequencerChannelsModel",
     "SequencerChannelView",
     "SequencerConfigurationEditor",
+    "SequencerChannelWidget",
 ]
