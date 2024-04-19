16fb03e60e93d6f58aeb58eeae7f3155a54714ba
damien <damien.bloch@institutoptique.fr>
2024-04-19 14:41:11 +0200

    Add constant node

--- caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/channel_output_editor/_channel_output_editor.py
+++ caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/channel_output_editor/_channel_output_editor.py
@@ -1,9 +1,11 @@
+import functools
 from typing import Optional
 
 from PySide6.QtWidgets import QWidget, QHBoxLayout
 
-from NodeGraphQt import NodeGraph
-from caqtus.device.sequencer.configuration import ChannelOutput
+from NodeGraphQt import NodeGraph, BaseNode
+from caqtus.device.sequencer.configuration import ChannelOutput, Constant
+from ._constant_node import ConstantNode
 from ._output_node import OutputNode
 
 
@@ -26,6 +28,20 @@ class NewChannelOutputEditor(QWidget):
         output_node.set_name()
         self.graph.add_node(output_node, selected=False, pos=[0, 0], push_undo=False)
 
+        node = self.build_node(channel_output)
+        node.outputs()["out"].connect_to(output_node.inputs()["in"])
+
     def clear_graph(self) -> None:
         for node in self.graph.all_nodes():
             self.graph.delete_node(node)
+
+    @functools.singledispatchmethod
+    def build_node(self, channel_output: ChannelOutput) -> BaseNode:
+        raise NotImplementedError
+
+    @build_node.register
+    def build_constant(self, constant: Constant) -> ConstantNode:
+        node = ConstantNode()
+        node.set_value(constant.value)
+        self.graph.add_node(node, selected=False, push_undo=False)
+        return node
---
+++ caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/channel_output_editor/_constant_node.py
@@ -0,0 +1,21 @@
+from NodeGraphQt import BaseNode
+from caqtus.types.expression import Expression
+
+
+class ConstantNode(BaseNode):
+    __identifier__ = "caqtus.sequencer_node"
+    NODE_NAME = "Constant"
+
+    def __init__(self):
+        super().__init__()
+        self.add_output("out", multi_output=False, display_name=False)
+        self.add_text_input(
+            "Value",
+            text="...",
+            placeholder_text="value",
+            tooltip="The output of this node is held constant during the whole shot.",
+        )
+        self.set_port_deletion_allowed(True)
+
+    def set_value(self, expression: Expression) -> None:
+        self.set_property("Value", str(expression))
