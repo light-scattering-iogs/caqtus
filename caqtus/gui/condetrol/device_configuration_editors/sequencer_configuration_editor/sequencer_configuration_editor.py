df9cdc67907e2244b6476e8c494d559c7b5bdc6c
damien <damien.bloch@institutoptique.fr>
2024-04-19 16:55:52 +0200

    Update sequencer configuration editor to use NodeGraphQt

--- caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/sequencer_configuration_editor.py
+++ caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/sequencer_configuration_editor.py
@@ -3,7 +3,7 @@ from typing import TypeVar, Generic, Optional
 from PySide6.QtWidgets import QWidget, QSpinBox
 
 from caqtus.device.sequencer import SequencerConfiguration
-from .channel_view import SequencerChannelView
+from .channels_widget import SequencerChannelWidget
 from ..device_configuration_editor import FormDeviceConfigurationEditor
 
 S = TypeVar("S", bound=SequencerConfiguration)
@@ -19,11 +19,13 @@ class SequencerConfigurationEditor(FormDeviceConfigurationEditor[S], Generic[S])
         self.form.addRow("Time step", self.time_step_spinbox)
         self.time_step_spinbox.setValue(self.device_configuration.time_step)
 
-        self._channels_view = SequencerChannelView(self.device_configuration.channels)
-        self.form.addRow("Channels", self._channels_view)
+        self._channels_widget = SequencerChannelWidget(
+            self.device_configuration.channels, self
+        )
+        self.form.addRow("Channels", self._channels_widget)
 
     def get_configuration(self) -> S:
         config = super().get_configuration()
         config.time_step = self.time_step_spinbox.value()
-        config.channels = self._channels_view.channel_model.channels
+        config.channels = self._channels_widget.get_channel_configurations()
         return config
