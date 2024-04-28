a7b0f2f265b64228defd10fcd6dfa70050f60c31
Dear <lechadoq@gmail.com>
2024-04-28 13:51:59 +0200

    Fix sequence caching

--- caqtus/analysis/loading/sequence_cache.py
+++ caqtus/analysis/loading/sequence_cache.py
@@ -1,14 +1,13 @@
 import datetime
 from typing import Protocol, TypeVar, Optional
 
-from caqtus.session import ExperimentSession, Sequence, PureSequencePath
+from caqtus.session import Sequence, PureSequencePath
 
 T = TypeVar("T", covariant=True)
 
 
 class SequenceFunction(Protocol[T]):
-    def __call__(self, sequence: Sequence, session: ExperimentSession) -> T:
-        ...
+    def __call__(self, sequence: Sequence) -> T: ...
 
 
 # noinspection PyPep8Naming
@@ -17,11 +16,11 @@ class cache_per_sequence:
         self.func = func
         self.cache: dict[PureSequencePath, tuple[Optional[datetime.datetime], T]] = {}
 
-    def __call__(self, sequence: Sequence, session: ExperimentSession) -> T:
+    def __call__(self, sequence: Sequence) -> T:
         path = sequence.path
-        last_start_time = sequence.get_start_time(session)
+        last_start_time = sequence.get_start_time()
         if path not in self.cache:
-            result = self.func(sequence, session)
+            result = self.func(sequence)
             self.cache[path] = (last_start_time, result)
             return result
         else:
@@ -29,7 +28,7 @@ class cache_per_sequence:
             sequence_has_been_started = last_start_time is not None
             sequence_has_been_reset = last_start_time != cached_start_time
             if sequence_has_been_reset or not sequence_has_been_started:
-                result = self.func(sequence, session)
+                result = self.func(sequence)
                 self.cache[path] = (last_start_time, result)
                 return result
             else:
