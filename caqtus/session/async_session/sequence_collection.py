af9f08a3fbab5717b76c0c56c2b4cf58a6d1194c
Dear <lechadoq@gmail.com>
2024-04-28 11:16:42 +0200

    Add methods to get iterations, time lanes and global parameters for a sequence to async experiment session

--- caqtus/session/async_session/sequence_collection.py
+++ caqtus/session/async_session/sequence_collection.py
@@ -3,9 +3,13 @@ from typing import Protocol
 
 from returns.result import Result
 
+from .. import ParameterNamespace
 from ..path import PureSequencePath
 from ..path_hierarchy import PathNotFoundError
+from ..sequence import State
+from ..sequence.iteration_configuration import IterationConfiguration
 from ..sequence_collection import PathIsNotSequenceError, SequenceStats
+from ..shot import TimeLanes
 
 
 class AsyncSequenceCollection(Protocol):
@@ -19,4 +23,24 @@ class AsyncSequenceCollection(Protocol):
     async def get_stats(
         self, path: PureSequencePath
     ) -> Result[SequenceStats, PathNotFoundError | PathIsNotSequenceError]:
+        raise NotImplementedError
+
+    async def get_state(
+        self, path: PureSequencePath
+    ) -> Result[State, PathNotFoundError | PathIsNotSequenceError]:
+
+        return (await self.get_stats(path)).map(lambda stats: stats.state)
+
+    @abc.abstractmethod
+    async def get_iteration_configuration(
+        self, path: PureSequencePath
+    ) -> IterationConfiguration:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def get_time_lanes(self, path: PureSequencePath) -> TimeLanes:
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    async def get_global_parameters(self, path: PureSequencePath) -> ParameterNamespace:
         raise NotImplementedError
