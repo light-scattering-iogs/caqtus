7b75fae103e66d0cb04a0798c9c552644dadc97d
Dear <lechadoq@gmail.com>
2024-05-04 15:28:36 +0200

    Remove unused file

--- caqtus/gui/condetrol/device_configuration_editors/__init__.py
+++ caqtus/gui/condetrol/device_configuration_editors/__init__.py
@@ -1,4 +1,3 @@
-from ._device_configurations_plugin import DeviceConfigurationsPlugin
 from .configurations_editor import DeviceConfigurationsDialog
 from .device_configuration_editor import DeviceConfigurationEditor
 from .sequencer_configuration_editor import SequencerConfigurationEditor
@@ -6,6 +5,5 @@ from .sequencer_configuration_editor import SequencerConfigurationEditor
 __all__ = [
     "DeviceConfigurationEditor",
     "DeviceConfigurationsDialog",
-    "DeviceConfigurationsPlugin",
     "SequencerConfigurationEditor",
 ]
