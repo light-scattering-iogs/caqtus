7270e4f359692d5e0568153bea7615662ed73101
Dear <lechadoq@gmail.com>
2024-05-04 15:27:38 +0200

    Remove unused file

--- caqtus/gui/condetrol/timelanes_editor/__init__.py
+++ caqtus/gui/condetrol/timelanes_editor/__init__.py
@@ -1,20 +1,10 @@
 from .digital_lane_model import DigitalTimeLaneModel
-from .lane_customization import default_time_lanes_plugin, TimeLanesPlugin, LaneFactory
 from .model import TimeLanesModel, TimeLaneModel
-from .time_lanes_editor import (
-    TimeLanesEditor,
-    LaneModelFactory,
-    LaneDelegateFactory,
-)
+from .time_lanes_editor import TimeLanesEditor
 
 __all__ = [
     "TimeLanesEditor",
     "TimeLanesModel",
-    "LaneModelFactory",
     "DigitalTimeLaneModel",
-    "LaneDelegateFactory",
-    "default_time_lanes_plugin",
-    "TimeLanesPlugin",
-    "LaneFactory",
     "TimeLaneModel",
 ]
