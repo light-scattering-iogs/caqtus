58732ebbf47675f3b520a8ca97a332c8178ae5d8
Dear <lechadoq@gmail.com>
2024-05-04 22:27:29 +0200

    Add AsyncSequence

--- caqtus/session/sequence/__init__.py
+++ caqtus/session/sequence/__init__.py
@@ -1,5 +1,6 @@
+from ._async_sequence import AsyncSequence
 from .sequence import Sequence
 from .shot import Shot
 from .state import State
 
-__all__ = ["Sequence", "State", "Shot"]
+__all__ = ["Sequence", "State", "Shot", "AsyncSequence"]
