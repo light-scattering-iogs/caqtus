79028f9001fa1a780d09198b5bd4ba06264153c3
Dear <lechadoq@gmail.com>
2024-05-05 17:50:35 +0200

    Add time lane serializer to caqtus extension

--- caqtus/extension/caqtus_extension/_implementation.py
+++ caqtus/extension/caqtus_extension/_implementation.py
@@ -2,6 +2,7 @@ import attrs
 
 from caqtus.device.configuration.serializer import DeviceConfigJSONSerializer
 from caqtus.gui.condetrol.extension import CondetrolExtension
+from caqtus.session.shot.timelane.serializer import TimeLaneSerializer
 from ._protocol import CaqtusExtensionProtocol
 from ..device_extension import DeviceExtension
 
@@ -12,6 +13,7 @@ class CaqtusExtension(CaqtusExtensionProtocol):
     device_configurations_serializer: DeviceConfigJSONSerializer = attrs.field(
         factory=DeviceConfigJSONSerializer
     )
+    time_lane_serializer: TimeLaneSerializer = attrs.field(factory=TimeLaneSerializer)
 
     def register_device_extension(self, device_extension: DeviceExtension) -> None:
         self.condetrol_extension.device_extension.register_device_configuration_editor(
--- caqtus/extension/caqtus_extension/_protocol.py
+++ caqtus/extension/caqtus_extension/_protocol.py
@@ -2,8 +2,10 @@ from typing import Protocol
 
 from caqtus.device.configuration.serializer import DeviceConfigJSONSerializerProtocol
 from caqtus.gui.condetrol.extension import CondetrolExtensionProtocol
+from caqtus.session.shot.timelane.serializer import TimeLaneSerializerProtocol
 
 
 class CaqtusExtensionProtocol(Protocol):
     condetrol_extension: CondetrolExtensionProtocol
     device_configurations_serializer: DeviceConfigJSONSerializerProtocol
+    time_lane_serializer: TimeLaneSerializerProtocol
