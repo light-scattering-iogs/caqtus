c857a395cf2d689afe71af34a60c22b3ab2d776c
Dear <lechadoq@gmail.com>
2024-05-05 15:26:51 +0200

    Add method to register device extension to caqtus extension

--- caqtus/extension/caqtus_extension/_implementation.py
+++ caqtus/extension/caqtus_extension/_implementation.py
@@ -2,8 +2,17 @@ import attrs
 
 from caqtus.gui.condetrol.extension import CondetrolExtension
 from ._protocol import CaqtusExtensionProtocol
+from ..device_extension import DeviceExtension
 
 
 @attrs.frozen
 class CaqtusExtension(CaqtusExtensionProtocol):
     condetrol_extension: CondetrolExtension = attrs.field(factory=CondetrolExtension)
+
+    def register_device_extension(self, device_extension: DeviceExtension) -> None:
+        self.condetrol_extension.device_extension.register_device_configuration_editor(
+            device_extension.configuration_type, device_extension.editor_type
+        )
+        self.condetrol_extension.device_extension.register_configuration_factory(
+            device_extension.label, device_extension.configuration_factory
+        )
--- caqtus/extension/device_extension/_implementation.py
+++ caqtus/extension/device_extension/_implementation.py
@@ -1,5 +1,22 @@
-from ._protocol import DeviceExtensionProtocol
+from collections.abc import Callable
+from typing import Generic, TypeVar
+
+import attrs
+
+from caqtus.device import DeviceConfiguration
+from caqtus.gui.condetrol.device_configuration_editors import DeviceConfigurationEditor
+
+C = TypeVar("C", bound=DeviceConfiguration)
 
 
-class DeviceExtension(DeviceExtensionProtocol):
-    pass
+@attrs.frozen
+class DeviceExtension(Generic[C]):
+    label: str = attrs.field(converter=str)
+    configuration_type: type[C] = attrs.field()
+    configuration_factory: Callable[[], C] = attrs.field()
+    editor_type: Callable[[C], DeviceConfigurationEditor[C]] = attrs.field()
+
+    @configuration_type.validator  # type: ignore
+    def _validate_configuration_type(self, attribute, value):
+        if not issubclass(value, DeviceConfiguration):
+            raise ValueError(f"Invalid configuration type: {value}.")
