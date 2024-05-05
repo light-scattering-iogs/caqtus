d9abd673276735ee12191b9ab291be45cada8ba0
Dear <lechadoq@gmail.com>
2024-05-05 17:58:19 +0200

    Export time lane extension in __init__.py

--- caqtus/extension/__init__.py
+++ caqtus/extension/__init__.py
@@ -1,4 +1,5 @@
 from .caqtus_extension import CaqtusExtension
 from .device_extension import DeviceExtension
+from .time_lane_extension import TimeLaneExtension
 
-__all__ = ["DeviceExtension", "CaqtusExtension"]
+__all__ = ["DeviceExtension", "CaqtusExtension", "TimeLaneExtension"]
