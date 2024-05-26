5879e65e15bd63a0752b79a94a6cf9b229d43f91
Dear <lechadoq@gmail.com>
2024-05-26 17:20:26 +0200

    Add docstring to shot_compilation module

--- caqtus/shot_compilation/__init__.py
+++ caqtus/shot_compilation/__init__.py
@@ -1,3 +1,8 @@
+"""
+This module contains functions and classes to compile user-friendly sequence
+configurations into low-level device parameters.
+"""
+
 from caqtus.types.units.unit_namespace import units
 from ._device_compiler import DeviceCompiler
 from .compilation_contexts import ShotContext, SequenceContext
