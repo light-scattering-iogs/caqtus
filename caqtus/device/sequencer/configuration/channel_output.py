933d356d4abdec51f1d03638c4f765ec51072cd8
damien <damien.bloch@institutoptique.fr>
2024-06-08 20:46:35 +0200

    Add broaden left channel output instruction

--- caqtus/device/sequencer/configuration/channel_output.py
+++ caqtus/device/sequencer/configuration/channel_output.py
@@ -173,6 +173,31 @@ class Delay:
         return f"{self.delay} >> {self.input_}"
 
 
+@attrs.define
+class BroadenLeft:
+    """Indicates that output should go high before the input pulses go high.
+
+    The output y(t) of this operation should be high when any of the input x(s) is high
+    for s in [t, t + width].
+
+    The operation is only valid for boolean inputs, and it will produce a boolean
+    output.
+
+    It is meant to be used to compensate for finite rise times in the hardware.
+    For example, if a shutter takes 10 ms to open, and we want to open it at time t, we
+    can use this operation to start opening the shutter at time t - 10 ms.
+    """
+
+    input_: ChannelOutput = attrs.field(
+        validator=validate_channel_output,
+        on_setattr=attrs.setters.validate,
+    )
+    width: Expression = attrs.field(
+        validator=attrs.validators.instance_of(Expression),
+        on_setattr=attrs.setters.validate,
+    )
+
+
 def data_points_converter(data_points: Iterable[tuple[float, float]]):
     point_to_tuple = [(x, y) for x, y in data_points]
     return tuple(sorted(point_to_tuple))
@@ -290,7 +315,13 @@ class CalibratedAnalogMapping(TimeIndependentMapping):
 
 
 ChannelOutput = (
-    LaneValues | DeviceTrigger | Constant | Advance | Delay | CalibratedAnalogMapping
+    LaneValues
+    | DeviceTrigger
+    | Constant
+    | Advance
+    | Delay
+    | CalibratedAnalogMapping
+    | BroadenLeft
 )
 
 serialization.register_structure_hook(LaneValues, structure_lane_values)
