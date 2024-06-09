937ca4bb6a33db2a4e615263986b84e56c2610d8
damien <damien.bloch@institutoptique.fr>
2024-06-09 15:38:33 +0200

    Add docstring to evaluate channel output

--- caqtus/device/sequencer/configuration/channel_output.py
+++ caqtus/device/sequencer/configuration/channel_output.py
@@ -37,4 +37,16 @@ class ChannelOutput(abc.ABC):
         append: int,
         shot_context: ShotContext,
     ) -> SequencerInstruction:
+        """Evaluate the output of a channel with the required parameters.
+
+        Args:
+            required_time_step: The time step of the sequencer that will use the
+            output, in ns.
+            required_unit: The unit in which the output should be expressed when
+            evaluated.
+            prepend: The number of time steps to add at the beginning of the output.
+            append: The number of time steps to add at the end of the output.
+            shot_context: The context of the shot in which the output is evaluated.
+        """
+
         raise NotImplementedError
