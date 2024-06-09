ccbc376002546baf4c983e45f2f2a092c0694718
damien <damien.bloch@institutoptique.fr>
2024-06-09 17:51:31 +0200

    Remove caqtus utils run_on_change

--- caqtus/utils/__init__.py
+++ caqtus/utils/__init__.py
@@ -1,13 +1,11 @@
 import attrs as attrs
 
 from ._add_exc_note import add_exc_note
-from ._log_exception import log_exception
-from ._run_on_change_only import run_on_change_method
-from .duration_timer import DurationTimer, DurationTimerLog
 from ._log_duration import log_duration
+from ._log_exception import log_exception
+from .duration_timer import DurationTimer, DurationTimerLog
 
 __all__ = [
-    "run_on_change_method",
     "log_exception",
     "log_duration",
     "DurationTimer",
