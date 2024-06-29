9acc61dfd3184fb5547a15f80089eebac6771661
damien <damien.bloch@institutoptique.fr>
2024-06-29 20:43:37 +0200

    Remove grpc server and client

--- caqtus/device/remote/rpc/_configuration.py
+++ caqtus/device/remote/rpc/_configuration.py
@@ -1,27 +1,4 @@
-from typing import TypeAlias
-
 import attrs
-import grpc
-
-
-@attrs.define
-class LocalRPCCredentials:
-    connection_type: grpc.LocalConnectionType
-
-    def get_channel_credentials(self) -> grpc.ChannelCredentials:
-        return grpc.local_channel_credentials(self.connection_type)
-
-    def get_server_credentials(self) -> grpc.ServerCredentials:
-        return grpc.local_server_credentials(self.connection_type)
-
-
-Credentials: TypeAlias = LocalRPCCredentials
-
-
-@attrs.define
-class SecureRPCConfiguration:
-    target: str = attrs.field(converter=str, on_setattr=attrs.setters.convert)
-    credentials: Credentials = attrs.field()
 
 
 @attrs.define
@@ -29,4 +6,4 @@ class InsecureRPCConfiguration:
     target: str = attrs.field(converter=str, on_setattr=attrs.setters.convert)
 
 
-RPCConfiguration = SecureRPCConfiguration | InsecureRPCConfiguration
+RPCConfiguration = InsecureRPCConfiguration
