a23a3013515120570c793ec60d7ed8a254eb7885
damien <damien.bloch@institutoptique.fr>
2024-06-30 19:21:29 +0200

    Make SequenceInterruptedException a RecoverableException

--- caqtus/experiment_control/sequence_runner/sequence_manager.py
+++ caqtus/experiment_control/sequence_runner/sequence_manager.py
@@ -23,6 +23,7 @@ from .shots_manager import ShotManager, ShotData, ShotScheduler
 from .shots_manager import ShotRetryConfig
 from ..device_manager_extension import DeviceManagerExtensionProtocol
 from ..initialize_resources import create_shot_runner, create_shot_compiler
+from ...types.exceptions import RecoverableException
 
 logger = logging.getLogger(__name__)
 
@@ -184,5 +185,5 @@ class SequenceManager:
             )
 
 
-class SequenceInterruptedException(RuntimeError):
+class SequenceInterruptedException(RecoverableException):
     pass
