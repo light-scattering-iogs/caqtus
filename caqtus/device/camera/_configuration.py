a354d696c9ef8458236f024a83c3fef746d38d2f
damien <damien.bloch@institutoptique.fr>
2024-07-22 22:11:00 +0200

    Simplify camera configuration

--- caqtus/device/camera/_configuration.py
+++ caqtus/device/camera/_configuration.py
@@ -1,27 +1,20 @@
-from typing import TypeVar, Generic
-
 import attrs
 
 from caqtus.utils.roi import RectangularROI
-from ._controller import CameraController
 from ._runtime import Camera
 from ..configuration import DeviceConfiguration
 
-CameraType = TypeVar("CameraType", bound=Camera)
-
 
 @attrs.define
-class CameraConfiguration(DeviceConfiguration[CameraType], Generic[CameraType]):
+class CameraConfiguration[C: Camera](DeviceConfiguration[C]):
     """Contains the necessary information about a camera.
 
     Attributes:
-        roi: The region of interest to keep from the image taken by the camera.
+        roi: The region of interest to keep for the images taken by the camera for all
+            acquisitions.
     """
 
     roi: RectangularROI = attrs.field(
         validator=attrs.validators.instance_of(RectangularROI),
         on_setattr=attrs.setters.validate,
     )
-
-    def get_controller_type(self):
-        return CameraController
