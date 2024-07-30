ec1248282e39a1b29d982a616205fa8aee8189d7
damien <damien.bloch@institutoptique.fr>
2024-07-30 20:14:39 +0200

    Export UnitLike in caqtus.types.units

--- caqtus/types/units/__init__.py
+++ caqtus/types/units/__init__.py
@@ -16,6 +16,7 @@ from .units import (
     CURRENT_UNITS,
     VOLTAGE_UNITS,
     UNITS,
+    UnitLike,
 )
 
 __all__ = [
@@ -35,4 +36,5 @@ __all__ = [
     "VOLTAGE_UNITS",
     "UNITS",
     "units",
+    "UnitLike",
 ]
