0f7168b4b201ef41171403b4bf7ee40ea5a6b1e2
damien <damien.bloch@institutoptique.fr>
2024-07-31 17:41:26 +0200

    Update lane evaluation to use evaluated output

--- caqtus/device/sequencer/channel_commands/_channel_sources/_lane_values.py
+++ caqtus/device/sequencer/channel_commands/_channel_sources/_lane_values.py
@@ -5,23 +5,23 @@ from typing import Optional, Mapping, Any
 import attrs
 from cattrs.gen import make_dict_structure_fn, override
 
+import caqtus.formatter as fmt
 from caqtus.shot_compilation import ShotContext
 from caqtus.types.expression import Expression
+from caqtus.types.recoverable_exceptions import InvalidValueError, InvalidTypeError
 from caqtus.types.timelane import DigitalTimeLane, AnalogTimeLane
-from caqtus.types.units import Unit
 from caqtus.types.variable_name import DottedVariableName
-from caqtus.utils import serialization, add_exc_note
+from caqtus.utils import serialization
 from ._compile_digital_lane import compile_digital_lane
 from ._constant import Constant
 from .compile_analog_lane import compile_analog_lane
-from ..channel_output import ChannelOutput
-from ...instructions import SequencerInstruction, Pattern
+from ..channel_output import ChannelOutput, EvaluatedOutput
+from ...instructions import Pattern
 
 
 @attrs.define
 class LaneValues(ChannelOutput):
     """Indicates that the output should be the values taken by a given lane.
-
 
     Attributes:
         lane: The name of the lane from which to take the values.
@@ -49,11 +49,10 @@ class LaneValues(ChannelOutput):
     def evaluate(
         self,
         required_time_step: int,
-        required_unit: Optional[Unit],
         prepend: int,
         append: int,
         shot_context: ShotContext,
-    ) -> SequencerInstruction:
+    ):
         """Evaluate the output of a channel as the values of a lane.
 
         This function will look in the shot time lanes to find the lane referenced by
@@ -71,36 +70,36 @@ class LaneValues(ChannelOutput):
             if self.default is not None:
                 return self.default.evaluate(
                     required_time_step,
-                    required_unit,
                     prepend,
                     append,
                     shot_context,
                 )
             else:
-                raise ValueError(
-                    f"Could not find lane <{lane_name}> when evaluating output "
-                    f"<{self}>"
-                )
+                raise InvalidValueError(f"Could not find lane {fmt.lane(lane_name)}")
         if isinstance(lane, DigitalTimeLane):
-            if required_unit is not None:
-                raise ValueError(
-                    f"Cannot evaluate digital lane <{lane_name}> with unit "
-                    f"{required_unit:~}"
-                )
-            with add_exc_note(f"When evaluating digital lane <{lane_name}>"):
-                lane_values = compile_digital_lane(
-                    lane, required_time_step, shot_context
-                )
+            lane_values = compile_digital_lane(lane, required_time_step, shot_context)
+            result = EvaluatedOutput(lane_values, units=None)
         elif isinstance(lane, AnalogTimeLane):
-            with add_exc_note(f"When evaluating analog lane <{lane_name}>"):
-                lane_values = compile_analog_lane(
-                    lane, required_unit, required_time_step, shot_context
-                )
+            result = compile_analog_lane(
+                lane,
+                shot_context.get_variables(),
+                shot_context.get_step_start_times(),
+                required_time_step,
+            )
         else:
-            raise TypeError(f"Cannot evaluate values of lane with type {type(lane)}")
-        prepend_pattern = prepend * Pattern([lane_values[0]])
-        append_pattern = append * Pattern([lane_values[-1]])
-        return prepend_pattern + lane_values + append_pattern
+            raise InvalidTypeError(
+                f"Don't know how to compile lane {fmt.lane(lane_name)} with type "
+                f"{fmt.type_(type(lane))}"
+            )
+
+        prepend_value = result.values[0]
+        prepend_pattern = prepend * Pattern([prepend_value])
+        append_value = result.values[-1]
+        append_pattern = append * Pattern([append_value])
+        return EvaluatedOutput(
+            values=prepend_pattern + result.values + append_pattern,
+            units=result.units,
+        )
 
     def evaluate_max_advance_and_delay(
         self,
