5e0b9281f55001aec89a934dbffeedf744d01362
damien <damien.bloch@institutoptique.fr>
2024-07-31 16:04:35 +0200

    Add tests for analog lane compilation

--- tests/test_device/test_sequencer/test_analog_lane_compilation.py
+++ tests/test_device/test_sequencer/test_analog_lane_compilation.py
@@ -1,14 +1,18 @@
 import numpy as np
-import pytest
+from pytest import approx, raises
 
+from caqtus.device.sequencer.channel_commands import compile_analog_lane
 from caqtus.device.sequencer.channel_commands._channel_sources.compile_analog_lane import (
     evaluate_constant_expression,
     evaluate_time_dependent_expression,
     ConstantBlockResult,
     TimeDependentBlockResult,
 )
+from caqtus.device.sequencer.instructions import Pattern
 from caqtus.types.expression import Expression
 from caqtus.types.recoverable_exceptions import InvalidValueError
+from caqtus.types.recoverable_exceptions import RecoverableException
+from caqtus.types.timelane import AnalogTimeLane, Ramp
 from caqtus.types.units import Unit
 
 
@@ -40,7 +44,7 @@ def test_evaluate_constant_expression_3():
     expression = Expression("...")
     variables = {}
     length = 1
-    with pytest.raises(InvalidValueError):
+    with raises(InvalidValueError):
         evaluate_constant_expression(expression, variables, length)
 
 
@@ -87,3 +91,64 @@ def test_evaluate_time_dependent_expression_2():
         initial_value=0,
         final_value=10,
     )
+
+
+def test_logarithmic_expression():
+    lane = AnalogTimeLane([Expression("0 dB"), Expression("10 dB")])
+    result = compile_analog_lane(lane, {}, [0.0, 10e-9, 20e-9], 1)
+    expected = Pattern([1]) * 10 + Pattern([10]) * 10
+    assert result.values == approx(expected)
+    assert result.units is None
+
+
+def test_ramp():
+    lane = AnalogTimeLane([Expression("0"), Ramp(), Expression("10")])
+    result = compile_analog_lane(lane, {}, [0, 0, 4e-9, 4e-9], 1)
+    expected = Pattern(np.linspace(0, 10, 4, endpoint=False))
+
+    assert result.values == approx(expected)
+    assert result.units is None
+
+
+def test_logarithmic_ramp():
+    lane = AnalogTimeLane([Expression("0 dB"), Ramp(), Expression("10 dB")])
+    result = compile_analog_lane(lane, {}, [0, 3e-9, 7e-9, 10e-9], 1)
+    expected = (
+        Pattern([1.0]) * 3
+        + Pattern(np.linspace(1, 10, 4, endpoint=False))
+        + 3 * Pattern([10.0])
+    )
+
+    assert result.values == approx(expected)
+    assert result.units is None
+
+
+def test_non_integer_ramp():
+    lane = AnalogTimeLane([Expression("0"), Ramp(), Expression("1")])
+    result = compile_analog_lane(lane, {}, [0.0, 0.5e-9, 2.5e-9, 4e-9], 1)
+    expected = Pattern([0, 1 / 4, 3 / 4, 1])
+    assert result.values == approx(expected)
+    assert result.units is None
+
+
+def test_expression_with_unit():
+    lane = AnalogTimeLane([Expression("10 Hz"), Expression("1 kHz")])
+    result = compile_analog_lane(lane, {}, [0, 10e-9, 20e-9], 1)
+    expected = Pattern([10.0]) * 10 + Pattern([1000.0]) * 10
+    assert result.values == approx(expected)
+    assert result.units == Unit("1/s")
+
+
+def test_time_dependent_expression():
+    lane = AnalogTimeLane([Expression("t / (10 ns) * 1 Hz")])
+    result = compile_analog_lane(lane, {}, [0, 10e-9], 1)
+    expected = Pattern([0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+
+    assert result.values == approx(expected)
+    assert result.units == Unit("1/s")
+
+
+def test_invalid_expression_cell():
+    lane = AnalogTimeLane([Expression("...")])
+    with raises(RecoverableException):
+        compile_analog_lane(lane, {}, [0, 10e-9], 1)
