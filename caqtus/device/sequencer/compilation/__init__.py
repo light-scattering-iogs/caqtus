daea830c880ac646c9d7b406be43295072d0a61f
damien <damien.bloch@institutoptique.fr>
2024-08-07 21:53:23 +0200

    Add TriggerCompiler protocol

--- caqtus/device/sequencer/compilation/__init__.py
+++ caqtus/device/sequencer/compilation/__init__.py
@@ -1,6 +1,5 @@
-from caqtus.device.sequencer.channel_commands._channel_sources._adaptative_clock import (
-    get_adaptive_clock,
-)
 from ._compiler import SequencerCompiler
+from ._trigger_compiler import TriggerCompiler
+from ..channel_commands._channel_sources._adaptative_clock import get_adaptive_clock
 
-__all__ = ["evaluate_device_trigger", "get_adaptive_clock", "SequencerCompiler"]
+__all__ = ["get_adaptive_clock", "SequencerCompiler", "TriggerCompiler"]
