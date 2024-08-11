8d78e4d1a04cde4855164a62fac2bfa18fc69da5
damien <damien.bloch@institutoptique.fr>
2024-08-11 20:10:17 +0200

    Add check for sequence duration in integration test for sequencer

--- tests/test_device/test_sequencer/test_integration.py
+++ tests/test_device/test_sequencer/test_integration.py
@@ -1,6 +1,7 @@
 from caqtus.device import DeviceName
 from caqtus.device.sequencer import SequencerCompiler
 from caqtus.shot_compilation import SequenceContext, ShotContext
+from caqtus.shot_compilation.lane_compilers.timing import number_ticks, ns
 from .fixtures import (
     time_lanes,
     spincore_config,
@@ -27,6 +28,27 @@ def test_single_digital_lane(
     }
 
     shot_context = ShotContext(sequence_context, variables, compilers)
-    compilers["Spincore"].compile_shot_parameters(shot_context)
-    compilers["Swabian pulse streamer"].compile_shot_parameters(shot_context)
-    compilers["NI6738"].compile_shot_parameters(shot_context)
+    spincore_sequence = compilers["Spincore"].compile_shot_parameters(shot_context)[
+        "sequence"
+    ]
+    swabian_sequence = compilers["Swabian pulse streamer"].compile_shot_parameters(
+        shot_context
+    )["sequence"]
+    ni6738_sequence = compilers["NI6738"].compile_shot_parameters(shot_context)[
+        "sequence"
+    ]
+
+    shot_duration = shot_context.get_shot_duration()
+
+    assert len(spincore_sequence) == number_ticks(
+        0, shot_duration, spincore_config.time_step * ns
+    )
+    assert len(swabian_sequence) == number_ticks(
+        0.0,
+        shot_duration
+        + 1.1e-6,  # Need to tak into account time shift when computing shot duration
+        swabian_configuration.time_step * ns,
+    )
+    assert len(ni6738_sequence) == number_ticks(
+        0, shot_duration, ni6738_configuration.time_step * ns
+    )
