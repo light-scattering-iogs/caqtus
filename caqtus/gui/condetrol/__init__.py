6d0c6d07767288963e7a490099344d64aab9d15a
damien <damien.bloch@institutoptique.fr>
2024-08-13 19:10:52 +0200

    Make default method to connect to experiment manager private

--- caqtus/gui/condetrol/__init__.py
+++ caqtus/gui/condetrol/__init__.py
@@ -1,7 +1,1 @@
 """Contains the main GUI from which user can edit and launch sequences."""
-
-from ._condetrol import default_connect_to_experiment_manager
-
-__all__ = [
-    "default_connect_to_experiment_manager",
-]
