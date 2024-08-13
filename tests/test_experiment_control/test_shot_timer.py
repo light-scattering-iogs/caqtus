f8c7b256f74fb63bdb539b14e88828383fb4833b
damien <damien.bloch@institutoptique.fr>
2024-08-13 22:39:11 +0200

    Add test for shot timer

---
+++ tests/test_experiment_control/test_shot_timer.py
@@ -0,0 +1,35 @@
+import pytest
+
+from caqtus.experiment_control.shot_timing import ShotTimer
+import trio.testing
+
+
+def test_elapsed():
+    async def main():
+        timer = ShotTimer._create()
+        assert timer.elapsed() == pytest.approx(0, abs=1e-3)
+        await trio.sleep(1)
+        assert timer.elapsed() == pytest.approx(1, rel=1e-3)
+
+    trio.run(main, clock=trio.testing.MockClock(autojump_threshold=0))
+
+
+def test_positive_wait():
+    async def main():
+        timer = ShotTimer._create()
+        duration = await timer.wait_until(1)
+        assert duration == pytest.approx(1, rel=1e-3)
+        assert timer.elapsed() == pytest.approx(1, rel=1e-3)
+
+    trio.run(main, clock=trio.testing.MockClock(autojump_threshold=0))
+
+
+def test_negative_wait():
+    async def main():
+        timer = ShotTimer._create()
+        await trio.sleep(1)
+        duration = await timer.wait_until(0.5)
+        assert duration == pytest.approx(-0.5, rel=1e-3)
+        assert timer.elapsed() == pytest.approx(1, rel=1e-3)
+
+    trio.run(main, clock=trio.testing.MockClock(autojump_threshold=0))
