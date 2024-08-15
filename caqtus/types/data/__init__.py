e2a89cba9209239adbf9a6b125a440aa134e585b
damien <damien.bloch@institutoptique.fr>
2024-08-15 22:52:54 +0200

    Make data subpackages private

--- caqtus/types/data/__init__.py
+++ caqtus/types/data/__init__.py
@@ -1,7 +1,7 @@
 """This module define the type of data that can be saved for a shot."""
 
-from .data_label import DataLabel, is_data_label
+from ._data_label import DataLabel, is_data_label
 from ._data_type import Data, Array, StructuredData
-from .is_data import is_data
+from ._is_data import is_data
 
 __all__ = ["Data", "Array", "StructuredData", "is_data", "DataLabel", "is_data_label"]
