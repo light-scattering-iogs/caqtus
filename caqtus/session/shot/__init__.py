3e7cbcf17b05b2b3b83ab077fd92ffa0e226cfff
damien <damien.bloch@institutoptique.fr>
2024-08-15 14:36:47 +0200

    Stop exporting AnalogTimeLanes in session.shot

--- caqtus/session/shot/__init__.py
+++ caqtus/session/shot/__init__.py
@@ -1,14 +1,10 @@
 from caqtus.types.timelane import (
-    AnalogTimeLane,
-    Ramp,
     CameraTimeLane,
     TakePicture,
 )
 from ..sequence.shot import Shot
 
 __all__ = [
-    "AnalogTimeLane",
-    "Ramp",
     "CameraTimeLane",
     "TakePicture",
     "Shot",
