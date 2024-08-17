0db7a286241d214803631372cd833bab02b66f05
damien <damien.bloch@institutoptique.fr>
2024-08-17 11:10:21 +0200

    Only import matplotlib when needed

--- caqtus/device/sequencer/instructions/_plot.py
+++ caqtus/device/sequencer/instructions/_plot.py
@@ -1,16 +1,18 @@
-from typing import Optional
-
-import matplotlib.pyplot as plt
-from matplotlib.axes import Axes
+from typing import Optional, TYPE_CHECKING
 
 from ._instructions import SequencerInstruction
 from ._to_time_array import convert_to_change_arrays
 
+if TYPE_CHECKING:
+    from matplotlib.axes import Axes
+
 
 def plot_instruction(
-    instruction: SequencerInstruction, ax: Optional[Axes] = None
-) -> Axes:
+    instruction: SequencerInstruction, ax: Optional["Axes"] = None
+) -> "Axes":
     """Plot the instruction on the given axis."""
+
+    import matplotlib.pyplot as plt
 
     if ax is None:
         fig, ax = plt.subplots()
