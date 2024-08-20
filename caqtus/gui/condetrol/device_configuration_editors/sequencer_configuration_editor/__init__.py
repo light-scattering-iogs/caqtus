4e670647ce6e482dbaf0d60ae93de2b2a470d04a
Dear <lechadoq@gmail.com>
2024-08-20 14:09:09 +0200

    Export ChannelOutputEditor

--- caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/__init__.py
+++ caqtus/gui/condetrol/device_configuration_editors/sequencer_configuration_editor/__init__.py
@@ -3,10 +3,12 @@
 from ._trigger_selector import TriggerSelector
 from .channels_widget import SequencerChannelWidget
 from .sequencer_configuration_editor import SequencerConfigurationEditor, TimeStepEditor
+from .channel_output_editor import ChannelOutputEditor
 
 __all__ = [
     "SequencerConfigurationEditor",
     "TimeStepEditor",
     "SequencerChannelWidget",
     "TriggerSelector",
+    "ChannelOutputEditor",
 ]
