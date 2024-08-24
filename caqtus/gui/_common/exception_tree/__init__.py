0574cb741fc6bb9b163f62370cfe3347065c122b
damien <damien.bloch@institutoptique.fr>
2024-08-24 21:53:17 +0200

    Stop exporting function to create exception tree

--- caqtus/gui/_common/exception_tree/__init__.py
+++ caqtus/gui/_common/exception_tree/__init__.py
@@ -1,4 +1,3 @@
-from ._exception_tree import create_exception_tree
 from .exception_dialog import ExceptionDialog
 
-__all__ = ["create_exception_tree", "ExceptionDialog"]
+__all__ = ["ExceptionDialog"]
