7a8da4270efa24edb9011707fce10d2f2db1b862
damien <damien.bloch@institutoptique.fr>
2024-08-25 12:32:25 +0200

    Reduce depth for path test

--- tests/test_session/generate_path.py
+++ tests/test_session/generate_path.py
@@ -4,5 +4,5 @@ from caqtus.session import PureSequencePath
 from caqtus.session._path import _PATH_NAME_REGEX
 
 path_name = hypothesis.strategies.from_regex(_PATH_NAME_REGEX)
-path_parts = hypothesis.strategies.lists(path_name, min_size=0)
+path_parts = hypothesis.strategies.lists(path_name, min_size=0, max_size=5)
 path = hypothesis.strategies.builds(PureSequencePath.from_parts, path_parts)
