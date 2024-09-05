352e6fe1ce793c70f19befb4a1579d2e7ea3ff8b
damien <damien.bloch@institutoptique.fr>
2024-09-05 23:12:01 +0200

    Make timing functions take a decimal as argument

--- caqtus/shot_compilation/lane_compilers/timing.py
+++ caqtus/shot_compilation/lane_compilers/timing.py
@@ -2,28 +2,32 @@
 
 import decimal
 import math
-from collections.abc import Iterable
+from collections.abc import Iterable, Sequence
 from itertools import accumulate
-from typing import SupportsFloat
+from typing import NewType
+
+Time = NewType("Time", decimal.Decimal)
+"""A type for representing time in seconds.
+
+It uses a decimal.Decimal to represent time in seconds to avoid floating point errors.
+"""
 
 ns = decimal.Decimal("1e-9")
 
 
-def start_tick(start_time: SupportsFloat, time_step: SupportsFloat) -> int:
+def start_tick(start_time: Time, time_step: Time) -> int:
     """Returns the included first tick index of the step starting at start_time."""
 
-    return math.ceil(float(start_time) / float(time_step))
+    return math.ceil(start_time / time_step)
 
 
-def stop_tick(stop_time: SupportsFloat, time_step: SupportsFloat) -> int:
+def stop_tick(stop_time: Time, time_step: Time) -> int:
     """Returns the excluded last tick index of the step ending at stop_time."""
 
-    return math.ceil(float(stop_time) / float(time_step))
+    return math.ceil(stop_time / time_step)
 
 
-def number_ticks(
-    start_time: SupportsFloat, stop_time: SupportsFloat, time_step: SupportsFloat
-) -> int:
+def number_ticks(start_time: Time, stop_time: Time, time_step: Time) -> int:
     """Returns the number of ticks between start_time and stop_time.
 
     Args:
@@ -35,7 +39,7 @@ def number_ticks(
     return stop_tick(stop_time, time_step) - start_tick(start_time, time_step)
 
 
-def get_step_bounds(step_durations: Iterable[float]) -> list[float]:
+def get_step_bounds(step_durations: Iterable[Time]) -> Sequence[Time]:
     """Returns the time at which each step starts from their durations.
 
     For an iterable of step durations [d_0, d_1, ..., d_n], the step starts are
@@ -43,4 +47,5 @@ def get_step_bounds(step_durations: Iterable[float]) -> list[float]:
     iterable of step durations, with the last element being the total duration.
     """
 
-    return [0.0] + list((accumulate(step_durations)))
+    zero = Time(decimal.Decimal(0))
+    return [zero] + list((accumulate(step_durations)))
