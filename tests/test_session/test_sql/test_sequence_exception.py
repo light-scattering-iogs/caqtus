80dda2b0593307453fd5b10b2594f0e35d966f30
damien <damien.bloch@institutoptique.fr>
2024-09-07 21:00:42 +0200

    Fix test for sequence exception

--- tests/test_session/test_sql/test_sequence_exception.py
+++ tests/test_session/test_sql/test_sequence_exception.py
@@ -1,18 +1,17 @@
 import pytest
 
 from caqtus.session import SequenceNotCrashedError, TracebackSummary, State
-from caqtus.session._return_or_raise import unwrap
 
 
 def test_get_exception_not_crashed(session_maker, draft_sequence):
     with session_maker.session() as session:
         with pytest.raises(SequenceNotCrashedError):
-            unwrap(session.sequences.get_exception(draft_sequence))
+            session.sequences.get_exception(draft_sequence).unwrap()
 
 
 def test_get_exception_crashed(session_maker, crashed_sequence):
     with session_maker.session() as session:
-        assert unwrap(session.sequences.get_exception(crashed_sequence)) is None
+        assert session.sequences.get_exception(crashed_sequence).unwrap() is None
 
 
 def test_set_exception_crashed(session_maker, crashed_sequence):
@@ -20,9 +19,9 @@ def test_set_exception_crashed(session_maker, crashed_sequence):
 
     tb = TracebackSummary.from_exception(error)
     with session_maker.session() as session:
-        unwrap(session.sequences.set_exception(crashed_sequence, tb))
+        session.sequences.set_exception(crashed_sequence, tb).unwrap()
     with session_maker.session() as session:
-        assert unwrap(session.sequences.get_exception(crashed_sequence)) == tb
+        assert session.sequences.get_exception(crashed_sequence).unwrap() == tb
 
 
 def test_set_exception_after_reset(session_maker, crashed_sequence):
@@ -30,7 +29,7 @@ def test_set_exception_after_reset(session_maker, crashed_sequence):
 
     tb = TracebackSummary.from_exception(error)
     with session_maker.session() as session:
-        unwrap(session.sequences.set_exception(crashed_sequence, tb))
+        session.sequences.set_exception(crashed_sequence, tb).unwrap()
 
     with session_maker.session() as session:
         session.sequences.set_state(crashed_sequence, State.DRAFT)
@@ -39,5 +38,5 @@ def test_set_exception_after_reset(session_maker, crashed_sequence):
         session.sequences.set_state(crashed_sequence, State.CRASHED)
 
     with session_maker.session() as session:
-        assert unwrap(session.sequences.get_exception(crashed_sequence)) is None
-        unwrap(session.sequences.set_exception(crashed_sequence, tb))
+        assert session.sequences.get_exception(crashed_sequence).unwrap() is None
+        session.sequences.set_exception(crashed_sequence, tb).unwrap()
