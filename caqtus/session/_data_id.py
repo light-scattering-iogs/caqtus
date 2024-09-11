12762e5d2b85614b14bd8a236368f22036431ba5
damien <damien.bloch@institutoptique.fr>
2024-09-11 21:05:15 +0200

    Add data id class

---
+++ caqtus/session/_data_id.py
@@ -0,0 +1,19 @@
+import attrs
+
+from caqtus.session._sequence_collection import PureShot
+from caqtus.types.data import DataLabel
+
+
+@attrs.frozen
+class DataId:
+    """Represents a unique identifier for data in the session.
+
+    This is used to identify data in the session and is unique for each data.
+
+    Attributes:
+        shot_id: The shot to which the data belongs.
+        data_label: The label of the data.
+    """
+
+    shot_id: PureShot
+    data_label: DataLabel
