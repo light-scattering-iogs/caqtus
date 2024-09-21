8c573b3ef63600d5f50001742af4c3166b051702
damien <damien.bloch@institutoptique.fr>
2024-09-21 13:45:41 +0200

    Add caqtus specific classes for quantity and unit to support magnitude type hint

--- caqtus/types/units/_units.py
+++ caqtus/types/units/_units.py
@@ -1,15 +1,42 @@
 import importlib.resources
-from typing import TYPE_CHECKING
 
 import pint._typing
+import pint.facets
+import pint.facets.nonmultiplicative.objects
+import pint.facets.numpy.quantity
+import pint.facets.numpy.unit
 
 from caqtus.types.recoverable_exceptions import InvalidValueError
+
+
+class Quantity[M: pint._typing.Magnitude](
+    pint.facets.system.objects.SystemQuantity[M],
+    pint.facets.numpy.quantity.NumpyQuantity[M],
+    pint.facets.nonmultiplicative.objects.NonMultiplicativeQuantity[M],
+    pint.facets.plain.PlainQuantity[M],
+):
+    pass
+
+
+class Unit(
+    pint.facets.SystemRegistry.Unit,
+    pint.facets.numpy.unit.NumpyUnit,
+    pint.facets.nonmultiplicative.objects.NonMultiplicativeUnit,
+    pint.facets.plain.PlainUnit,
+):
+    pass
+
+
+class UnitRegistry(pint.UnitRegistry):
+    Quantity = Quantity  # pyright: ignore[reportAssignmentType]
+    Unit = Unit  # pyright: ignore[reportAssignmentType]
+
 
 units_definition_file = importlib.resources.files("caqtus.types.units").joinpath(
     "units_definition.txt"
 )
 
-ureg = pint.UnitRegistry(
+ureg = UnitRegistry(
     str(units_definition_file),
     autoconvert_offset_to_baseunit=True,
     cache_folder=":auto:",
@@ -17,14 +44,6 @@ ureg = pint.UnitRegistry(
 unit_registry = ureg
 pint.set_application_registry(unit_registry)
 
-if TYPE_CHECKING:
-    from pint.facets.plain import PlainQuantity, PlainUnit
-
-    Quantity = PlainQuantity
-    Unit = PlainUnit
-else:
-    Quantity = pint.Quantity
-    Unit = pint.Unit
 
 UnitLike = pint._typing.UnitLike
 UndefinedUnitError = pint.UndefinedUnitError
