0bbf61f797cb5b4086813a49923ff70ea4d245f9
damien <damien.bloch@institutoptique.fr>
2024-09-24 23:15:56 +0200

    Export shot compilation timing module

--- caqtus/shot_compilation/__init__.py
+++ caqtus/shot_compilation/__init__.py
@@ -1,7 +1,7 @@
 """Used to compile user-friendly parameters into low-level device parameters."""
 
 from caqtus.types.units.unit_namespace import units
-from . import lane_compilation, timed_instructions
+from . import lane_compilation, timed_instructions, timing
 from ._device_compiler import DeviceCompiler, DeviceNotUsedException
 from .compilation_contexts import ShotContext, SequenceContext
 from .variable_namespace import VariableNamespace
@@ -15,4 +15,5 @@ __all__ = [
     "DeviceNotUsedException",
     "lane_compilation",
     "timed_instructions",
+    "timing",
 ]
