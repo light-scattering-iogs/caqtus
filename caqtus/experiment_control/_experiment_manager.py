ecd9c2fb593b68751929df748feaced87d0e8789
damien <damien.bloch@institutoptique.fr>
2024-09-25 22:18:17 +0200

    Define experiment manager

---
+++ caqtus/experiment_control/_experiment_manager.py
@@ -0,0 +1,14 @@
+from collections.abc import Mapping
+
+from ._procedures import Procedure, ProcedureName
+
+
+class ExperimentManager:
+    """Controls an experiment and schedule procedures to run on the setup.
+
+    There should only be at most one experiment manager per setup since it is in charge
+    of ensuring that only once procedure can run at a time.
+    """
+
+    def __init__(self, available_procedures: Mapping[ProcedureName, Procedure]):
+        self._current_procedure = None
