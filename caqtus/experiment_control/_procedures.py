6f19b5396abd438bc27e8d3c3e0f687aed95c0c2
damien <damien.bloch@institutoptique.fr>
2024-09-25 22:18:06 +0200

    Define run_sequence procedure

--- caqtus/experiment_control/_procedures.py
+++ caqtus/experiment_control/_procedures.py
@@ -25,3 +25,14 @@ class Procedure(Protocol):
         """
 
         ...
+
+
+async def run_sequence(kernel: ExperimentKernel, sequence_path: str) -> None:
+    """Run a sequence of instructions on the setup.
+
+    Args:
+        kernel: The experiment kernel that gives access to the setup.
+        sequence_path: The path to the sequence file.
+    """
+
+    raise NotImplementedError
