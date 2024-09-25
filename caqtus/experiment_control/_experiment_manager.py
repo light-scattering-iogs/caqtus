fd5e09f772dea80c569897b11b97227c5213281f
damien <damien.bloch@institutoptique.fr>
2024-09-25 23:41:25 +0200

    Add running procedure to experiment manager

--- caqtus/experiment_control/_experiment_manager.py
+++ caqtus/experiment_control/_experiment_manager.py
@@ -1,5 +1,10 @@
 from collections.abc import Mapping
+from typing import Optional
 
+import anyio
+import attrs
+
+from caqtus.utils.serialization import JSON
 from .procedures import Procedure, ProcedureName
 
 
@@ -8,7 +13,29 @@ class ExperimentManager:
 
     There should only be at most one experiment manager per setup since it is in charge
     of ensuring that only once procedure can run at a time.
+
+    Args:
+        available_procedures: The procedures that can be run on the setup.
+            Only procedures that are listed here can be selected to run.
     """
 
     def __init__(self, available_procedures: Mapping[ProcedureName, Procedure]):
-        self._current_procedure = None
+        self._available_procedures = available_procedures
+        self._currently_running_procedure: Optional[RunningProcedure] = None
+
+
+@attrs.frozen
+class RunningProcedure:
+    """Represents a procedure that is currently running on the setup.
+
+    Attributes:
+        procedure_name: The name of the procedure that is running.
+        args: The arguments that were passed to the procedure.
+        kwargs: The keyword arguments that were passed to the procedure.
+    """
+
+    procedure_name: ProcedureName = attrs.field()
+    args: tuple[JSON, ...] = attrs.field()
+    kwargs: dict[str, JSON] = attrs.field()
+
+    _cancel_scope: anyio.CancelScope = attrs.field()
