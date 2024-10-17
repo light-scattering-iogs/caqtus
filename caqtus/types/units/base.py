45e27ac2eb5d14453810d043ec07ac49f3d60a73
Dear <lechadoq@gmail.com>
2024-10-17 17:23:38 +0200

    Improve type hints for base units

--- caqtus/types/units/base.py
+++ caqtus/types/units/base.py
@@ -1,11 +1,16 @@
-from typing import overload, Optional, Any
+from typing import overload, Optional, Any, NewType
 
 import numpy as np
+from typing_extensions import TypeIs
 
-from ._units import Unit, Quantity, dimensionless
+from ._units import Unit, Quantity, dimensionless, FloatArray
+
+BaseUnit = NewType("BaseUnit", Unit)
+ScalarBaseQuantity = NewType("ScalarBaseQuantity", Quantity[float])
+ArrayBaseQuantity = NewType("ArrayBaseQuantity", Quantity[FloatArray])
 
 
-def base_units(units: Unit) -> Unit:
+def base_units(units: Unit) -> BaseUnit:
     """Return the base units of the given units.
 
     Args:
@@ -15,12 +20,10 @@ def base_units(units: Unit) -> Unit:
         The base units of the given units.
     """
 
-    return (
-        Quantity(1.0, units).to_base_units().units
-    )  # pyright: ignore[reportReturnType]
+    return BaseUnit(Quantity(1.0, units).to_base_units().units)
 
 
-def is_in_base_units(units: Unit) -> bool:
+def is_in_base_units(units: Unit) -> TypeIs[BaseUnit]:
     """Check if the units is only expressed in terms of base SI units.
 
     For example, `kg`, `m/s` are expressed in terms of base units, but `mg`, `km/h` or
@@ -37,39 +40,50 @@ def is_in_base_units(units: Unit) -> bool:
 
 
 @overload
-def convert_to_base_units(magnitude: float, units: None) -> tuple[float, None]: ...
+def convert_to_base_units(magnitude: float, unit: None) -> tuple[float, None]: ...
 
 
 @overload
 def convert_to_base_units[
     A: np.ndarray[Any, np.dtype[np.floating]]
-](magnitude: A, units: None) -> tuple[A, None]: ...
+](magnitude: A, unit: None) -> tuple[A, None]: ...
 
 
 @overload
 def convert_to_base_units(
-    magnitude: float, units: Unit
-) -> tuple[float, Optional[Unit]]: ...
+    magnitude: float, unit: Unit
+) -> tuple[float, Optional[BaseUnit]]: ...
 
 
 @overload
 def convert_to_base_units[
     A: np.ndarray[Any, np.dtype[np.floating]]
-](magnitude: A, units: Unit) -> tuple[A, Optional[Unit]]: ...
+](magnitude: A, unit: Unit) -> tuple[A, Optional[BaseUnit]]: ...
 
 
-def convert_to_base_units(  # pyright: ignore[reportInconsistentOverload]
-    magnitude, units
+def convert_to_base_units(
+    magnitude: float | np.ndarray[Any, np.dtype[np.floating]], unit: Optional[Unit]
 ):
-    if units is None:
+    """Convert values into base units.
+
+    Args:
+        magnitude: The value to convert.
+            Can be a scalar or an array of values.
+        unit: The unit in which the magnitude is expressed.
+
+    Returns:
+        The magnitude in base units and the base units.
+    """
+
+    if unit is None:
         return magnitude, None
     else:
-        quantity = Quantity(magnitude, units)
+        quantity = Quantity(magnitude, unit)
         in_base_units = quantity.to_base_units()
         magnitude_in_base_units = in_base_units.magnitude
         base_units = in_base_units.units
         if base_units == dimensionless:
             base_units = None
         else:
-            assert is_in_base_units(base_units)  # pyright: ignore[reportArgumentType]
+            assert is_in_base_units(base_units)
         return magnitude_in_base_units, base_units
