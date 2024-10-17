1b219969e98e4bb6d82acff0db6fa46e1fbac630
Dear <lechadoq@gmail.com>
2024-10-17 17:36:19 +0200

    Ensure that the output of transform evaluation is in base units

--- caqtus/device/output_transform/_transformation.py
+++ caqtus/device/output_transform/_transformation.py
@@ -2,15 +2,15 @@ from __future__ import annotations
 
 import abc
 from collections.abc import Mapping
-from typing import Any, TypeAlias, Union, assert_never
+from typing import Any, TypeAlias, Union, assert_never, assert_type
 
 import attrs
 
 import caqtus.formatter as fmt
 from caqtus.types.expression import Expression
 from caqtus.types.recoverable_exceptions import InvalidTypeError
-from caqtus.types.units import Quantity, is_scalar_quantity
-from caqtus.types.units.base import is_in_base_units
+from caqtus.types.units import is_scalar_quantity, is_quantity
+from caqtus.types.units.base import ScalarBaseQuantity, is_base_quantity, to_base_units
 from caqtus.types.variable_name import DottedVariableName
 
 
@@ -28,8 +28,11 @@ class Transformation(abc.ABC):
         raise NotImplementedError
 
 
-type OutputValue = float | Quantity[float] | int | bool
-"""A value that can be used to compute the output of a device."""
+type OutputValue = float | int | bool | ScalarBaseQuantity
+"""A value that can be used to compute the output of a device.
+
+If the value is a quantity, it must be a scalar and expressed in base units.
+"""
 
 EvaluableOutput: TypeAlias = Union[Expression, Transformation]
 """Defines an operation that can be evaluated to an output value.
@@ -48,9 +51,10 @@ def evaluate(
 
     if isinstance(input_, Transformation):
         evaluated = input_.evaluate(variables)
-        if is_scalar_quantity(evaluated):
-            assert is_in_base_units(evaluated.units)
-            return evaluated.to_base_units()
+        if is_quantity(evaluated):
+            assert_type(evaluated, ScalarBaseQuantity)
+            assert is_scalar_quantity(evaluated) and is_base_quantity(evaluated)
+            return evaluated
         else:
             return evaluated
     elif isinstance(input_, Expression):
@@ -59,7 +63,7 @@ def evaluate(
         if isinstance(evaluated, (float, int, bool)):
             return evaluated
         elif is_scalar_quantity(evaluated):
-            return evaluated.to_base_units()
+            return to_base_units(evaluated)
         else:
             raise InvalidTypeError(
                 f"{fmt.expression(input_)} does not evaluate to a parameter, "
