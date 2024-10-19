0a25741d3d8bd323ff46ddb09c2027e82beeb28b
damien <damien.bloch@institutoptique.fr>
2024-10-19 15:48:22 +0200

    Remove unit compatible class

--- caqtus/types/units/_unit_list.py
+++ caqtus/types/units/_unit_list.py
@@ -3,7 +3,7 @@ from typing import NewType
 from ._units import Unit, BaseUnit
 
 Second = NewType("Second", BaseUnit)
-SECOND = Second(BaseUnit(Unit("s")))
+SECOND = Second(Unit("s").to_base())
 NANOSECOND = Unit("ns")
 
 HERTZ = Unit("Hz")
