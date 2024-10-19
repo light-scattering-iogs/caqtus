ac596496d4eb9e9efba2101d4335365d3ad2f273
damien <damien.bloch@institutoptique.fr>
2024-10-19 21:57:13 +0200

    Fix type hint for device serialization implementation

--- caqtus/device/configuration/serializer/_implementation.py
+++ caqtus/device/configuration/serializer/_implementation.py
@@ -1,19 +1,24 @@
 from collections.abc import Callable
-from typing import TypeVar
 
 from caqtus.utils.serialization import JSON
 from ._protocol import DeviceConfigJSONSerializerProtocol
 from .._configuration import DeviceConfiguration
 
-C = TypeVar("C", bound=DeviceConfiguration)
-
 
 class DeviceConfigJSONSerializer(DeviceConfigJSONSerializerProtocol):
+    """Handles serialization of device configurations.
+
+    For each device configuration type to handle, a dumper and a constructor function
+    must be registered.
+    """
+
     def __init__(self):
         self.loaders: dict[str, Callable[[JSON], DeviceConfiguration]] = {}
         self.dumpers: dict[str, Callable[[DeviceConfiguration], JSON]] = {}
 
-    def register_device_configuration(
+    def register_device_configuration[
+        C: DeviceConfiguration
+    ](
         self,
         config_type: type[C],
         dumper: Callable[[C], JSON],
@@ -32,7 +37,9 @@ class DeviceConfigJSONSerializer(DeviceConfigJSONSerializerProtocol):
 
         type_name = config_type.__qualname__
 
-        self.dumpers[type_name] = dumper
+        # We need to transform the dumper into a function that can handle any device
+        # configuration type, not just the one it was registered for.
+        self.dumpers[type_name] = wrap_dumper(config_type, dumper)
         self.loaders[type_name] = constructor
 
     def dump_device_configuration(
@@ -45,3 +52,16 @@ class DeviceConfigJSONSerializer(DeviceConfigJSONSerializerProtocol):
     def load_device_configuration(self, tag: str, content: JSON) -> DeviceConfiguration:
         constructor = self.loaders[tag]
         return constructor(content)
+
+
+class wrap_dumper[C: DeviceConfiguration]:  # noqa: N801
+    """Make a dumper function accept all device configuration types."""
+
+    def __init__(self, config_type: type[C], dumper: Callable[[C], JSON]):
+        self.dumper = dumper
+        self.config_type = config_type
+
+    def __call__(self, config: DeviceConfiguration) -> JSON:
+        if not isinstance(config, self.config_type):
+            raise TypeError(f"Expected {self.config_type}, got {type(config)}")
+        return self.dumper(config)
