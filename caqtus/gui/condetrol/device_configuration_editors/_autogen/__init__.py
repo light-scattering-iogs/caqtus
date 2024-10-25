eb40b6dcb82cb98c663b0974415d8fc7f1939aee
damien <damien.bloch@institutoptique.fr>
2024-10-25 14:17:56 +0200

    Add editor for integer values

--- caqtus/gui/condetrol/device_configuration_editors/_autogen/__init__.py
+++ caqtus/gui/condetrol/device_configuration_editors/_autogen/__init__.py
@@ -1,7 +1,14 @@
 """Automatically generates editors for device configurations."""
 
 from ._editor_builder import EditorBuilder, TypeNotRegisteredError
+from ._int_editor import IntegerEditor
 from ._string_editor import StringEditor
 from ._value_editor import ValueEditor
 
-__all__ = ["EditorBuilder", "StringEditor", "ValueEditor", "TypeNotRegisteredError"]
+__all__ = [
+    "EditorBuilder",
+    "IntegerEditor",
+    "StringEditor",
+    "ValueEditor",
+    "TypeNotRegisteredError",
+]
