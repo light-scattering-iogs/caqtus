489ef3b0d5ef82d189d75d68cc3833d66ac3a805
damien <damien.bloch@institutoptique.fr>
2024-10-26 12:47:28 +0200

    Add base editor builder

--- caqtus/gui/condetrol/device_configuration_editors/_autogen/__init__.py
+++ caqtus/gui/condetrol/device_configuration_editors/_autogen/__init__.py
@@ -1,6 +1,6 @@
 """Automatically generates editors for device configurations."""
 
-from ._device_config_editor import build_device_configuration_editor
+from ._device_config_editor import build_device_configuration_editor, get_editor_builder
 from ._editor_builder import EditorBuilder, TypeNotRegisteredError
 from ._int_editor import IntegerEditor
 from ._string_editor import StringEditor
@@ -8,6 +8,7 @@ from ._value_editor import ValueEditor
 
 __all__ = [
     "build_device_configuration_editor",
+    "get_editor_builder",
     "EditorBuilder",
     "IntegerEditor",
     "StringEditor",
--- caqtus/gui/condetrol/device_configuration_editors/_autogen/_device_config_editor.py
+++ caqtus/gui/condetrol/device_configuration_editors/_autogen/_device_config_editor.py
@@ -1,9 +1,11 @@
+import copy
 import functools
 from typing import Optional, Protocol
 
-from PySide6.QtWidgets import QWidget, QLayout
+from PySide6.QtWidgets import QWidget, QLineEdit, QVBoxLayout
 
 from caqtus.device import DeviceConfiguration
+from caqtus.device.configuration import DeviceServerName
 from caqtus.gui.condetrol.device_configuration_editors import DeviceConfigurationEditor
 from ._editor_builder import EditorBuilder
 from ._value_editor import ValueEditor
@@ -18,21 +20,21 @@ class GeneratedConfigEditor[C: DeviceConfiguration](DeviceConfigurationEditor[C]
         config_editor_type: type[ValueEditor[C]],
     ) -> None:
         super().__init__(parent)
-        self.editor = config_editor_type(config, self)
-        layout = QLayout()
+        self._editor = config_editor_type(config, self)
+        layout = QVBoxLayout()
         layout.setContentsMargins(0, 0, 0, 0)
-        layout.addWidget(self.editor.widget())
+        layout.addWidget(self._editor.widget())
         self.setLayout(layout)
 
     # TODO: Understand why need to silence pyright
     def get_configuration(self) -> C:  # type: ignore[reportIncompatibleMethodOverride]
-        return self.editor.read_value()
+        return self._editor.read_value()
 
 
 class DeviceConfigurationEditorType[C: DeviceConfiguration](Protocol):
     def __call__(
         self, config: C, parent: Optional[QWidget] = None
-    ) -> DeviceConfigurationEditor[C]: ...
+    ) -> GeneratedConfigEditor[C]: ...
 
 
 def build_device_configuration_editor[
@@ -53,3 +55,45 @@ def build_device_configuration_editor[
     return functools.partial(
         GeneratedConfigEditor, config_editor_type=config_editor_type
     )
+
+
+_builder = EditorBuilder()
+
+
+class DeviceServerNameEditor(ValueEditor[Optional[DeviceServerName]]):
+    def __init__(
+        self, value: Optional[DeviceServerName], parent: Optional[QWidget] = None
+    ) -> None:
+        self.line_edit = QLineEdit(parent)
+        self.line_edit.setPlaceholderText("None")
+        if value:
+            self.line_edit.setText(value)
+        else:
+            self.line_edit.setText("")
+
+    def read_value(self) -> Optional[DeviceServerName]:
+        text = self.line_edit.text()
+        if text:
+            return DeviceServerName(text)
+        else:
+            return None
+
+    def set_editable(self, editable: bool) -> None:
+        self.line_edit.setReadOnly(not editable)
+
+    def widget(self) -> QLineEdit:
+        return self.line_edit
+
+
+_builder.register_editor(Optional[DeviceServerName], DeviceServerNameEditor)
+
+
+def get_editor_builder() -> EditorBuilder:
+    """Return a new editor builder with basic types registered.
+
+    The editor builder returned knows how to handle:
+
+        - Device server name
+    """
+
+    return copy.deepcopy(_builder)
