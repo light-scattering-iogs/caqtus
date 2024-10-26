e718b6dc0d577df43d26deb910fa5668b7229b67
damien <damien.bloch@institutoptique.fr>
2024-10-26 11:30:20 +0200

    Use externally defined class for device configuration editor

--- caqtus/gui/condetrol/device_configuration_editors/_autogen/_device_config_editor.py
+++ caqtus/gui/condetrol/device_configuration_editors/_autogen/_device_config_editor.py
@@ -1,15 +1,43 @@
-from typing import Optional
+import functools
+from typing import Optional, Protocol
 
 from PySide6.QtWidgets import QWidget, QLayout
 
 from caqtus.device import DeviceConfiguration
 from caqtus.gui.condetrol.device_configuration_editors import DeviceConfigurationEditor
 from ._editor_builder import EditorBuilder
+from ._value_editor import ValueEditor
+
+
+class GeneratedConfigEditor[C: DeviceConfiguration](DeviceConfigurationEditor[C]):
+    def __init__(
+        self,
+        config: C,
+        parent: Optional[QWidget] = None,
+        *,
+        config_editor_type: type[ValueEditor[C]],
+    ) -> None:
+        super().__init__(parent)
+        self.editor = config_editor_type(config, self)
+        layout = QLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.editor.widget())
+        self.setLayout(layout)
+
+    # TODO: Understand why need to silence pyright
+    def get_configuration(self) -> C:  # type: ignore[reportIncompatibleMethodOverride]
+        return self.editor.read_value()
+
+
+class DeviceConfigurationEditorType[C: DeviceConfiguration](Protocol):
+    def __call__(
+        self, config: C, parent: Optional[QWidget] = None
+    ) -> DeviceConfigurationEditor[C]: ...
 
 
 def build_device_configuration_editor[
     C: DeviceConfiguration
-](config_type: type[C], builder: EditorBuilder) -> type[DeviceConfigurationEditor[C]]:
+](config_type: type[C], builder: EditorBuilder) -> DeviceConfigurationEditorType[C]:
     """Builds a device configuration editor for the given configuration type.
 
     Args:
@@ -22,18 +50,6 @@ def build_device_configuration_editor[
     """
 
     config_editor_type = builder.build_editor_for_type(config_type)
-
-    class GeneratedConfigEditor(DeviceConfigurationEditor[C]):
-        def __init__(self, config: C, parent: Optional[QWidget] = None):
-            super().__init__(parent)
-            self.editor = config_editor_type(config, self)
-            layout = QLayout()
-            layout.setContentsMargins(0, 0, 0, 0)
-            layout.addWidget(self.editor.widget())
-            self.setLayout(layout)
-
-        # TODO: Understand why need to silence pyright
-        def get_configuration(self) -> C:  # type: ignore[reportIncompatibleMethodOverride]
-            return self.editor.read_value()
-
-    return GeneratedConfigEditor
+    return functools.partial(
+        GeneratedConfigEditor, config_editor_type=config_editor_type
+    )
