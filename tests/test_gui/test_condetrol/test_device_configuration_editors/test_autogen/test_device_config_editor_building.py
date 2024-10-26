a1cc1f4aac7f66ea440e27ca80029fa6c3a36f8b
damien <damien.bloch@institutoptique.fr>
2024-10-26 12:51:03 +0200

    Add test for editing remote server

--- tests/test_gui/test_condetrol/test_device_configuration_editors/test_autogen/test_device_config_editor_building.py
+++ tests/test_gui/test_condetrol/test_device_configuration_editors/test_autogen/test_device_config_editor_building.py
@@ -1,19 +1,20 @@
 from pytestqt.qtbot import QtBot
 
 from caqtus.device import DeviceConfiguration
+from caqtus.device.configuration import DeviceServerName
 from caqtus.gui.condetrol.device_configuration_editors._autogen import (
-    EditorBuilder,
     build_device_configuration_editor,
+    get_editor_builder,
 )
 
 
-def test_simplest_class(qtbot: QtBot):
+def test_remote_server(qtbot: QtBot):
     class DeviceConfigA(DeviceConfiguration):
         pass
 
-    value = DeviceConfigA(remote_server=None)
+    value = DeviceConfigA(remote_server=DeviceServerName("James"))
 
-    builder = EditorBuilder()
+    builder = get_editor_builder()
     device_config_editor_type = build_device_configuration_editor(
         DeviceConfigA, builder
     )
@@ -21,3 +22,5 @@ def test_simplest_class(qtbot: QtBot):
     editor = device_config_editor_type(value)
     qtbot.add_widget(editor)
     editor.show()
+    editor._editor.editor_remote_server.widget().setText("")  # type: ignore
+    assert editor.get_configuration().remote_server is None
