a21aa75d0b5611c08c8df0cc45dcbbf3ea96cd91
damien <damien.bloch@institutoptique.fr>
2024-10-27 22:11:32 +0100

    Add suffix and prefix arguments to integer editor

--- caqtus/gui/autogen/_int_editor.py
+++ caqtus/gui/autogen/_int_editor.py
@@ -4,12 +4,23 @@ from ._value_editor import ValueEditor
 
 
 class IntegerEditor(ValueEditor[int]):
+    """An editor to display an integer.
+
+    Args:
+        min_value: The lowest value (inclusive) that can be entered.
+        max_value: The largest value (inclusive) that can be entered.
+        prefix: A string to be displayed before the number.
+        suffix: A string to be displayed after the number.
+    """
+
     def __init__(
         self,
         value: int,
         *,
         min_value=0,
         max_value=999,
+        prefix: str = "",
+        suffix: str = "",
     ) -> None:
         self.spin_box = QSpinBox()
         self.spin_box.setRange(min_value, max_value)
@@ -18,6 +29,10 @@ class IntegerEditor(ValueEditor[int]):
                 f"Value {value} is outside the editor range [{min_value}, {max_value}]"
             )
         self.spin_box.setValue(value)
+        if prefix:
+            self.spin_box.setPrefix(prefix)
+        if suffix:
+            self.spin_box.setSuffix(suffix)
 
     def read_value(self) -> int:
         return self.spin_box.value()
