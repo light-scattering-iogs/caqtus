cf37b2eb3e2eb3120ddeeddbceecba4dd0e086c6
damien <damien.bloch@institutoptique.fr>
2024-10-27 22:07:37 +0100

    Add placeholder argument to string editor

--- caqtus/gui/autogen/_string_editor.py
+++ caqtus/gui/autogen/_string_editor.py
@@ -1,14 +1,24 @@
-from typing import Optional
-
-from PySide6.QtWidgets import QLineEdit, QWidget
+from PySide6.QtWidgets import QLineEdit
 
 from ._value_editor import ValueEditor
 
 
 class StringEditor(ValueEditor[str]):
-    def __init__(self, value: str, parent: Optional[QWidget] = None) -> None:
-        self.line_edit = QLineEdit(parent)
+    """An editor to display a string.
+
+    Args:
+        value: The initial value to edit.
+            Once initialized, the value can only be changed by the user through the
+            widget.
+        placeholder: The text to display when the editor is empty.
+    """
+
+    def __init__(self, value: str, placeholder: str = "") -> None:
+        self.line_edit = QLineEdit()
         self.line_edit.setText(value)
+
+        if placeholder:
+            self.line_edit.setPlaceholderText(placeholder)
 
     def read_value(self) -> str:
         return self.line_edit.text()
