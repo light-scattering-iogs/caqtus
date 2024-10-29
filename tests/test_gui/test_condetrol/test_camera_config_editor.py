e38416b2fdb7f57cbc7095b5337cc7b6cf9d1ea7
damien <damien.bloch@institutoptique.fr>
2024-10-29 20:32:02 +0100

    Fix test when editing roi

--- tests/test_gui/test_condetrol/test_camera_config_editor.py
+++ tests/test_gui/test_condetrol/test_camera_config_editor.py
@@ -1,34 +1,37 @@
 from caqtus.device.camera import CameraConfiguration
-from caqtus.gui.condetrol.device_configuration_editors.camera_configuration_editor import (
+from caqtus.device.configuration import DeviceServerName
+from caqtus.gui.condetrol.device_configuration_editors.camera_configuration_editor import (  # noqa: E501
     RectangularROIEditor,
     CameraConfigurationEditor,
 )
+from caqtus.types.image import Width, Height
 from caqtus.types.image.roi import RectangularROI
 
 
 def test_1(qtbot):
-    roi = RectangularROI((100, 100), 0, 100, 0, 100)
+    roi = RectangularROI((Width(100), Height(100)), 0, 100, 0, 100)
     editor = RectangularROIEditor(max_width=100, max_height=100)
     editor.set_roi(roi)
     editor.show()
     qtbot.addWidget(editor)
 
     editor._x_spinbox.setValue(1)
-
     new_roi = editor.get_roi()
 
-    assert new_roi == roi
+    assert new_roi.x == 1
+    assert new_roi.width == 99
 
 
 def test_2(qtbot):
     camera_config = CameraConfiguration(
-        remote_server=None, roi=RectangularROI((100, 100), 0, 100, 0, 100)
+        remote_server=None,
+        roi=RectangularROI((Width(100), Height(100)), 0, 100, 0, 100),
     )
     editor = CameraConfigurationEditor(camera_config)
     editor.show()
     qtbot.addWidget(editor)
 
-    editor.set_remote_server("test")
+    editor.set_remote_server(DeviceServerName("test"))
 
     new_config = editor.get_configuration()
 
