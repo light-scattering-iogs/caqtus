e266e9157df9be7d20096bff9cde29858bd8683e
damien <damien.bloch@institutoptique.fr>
2024-11-04 16:39:13 +0100

    Export caqtus.gui.timelanes_editor

--- caqtus/gui/condetrol/timelanes_editor/__init__.py
+++ caqtus/gui/condetrol/timelanes_editor/__init__.py
@@ -1,3 +1,13 @@
+"""Provides a widget for editing time lanes.
+
+The main class is :class:`TimeLanesEditor` which is a widget that displays multiple
+time lanes, along with the time steps, in a table-like view.
+
+To know how to display and edit a specific type of lane, the editor uses
+the :class:`extension.CondetrolLaneExtensionProtocol` to know how to build a
+:class:`TimeLaneModel` and a :class:`TimeLaneDelegate` for each lane.
+"""
+
 from . import digital_lane_editor, analog_lane_editor, camera_lane_editor, extension
 from ._delegate import TimeLaneDelegate
 from ._time_lanes_editor import TimeLanesEditor
