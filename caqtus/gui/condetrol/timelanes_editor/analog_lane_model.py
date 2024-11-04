3d33ed1363d6e1644b7af7e07dd147f460ce4938
damien <damien.bloch@institutoptique.fr>
2024-11-04 16:19:09 +0100

    Fix warnings for analog lane model

--- caqtus/gui/condetrol/timelanes_editor/analog_lane_model.py
+++ caqtus/gui/condetrol/timelanes_editor/analog_lane_model.py
@@ -1,22 +1,23 @@
 from typing import Optional, Any, assert_never
 
-from PySide6.QtCore import QObject, QModelIndex, Qt
+from PySide6.QtCore import QObject, QModelIndex, Qt, QPersistentModelIndex
 from PySide6.QtGui import QAction
 from PySide6.QtWidgets import QMenu
 
-from caqtus.types.timelane import AnalogTimeLane, Ramp
 from caqtus.types.expression import Expression
+from caqtus.types.timelane import AnalogTimeLane, Ramp, Step
 from .model import ColoredTimeLaneModel
 
+_DEFAULT_INDEX = QModelIndex()
 
-class AnalogTimeLaneModel(ColoredTimeLaneModel[AnalogTimeLane, None]):
+
+class AnalogTimeLaneModel(ColoredTimeLaneModel[AnalogTimeLane]):
+    # ruff: noqa: N802
     def __init__(self, name: str, parent: Optional[QObject] = None):
         lane = AnalogTimeLane([Expression("...")])
         super().__init__(name, lane, parent)
 
-    def data(
-        self, index: QModelIndex, role: Qt.ItemDataRole = Qt.ItemDataRole.DisplayRole
-    ):
+    def data(self, index, role: int = Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
         value = self._lane[index.row()]
@@ -36,7 +37,7 @@ class AnalogTimeLaneModel(ColoredTimeLaneModel[AnalogTimeLane, None]):
         else:
             return super().data(index, role)
 
-    def flags(self, index: QModelIndex) -> Qt.ItemFlag:
+    def flags(self, index) -> Qt.ItemFlag:
         if not index.isValid():
             return Qt.ItemFlag.NoItemFlags
         flags = super().flags(index)
@@ -44,13 +45,11 @@ class AnalogTimeLaneModel(ColoredTimeLaneModel[AnalogTimeLane, None]):
             flags &= ~Qt.ItemFlag.ItemIsEditable
         return flags
 
-    def setData(
-        self, index: QModelIndex, value: Any, role: int = Qt.ItemDataRole.EditRole
-    ):
+    def setData(self, index, value: Any, role: int = Qt.ItemDataRole.EditRole):
         if not index.isValid():
             return False
         if role == Qt.ItemDataRole.EditRole:
-            start, stop = self._lane.get_bounds(index.row())
+            start, stop = self._lane.get_bounds(Step(index.row()))
             if isinstance(value, str):
                 self._lane[start:stop] = Expression(value)
                 self.dataChanged.emit(index, index)
@@ -63,7 +62,9 @@ class AnalogTimeLaneModel(ColoredTimeLaneModel[AnalogTimeLane, None]):
                 raise TypeError(f"Invalid type for value: {type(value)}")
         return False
 
-    def insertRow(self, row, parent: QModelIndex = QModelIndex()) -> bool:
+    def insertRow(
+        self, row, parent: QModelIndex | QPersistentModelIndex = _DEFAULT_INDEX
+    ) -> bool:
         return self.insert_value(row, Expression("..."))
 
     def get_cell_context_actions(self, index: QModelIndex) -> list[QAction | QMenu]:
