f74400982fbfa8017aaa7a7d423380f292bcdafe
damien <damien.bloch@institutoptique.fr>
2024-11-14 18:04:43 +0100

    Transform time dependent evaluation result into sum type

--- caqtus/shot_compilation/_evaluation/_time_dependent_expression/_result.py
+++ caqtus/shot_compilation/_evaluation/_time_dependent_expression/_result.py
@@ -2,30 +2,42 @@ import attrs
 import numpy as np
 
 from caqtus.shot_compilation.timed_instructions import TimedInstruction
-from caqtus.types.units import Unit, BaseUnit
+from caqtus.types.units import BaseUnit
 from caqtus.types.units.base import is_in_base_units
 
 
 @attrs.frozen
-class EvaluationResult[T: (np.number, np.bool_)]:
-    """Represents a series of value to output on a channel with their units.
+class IntResult:
+    values: TimedInstruction[np.int64]
+    initial_value: int = attrs.field()
+    final_value: int = attrs.field()
 
-    Parameters:
-        values: The sequence of values to output.
-        units: The units in which the values are expressed.
-            They must be in base SI units.
-    """
 
-    values: TimedInstruction[T]
-    units: BaseUnit = attrs.field()
-    initial_value: T = attrs.field()
-    final_value: T = attrs.field()
+@attrs.frozen
+class FloatResult:
+    values: TimedInstruction[np.float64]
+    initial_value: float = attrs.field()
+    final_value: float = attrs.field()
 
-    @units.validator  # type: ignore[reportAttributeAccessIssue]
-    def _validate_units(self, _, units):
-        if not isinstance(units, Unit):
-            raise TypeError(f"Expected a unit, got {type(units)}")
-        if not is_in_base_units(units):
-            raise ValueError(
-                f"Unit {units} is not expressed in the base units of the registry."
-            )
+
+@attrs.frozen
+class BoolResult:
+    values: TimedInstruction[np.bool]
+    initial_value: bool = attrs.field()
+    final_value: bool = attrs.field()
+
+
+@attrs.frozen
+class QuantityResult:
+    values: TimedInstruction[np.float64]
+    initial_value: float = attrs.field()
+    final_value: float = attrs.field()
+    unit: BaseUnit = attrs.field()
+
+    @unit.validator
+    def _validate_unit(self, attribute, value):
+        if not is_in_base_units(value):
+            raise AssertionError(f"Unit {value} is not in base units.")
+
+
+type EvaluationResult = IntResult | FloatResult | BoolResult | QuantityResult
