66fc3c2d3e0d63c48011fe37aa5804a45c91998d
damien <damien.bloch@institutoptique.fr>
2024-11-14 17:12:10 +0100

    Remove unused import

--- caqtus/types/units/unit_namespace.py
+++ caqtus/types/units/unit_namespace.py
@@ -1,6 +1,5 @@
 from collections.abc import Mapping
 
 from ._units import UNITS, ureg, Unit
-from ..variable_name import VariableName
 
 units: Mapping[str, Unit] = {unit: getattr(ureg, unit) for unit in UNITS}
