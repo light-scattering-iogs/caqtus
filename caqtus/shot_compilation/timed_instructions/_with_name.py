75294052fab2b4c73120be10d03b1180a1474b62
damien <damien.bloch@institutoptique.fr>
2024-11-16 13:29:33 +0100

    Fix type hint for with name

--- caqtus/shot_compilation/timed_instructions/_with_name.py
+++ caqtus/shot_compilation/timed_instructions/_with_name.py
@@ -1,9 +1,11 @@
 import numpy as np
 
-from ._instructions import TimedInstruction
+from ._instructions import TimedInstruction, LT
 
 
-def with_name(instruction: TimedInstruction, name: str) -> TimedInstruction[np.void]:
+def with_name(
+    instruction: TimedInstruction[LT, np.generic], name: str
+) -> TimedInstruction[LT, np.void]:
     """
     Change the dtype of the instruction into a structured array with a single field
     with the given name and the same dtype as the original instruction.
