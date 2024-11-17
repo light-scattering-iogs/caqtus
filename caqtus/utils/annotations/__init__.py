11fa024ccd11109574f2fbae2adf2293ade5f257
damien <damien.bloch@institutoptique.fr>
2024-11-17 15:18:26 +0100

    Add caqtus.utils.annotations

---
+++ caqtus/utils/annotations/__init__.py
@@ -0,0 +1,13 @@
+"""Contains type annotations that can be used to document the code.
+
+The annotations defined here are only actually consumed when using the module
+:mod:`caqtus.gui.autogen`.
+"""
+
+import annotated_types
+
+doc = annotated_types.doc
+
+__all__ = [
+    "doc",
+]
