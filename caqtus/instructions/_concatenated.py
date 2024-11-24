637b30f378a01e942c187c7603670e72e3312c89
damien <damien.bloch@institutoptique.fr>
2024-11-24 12:03:41 +0100

    Add dunder methods to concatenated

--- caqtus/instructions/_concatenated.py
+++ caqtus/instructions/_concatenated.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+
 from typing import Generic
 
 import numpy as np
@@ -15,14 +16,19 @@ RightInstrT = TypeVar(
 
 
 class Concatenated(Generic[LeftInstrT, RightInstrT]):
-    """The concatenation of two instructions."""
+    """The concatenation of two instructions.
 
-    __slots__ = ("_left", "_right")
+    Use the `+` operator to create an instance of this class, do not instantiate it
+    directly.
+    """
+
+    __slots__ = ("_left", "_right", "_length")
     __match_args__ = ("left", "right")
 
     def __init__(self, left: LeftInstrT, right: RightInstrT) -> None:
         self._left = left
         self._right = right
+        self._length = len(left) + len(right)
 
     @property
     def left(self) -> LeftInstrT:
@@ -35,6 +41,15 @@ class Concatenated(Generic[LeftInstrT, RightInstrT]):
         """The right instruction."""
 
         return self._right
+
+    def __repr__(self) -> str:
+        return f"Concatenated({self._left!r}, {self._right!r})"
+
+    def __str__(self) -> str:
+        return f"({self._left} + {self._right})"
+
+    def __len__(self) -> int:
+        return self._length
 
     def dtype(self: Concatenated[HasDType[DataT], HasDType[DataT]]) -> np.dtype[DataT]:
         return self._left.dtype()
