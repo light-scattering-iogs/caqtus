cf4fafbb948a338ebb7522808808deed2c661f75
damien <damien.bloch@institutoptique.fr>
2024-11-24 19:31:06 +0100

    Add arithmetic protocols

--- caqtus/instructions/_typing.py
+++ caqtus/instructions/_typing.py
@@ -19,6 +19,29 @@ SubInstrT = TypeVar(
     "SubInstrT", bound=SubInstruction, covariant=True, default=SubInstruction
 )
 
+T = TypeVar("T", contravariant=True, bound=SubInstruction, default=SubInstruction)
+R = TypeVar("R", covariant=True, bound=SubInstruction, default=SubInstruction)
 
-class Addable[T, R](SubInstruction, Protocol):
-    def __add__(self, other: T) -> R: ...
+InstrT_co = TypeVar(
+    "InstrT_co", bound=SubInstruction, covariant=True, default=SubInstruction
+)
+InstrT_contra = TypeVar(
+    "InstrT_contra", bound=SubInstruction, contravariant=True, default=SubInstruction
+)
+InstrT_inv = TypeVar(
+    "InstrT_inv",
+    bound=SubInstruction,
+    contravariant=False,
+    covariant=False,
+    default=SubInstruction,
+)
+
+
+class Addable(SubInstruction, Protocol[InstrT_contra, InstrT_co]):
+    def __add__(self, other: InstrT_contra) -> InstrT_co: ...
+
+
+class Multipliable(SubInstruction, Protocol[InstrT_co]):
+    def __mul__(self, other: int) -> InstrT_co: ...
+
+    def __rmul__(self, other: int) -> InstrT_co: ...
