f67fc763ef44d2352cc87254a3c0e5b4be3af377
damien <damien.bloch@institutoptique.fr>
2024-11-24 19:44:34 +0100

    Remove length and get item for empty

--- caqtus/instructions/_empty.py
+++ caqtus/instructions/_empty.py
@@ -1,12 +1,9 @@
-from typing import Literal, Self, Never
+from typing import Self
 
 
 class Empty:
     def __eq__(self, other):
         return isinstance(other, Empty)
-
-    def __len__(self) -> Literal[0]:
-        return 0
 
     def __repr__(self) -> str:
         return "Empty()"
@@ -18,7 +15,4 @@ class Empty:
         return self
 
     def __rmul__(self, other: int) -> Self:
-        return self
-
-    def __getitem__(self, item: slice) -> Self:
         return self
