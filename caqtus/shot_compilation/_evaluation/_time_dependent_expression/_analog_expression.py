704f3bfd4f207470c287951834d2a41b458ad3cd
damien <damien.bloch@institutoptique.fr>
2024-12-01 18:09:55 +0100

    Start to implement evaluation of time dependent analog expression

--- caqtus/shot_compilation/_evaluation/_time_dependent_expression/_analog_expression.py
+++ caqtus/shot_compilation/_evaluation/_time_dependent_expression/_analog_expression.py
@@ -1,11 +1,19 @@
-from typing import assert_type
+import functools
+from typing import assert_type, assert_never
 
 import attrs
 import numpy as np
 
 import caqtus_parsing.nodes as nodes
-from caqtus.shot_compilation.timed_instructions import TimedInstruction, Pattern
-from caqtus.shot_compilation.timing import Time, number_ticks
+from caqtus.shot_compilation.timed_instructions import (
+    TimedInstruction,
+    Pattern,
+    create_ramp,
+    Concatenated,
+    Repeated,
+    Ramp,
+)
+from caqtus.shot_compilation.timing import Time, number_ticks, start_tick, stop_tick
 from caqtus.types.parameter import Parameters
 from caqtus.types.units import BaseUnit, dimensionless, Quantity
 from ._is_time_dependent import is_time_dependent
@@ -37,4 +45,73 @@ def evaluate_analog_ast(
                 units=in_base_units.units,
             )
     else:
-        raise NotImplementedError
+        match ast:
+            case int() | float() | nodes.Quantity():
+                raise AssertionError("Unreachable")
+            case nodes.Variable(name=name):
+                if name != "t":
+                    raise AssertionError("Unreachable")
+                tick_start = start_tick(t1, timestep)
+                tick_stop = stop_tick(t2, timestep)
+                value_start = tick_start * timestep - t1
+                value_stop = tick_stop * timestep - t1
+                length = tick_stop - tick_start
+                return AnalogInstruction(
+                    magnitudes=create_ramp(value_start, value_stop, length),
+                    units=dimensionless,
+                )
+            case nodes.Call():
+                raise NotImplementedError("Function calls are not supported.")
+            case nodes.Plus() | nodes.Minus() as unary_operator:
+                return evaluate_unary_operator(
+                    unary_operator, parameters, t1, t2, timestep
+                )
+            case _:
+                assert_never(ast)
+
+
+def evaluate_unary_operator(
+    unary_operator: nodes.UnaryOperator,
+    parameters: Parameters,
+    t1: Time,
+    t2: Time,
+    timestep: Time,
+) -> AnalogInstruction:
+    operand = evaluate_analog_ast(unary_operator.operand, parameters, t1, t2, timestep)
+    match unary_operator:
+        case nodes.Plus():
+            return operand
+        case nodes.Minus():
+            return AnalogInstruction(
+                magnitudes=negate(operand.magnitudes),
+                units=operand.units,
+            )
+        case _:
+            assert_never(unary_operator)
+
+
+@functools.singledispatch
+def negate(instruction) -> TimedInstruction[np.float64]:
+    raise NotImplementedError(f"Negation of {type(instruction)} is not supported.")
+
+
+@negate.register(Pattern)
+def negate_pattern(pattern: Pattern[np.float64]) -> Pattern[np.float64]:
+    return Pattern.create_without_copy(-pattern.array)
+
+
+@negate.register(Concatenated)
+def negate_concatenated(
+    concatenated: Concatenated[np.float64],
+) -> Concatenated[np.float64]:
+    return Concatenated(*(negate(instr) for instr in concatenated.instructions))
+
+
+@negate.register(Repeated)
+def negate_repeated(repeated: Repeated[np.float64]) -> Repeated[np.float64]:
+    return Repeated(repeated.repetitions, negate(repeated.instruction))
+
+
+@negate.register(Ramp)
+def negate_ramp(ramp: Ramp[np.float64]) -> TimedInstruction[np.float64]:
+    return create_ramp(-ramp.start, -ramp.stop, len(ramp))
