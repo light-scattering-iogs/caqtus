4a181ecdf105195c9b94ee277810be91091def5a
damien <damien.bloch@institutoptique.fr>
2024-12-26 10:21:46 +0100

    Fix warnings in tests

--- tests/test_session/test_timelane.py
+++ tests/test_session/test_timelane.py
@@ -1,4 +1,4 @@
-from caqtus.types.timelane import DigitalTimeLane
+from caqtus.types.timelane import DigitalTimeLane, Span
 
 
 def test_0():
@@ -31,40 +31,42 @@ def test_2():
 
 
 def test_3():
-    lane = DigitalTimeLane.from_spanned_values([(True, 2), (False, 1), (True, 3)])
+    lane = DigitalTimeLane.from_spanned_values(
+        [(True, Span(2)), (False, Span(1)), (True, Span(3))]
+    )
 
     lane[0] = False
     assert lane == [False, True, False, True, True, True], repr(lane)
 
 
 def test_4():
-    lane = DigitalTimeLane.from_spanned_values([(True, 1)])
+    lane = DigitalTimeLane.from_spanned_values([(True, Span(1))])
 
     lane.insert(1, False)
     assert lane == [True, False], repr(lane)
 
 
 def test_5():
-    lane = DigitalTimeLane.from_spanned_values([(True, 5)])
+    lane = DigitalTimeLane.from_spanned_values([(True, Span(5))])
 
     lane[0:2] = False
     assert lane == [False, False, True, True, True], repr(lane)
 
-    lane = DigitalTimeLane.from_spanned_values([(True, 5)])
+    lane = DigitalTimeLane.from_spanned_values([(True, Span(5))])
 
     lane[3:5] = False
     assert lane == [True, True, True, False, False], repr(lane)
 
 
 def test_6():
-    lane = DigitalTimeLane.from_spanned_values([(True, 5)])
+    lane = DigitalTimeLane.from_spanned_values([(True, Span(5))])
 
     lane[1:3] = False
     assert lane == [True, False, False, True, True], repr(lane)
 
 
 def test_7():
-    lane = DigitalTimeLane.from_spanned_values([(True, 3), (False, 2)])
+    lane = DigitalTimeLane.from_spanned_values([(True, Span(3)), (False, Span(2))])
 
     lane[1:4] = False
     assert lane == [True, False, False, False, False], repr(lane)
