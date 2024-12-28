0dd8fc94d4395573c19484c9d5f77494b0c2f90d
damien <damien.bloch@institutoptique.fr>
2024-12-28 18:16:57 +0100

    Fix issue with units instantiation

--- caqtus/types/units/unit_namespace.py
+++ caqtus/types/units/unit_namespace.py
@@ -1,5 +1,7 @@
 from collections.abc import Mapping
 
-from ._units import UNITS, Unit
+from ._units import UNITS, Unit, ureg
 
-units: Mapping[str, Unit] = {unit: Unit(unit) for unit in UNITS}
+# TODO: Some tests fail when trying to replace `getattr(ureg, unit)` with `Unit(unit)`.
+#  To check.
+units: Mapping[str, Unit] = {unit: getattr(ureg, unit) for unit in UNITS}
