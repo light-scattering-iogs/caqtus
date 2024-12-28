e48d4a2b5b89a5e28461b80f3b648fc4910872be
damien <damien.bloch@institutoptique.fr>
2024-12-28 16:13:31 +0100

    Add tests for parameter schema for steps iteration

--- tests/test_types/test_iteration/test_steps_iteration.py
+++ tests/test_types/test_iteration/test_steps_iteration.py
@@ -5,7 +5,10 @@ from caqtus.types.iteration import (
     ExecuteShot,
     ArangeLoop,
     is_unknown,
+    VariableDeclaration,
 )
+from caqtus.types.parameter import ParameterSchema
+from caqtus.types.units import Unit
 from caqtus.types.variable_name import DottedVariableName
 from tests.fixtures.steps_iteration import steps_configuration
 
@@ -65,3 +68,69 @@ def test_parameter_names():
         ]
     )
     assert steps.get_parameter_names() == {DottedVariableName("a")}
+
+
+def test_parameter_schema_no_constants():
+    steps = StepsConfiguration(
+        steps=[
+            LinspaceLoop(
+                variable=DottedVariableName("a"),
+                start=Expression("0 MHz"),
+                stop=Expression("10 kHz"),
+                num=10,
+                sub_steps=[
+                    ExecuteShot(),
+                ],
+            ),
+        ]
+    )
+    assert steps.get_parameter_schema({}) == ParameterSchema(
+        _constant_schema={},
+        _variable_schema={
+            DottedVariableName("a"): ParameterSchema.Quantity(Unit("MHz"))
+        },
+    )
+
+
+def test_parameter_schema_constants():
+    steps = StepsConfiguration(
+        steps=[
+            VariableDeclaration(DottedVariableName("var"), Expression("2 * a")),
+            LinspaceLoop(
+                variable=DottedVariableName("b"),
+                start=Expression("0"),
+                stop=Expression("1"),
+                num=10,
+                sub_steps=[
+                    ExecuteShot(),
+                ],
+            ),
+        ]
+    )
+    assert steps.get_parameter_schema({DottedVariableName("a"): 0}) == ParameterSchema(
+        _constant_schema={DottedVariableName("a"): 0},
+        _variable_schema={
+            DottedVariableName("var"): ParameterSchema.Integer(),
+            DottedVariableName("b"): ParameterSchema.Float(),
+        },
+    )
+
+
+def test_parameter_schema_constant_redefinition():
+    steps = StepsConfiguration(
+        steps=[
+            LinspaceLoop(
+                variable=DottedVariableName("a"),
+                start=Expression("0"),
+                stop=Expression("1"),
+                num=10,
+                sub_steps=[
+                    ExecuteShot(),
+                ],
+            ),
+        ]
+    )
+    assert steps.get_parameter_schema({DottedVariableName("a"): 0}) == ParameterSchema(
+        _constant_schema={},
+        _variable_schema={DottedVariableName("a"): ParameterSchema.Float()},
+    )
