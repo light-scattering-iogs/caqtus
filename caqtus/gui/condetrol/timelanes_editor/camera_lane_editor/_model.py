648bd135e1aadaeba6f48d9cdec1e988edc38589
damien <damien.bloch@institutoptique.fr>
2025-01-26 14:41:04 +0100

    Remove private attribute access for camera lane model

--- caqtus/gui/condetrol/timelanes_editor/camera_lane_editor/_model.py
+++ caqtus/gui/condetrol/timelanes_editor/camera_lane_editor/_model.py
@@ -1,12 +1,12 @@
 from typing import Optional, Any, assert_never
 
 from PySide6.QtCore import QObject, QModelIndex, Qt, QPersistentModelIndex
-from PySide6.QtGui import QPalette
+from PySide6.QtGui import QPalette, QUndoStack
 
 from caqtus.gui.condetrol._icons import get_icon
 from caqtus.types.data import DataLabel
 from caqtus.types.image import ImageLabel
-from caqtus.types.timelane import CameraTimeLane, TakePicture, Step
+from caqtus.types.timelane import CameraTimeLane, TakePicture
 from .._time_lane_model import TimeLaneModel
 
 _DEFAULT_INDEX = QModelIndex()
@@ -14,9 +14,11 @@ _DEFAULT_INDEX = QModelIndex()
 
 class CameraTimeLaneModel(TimeLaneModel[CameraTimeLane]):
     # ruff: noqa: N802
-    def __init__(self, name: str, parent: Optional[QObject] = None):
+    def __init__(
+        self, name: str, undo_stack: QUndoStack, parent: Optional[QObject] = None
+    ):
         lane = CameraTimeLane([None])
-        super().__init__(name, lane, parent)
+        super().__init__(name, lane, undo_stack, parent)
         self._brush = None
         palette = QPalette()
         color = palette.text().color()
@@ -25,7 +27,7 @@ class CameraTimeLaneModel(TimeLaneModel[CameraTimeLane]):
     def data(self, index, role: int = Qt.ItemDataRole.DisplayRole):
         if not index.isValid():
             return None
-        value = self._lane[index.row()]
+        value = self.lane_value(index.row())
         if role == Qt.ItemDataRole.DisplayRole:
             if isinstance(value, TakePicture):
                 return value.picture_name
@@ -54,21 +56,18 @@ class CameraTimeLaneModel(TimeLaneModel[CameraTimeLane]):
         if not index.isValid():
             return False
         if role == Qt.ItemDataRole.EditRole:
-            start, stop = self._lane.get_bounds(Step(index.row()))
-            if isinstance(value, str):
-                if value == "":
-                    self._lane[start:stop] = None
-                elif isinstance(value, str):
-                    self._lane[start:stop] = TakePicture(ImageLabel(DataLabel(value)))
-                else:
-                    raise TypeError(f"Invalid type for value: {type(value)}")
-                self.dataChanged.emit(index, index)
-                return True
+            assert isinstance(value, str)
+
+            if value == "":
+                new_value = None
             else:
-                raise TypeError(f"Invalid type for value: {type(value)}")
+                new_value = TakePicture(ImageLabel(DataLabel(value)))
+
+            return self.set_lane_value(index.row(), new_value)
+
         return False
 
     def insertRow(
         self, row, parent: QModelIndex | QPersistentModelIndex = _DEFAULT_INDEX
     ) -> bool:
-        return self.insert_value(row, None)
+        return self.insert_lane_value(row, None)
