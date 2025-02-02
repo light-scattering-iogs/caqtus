87822c64ead6e4a97ced07f266995ffeba9fd29b
damien <damien.bloch@institutoptique.fr>
2025-02-02 16:44:59 +0100

    Fix test for step iteration editor

--- tests/test_gui/test_condetrol/test_sequence_iteration_editors/test_sequence_steps_editor.py
+++ tests/test_gui/test_condetrol/test_sequence_iteration_editors/test_sequence_steps_editor.py
@@ -43,8 +43,8 @@ def test_0(qtbot: QtBot, qtmodeltester: ModelTester):
             ),
         ]
     )
-    with qtbot.assert_not_emitted(editor.iteration_edited):
-        editor.set_iteration(steps)
+
+    editor.set_iteration(steps)
 
     qtbot.addWidget(editor)
 
@@ -69,7 +69,6 @@ def test_1(qtbot: QtBot):
         editor.model().index(0, 0),
         VariableDeclarationData(DottedVariableName("b"), Expression("2")),
     )
-    qtbot.wait_signal(editor.iteration_edited)
     assert editor.get_iteration() == StepsConfiguration(
         [VariableDeclaration(DottedVariableName("b"), Expression("2"))]
     )
@@ -104,7 +103,6 @@ def test_2(qtbot: QtBot):
             num=10,
         ),
     )
-    qtbot.wait_signal(editor.iteration_edited)
     assert editor.get_iteration() == StepsConfiguration(
         [
             LinspaceLoop(
@@ -147,7 +145,6 @@ def test_3(qtbot: QtBot):
             step=Expression("0.2"),
         ),
     )
-    qtbot.wait_signal(editor.iteration_edited)
     assert editor.get_iteration() == StepsConfiguration(
         [
             ArangeLoop(
