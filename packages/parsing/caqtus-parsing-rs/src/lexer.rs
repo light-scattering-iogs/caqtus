6ef32d52c3354364a60bd392e7c4a49ac38bc2e8
damien <damien.bloch@institutoptique.fr>
2025-03-30 11:25:22 +0200

    Add parsing for float

--- packages/parsing/caqtus-parsing-rs/src/lexer.rs
+++ packages/parsing/caqtus-parsing-rs/src/lexer.rs
@@ -1,9 +1,10 @@
 use logos::{Logos, Span};
 use std::fmt::Display;
-use std::num::ParseIntError;
+use std::num::{ParseFloatError, ParseIntError};
 
 #[derive(Debug, PartialEq, Clone, Default)]
 pub enum LexingError {
+    ParseFloatError(ParseFloatError),
     ParseIntError(ParseIntError),
     #[default]
     InvalidToken,
@@ -15,25 +16,44 @@ impl From<ParseIntError> for LexingError {
     }
 }
 
+impl From<ParseFloatError> for LexingError {
+    fn from(err: ParseFloatError) -> Self {
+        LexingError::ParseFloatError(err)
+    }
+}
+
+fn callback_integer(lex: &mut logos::Lexer<Token>) -> Result<isize, LexingError> {
+    let slice = lex.slice().replace("_", "");
+    slice.parse().map_err(LexingError::from)
+}
+
+fn callback_float(lex: &mut logos::Lexer<Token>) -> Result<f64, LexingError> {
+    let slice = lex.slice();
+    slice.parse().map_err(LexingError::from)
+}
+
 #[derive(Logos, Debug, Clone, PartialEq)]
 #[logos(error = LexingError)]
 #[logos(skip r" ")]
 pub enum Token {
     Error(LexingError),
-    #[regex(r"[\+|-]?[0-9]+", |lex| lex.slice().parse())]
+    #[regex(r"[\+-]?\d+", callback_integer, priority=3)]
     Integer(isize),
+    #[regex(r"[\+-]?(\d+(\.\d*)?|\.\d+)([eE][\+-]?\d+)?", callback_float)]
+    Float(f64),
 }
 
 impl Display for Token {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Token::Integer(value) => write!(f, "Integer({})", value),
+            Token::Float(value) => write!(f, "Float({})", value),
             Token::Error(err) => write!(f, "Error({:?})", err),
         }
     }
 }
 
-pub fn lex(input: &str) -> impl Iterator<Item = (Token, Span)> {
+pub fn lex(input: &str) -> impl Iterator<Item=(Token, Span)> {
     Token::lexer(input).spanned().map(|(tok, span)| match tok {
         Ok(token) => (token, span),
         Err(err) => (Token::Error(err), span),
--- packages/parsing/caqtus-parsing-rs/src/parser.rs
+++ packages/parsing/caqtus-parsing-rs/src/parser.rs
@@ -3,10 +3,12 @@ use chumsky::error::Rich;
 use chumsky::input::{Input, Stream, ValueInput};
 use chumsky::span::SimpleSpan;
 use chumsky::{Parser, extra, select};
+use chumsky::prelude::end;
 
 #[derive(Debug, PartialEq)]
 pub enum ParseNode {
     Integer(isize),
+    Float(f64),
 }
 
 fn parser<'a, I>() -> impl Parser<'a, I, ParseNode, extra::Err<Rich<'a, Token>>>
@@ -15,8 +17,9 @@ where
 {
     let number = select! {
         Token::Integer(value) => ParseNode::Integer(value),
+        Token::Float(value) => ParseNode::Float(value),
     };
-    number
+    number.then_ignore(end())
 }
 
 fn parse(input: &str) -> Result<ParseNode, Vec<Rich<Token>>> {
@@ -44,10 +47,57 @@ mod tests {
         let result = parse("-45");
         assert_eq!(result, Ok(ParseNode::Integer(-45)));
     }
+
     #[test]
-    fn safely_fails_to_parse_huge_integer_string() {
+    fn successfully_parse_leading_zero_integer() {
+        let result = parse("01");
+        assert_eq!(result, Ok(ParseNode::Integer(1)));
+    }
+
+    #[test]
+    fn successfully_parse_leading_zero_with_only_zero() {
+        let result = parse("0");
+        assert_eq!(result, Ok(ParseNode::Integer(0)));
+    }
+    #[test]
+    fn fails_to_parse_huge_integer_string() {
         let result = parse("123456789012345678901234567890");
         assert!(result.is_err());
     }
 
+    #[test]
+    fn successfully_parse_float_string() {
+        let result = parse("45.0");
+        assert_eq!(result, Ok(ParseNode::Float(45.0)));
+    }
+
+    #[test]
+    fn successfully_parse_float_string_with_sign() {
+        let result = parse("+45.0");
+        assert_eq!(result, Ok(ParseNode::Float(45.0)));
+        let result = parse("-45.0");
+        assert_eq!(result, Ok(ParseNode::Float(-45.0)));
+    }
+
+    #[test]
+    fn successfully_parse_float_string_with_exponent() {
+        let result = parse("45.0e10");
+        assert_eq!(result, Ok(ParseNode::Float(45.0e10)));
+        let result = parse("45.0E10");
+        assert_eq!(result, Ok(ParseNode::Float(45.0e10)));
+    }
+
+    #[test]
+    fn successfully_parse_float_string_with_exponent_and_sign() {
+        let result = parse("-45.0e+10");
+        assert_eq!(result, Ok(ParseNode::Float(-45.0e10)));
+        let result = parse("45.0e-10");
+        assert_eq!(result, Ok(ParseNode::Float(45.0e-10)));
+    }
+
+    #[test]
+    fn fails_to_parse_number_seperated_by_space() {
+        let result = parse("45 0");
+        assert!(result.is_err());
+    }
 }
