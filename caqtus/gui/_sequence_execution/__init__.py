5942734dee5694b27a6a76d346b5b2f0840bfca3
damien <damien.bloch@institutoptique.fr>
2025-05-29 17:21:02 +0200

    Add package caqtus.gui._sequence_execution

---
+++ caqtus/gui/_sequence_execution/__init__.py
@@ -0,0 +1,5 @@
+"""This module contains GUI components used during the execution of a sequence.
+
+The code contained in this module cannot be used standalone but instead provides
+functionality for the :module:`caqtus.experiment_control` module.
+"""
