314ca46d1bb8089ba9321e481dacad5e3dc0bcb6
damien <damien.bloch@institutoptique.fr>
2025-05-30 17:50:26 +0200

    Add function to register structure hooks for expressions

--- caqtus/types/expression/__init__.py
+++ caqtus/types/expression/__init__.py
@@ -1,5 +1,15 @@
 """Defines user expressions that can be evaluated later."""
 
-from ._expression import Expression, expression_builtins, DEFAULT_BUILTINS
+from ._expression import (
+    DEFAULT_BUILTINS,
+    Expression,
+    configure_expression_conversion_hooks,
+    expression_builtins,
+)
 
-__all__ = ["Expression", "expression_builtins", "DEFAULT_BUILTINS"]
+__all__ = [
+    "Expression",
+    "expression_builtins",
+    "DEFAULT_BUILTINS",
+    "configure_expression_conversion_hooks",
+]
