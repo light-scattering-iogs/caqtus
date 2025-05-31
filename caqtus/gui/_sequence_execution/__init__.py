db0bc50aa399d445e627942f15c4a5b4e4201e3d
damien <damien.bloch@institutoptique.fr>
2025-05-31 10:59:41 +0200

    Remove converter for evaluated user input

--- caqtus/gui/_sequence_execution/__init__.py
+++ caqtus/gui/_sequence_execution/__init__.py
@@ -4,13 +4,6 @@ The code contained in this module cannot be used standalone but instead provides
 functionality for the :module:`caqtus.experiment_control` module.
 """
 
-from ._user_input_widget import UserInputWidget, AnalogRange, DigitalInput, InputType
-from ._converter import configure_input_type
+from ._user_input_widget import UserInputWidget
 
-__all__ = [
-    "UserInputWidget",
-    "InputType",
-    "AnalogRange",
-    "DigitalInput",
-    "configure_input_type",
-]
+__all__ = ["UserInputWidget"]
