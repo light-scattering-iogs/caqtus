8d58a5a7435a3dbaf8e5b3cb505128613edd9b35
damien <damien.bloch@institutoptique.fr>
2025-06-28 10:59:14 +0200

    Start to update step iteratino editor

--- caqtus/gui/condetrol/_sequence_iteration_editors/sequence_iteration_editor.py
+++ caqtus/gui/condetrol/_sequence_iteration_editors/sequence_iteration_editor.py
@@ -1,6 +1,6 @@
 import abc
 from collections.abc import Callable, Set
-from typing import TypeVar, Generic, TypeAlias
+from typing import Generic, TypeAlias, TypeVar
 
 import caqtus.gui.qtutil.qabc as qabc
 from caqtus.types.iteration import IterationConfiguration
@@ -20,6 +20,20 @@ class SequenceIterationEditor(Generic[T], metaclass=qabc.QABCMeta):
 
     @abc.abstractmethod
     def set_read_only(self, read_only: bool):
+        """Sets the editor in read-only mode.
+
+        When the editor is in read-only mode, the user cannot edit the steps.
+
+        Even if the editor is in read-only mode, the iteration can still be set
+        programmatically with :meth:`set_iteration`.
+        """
+
+        raise NotImplementedError
+
+    @abc.abstractmethod
+    def is_read_only(self) -> bool:
+        """Returns whether the editor is in read-only mode."""
+
         raise NotImplementedError
 
     @abc.abstractmethod
--- caqtus/gui/condetrol/_sequence_iteration_editors/steps_iteration_editor/steps_iteration_editor.py
+++ caqtus/gui/condetrol/_sequence_iteration_editors/steps_iteration_editor/steps_iteration_editor.py
@@ -4,31 +4,32 @@ from typing import Optional
 
 import yaml
 from PySide6 import QtWidgets
-from PySide6.QtCore import Qt, QPersistentModelIndex, QModelIndex
+from PySide6.QtCore import QModelIndex, QPersistentModelIndex, Qt
 from PySide6.QtGui import (
-    QKeySequence,
-    QShortcut,
     QFont,
     QGuiApplication,
+    QKeySequence,
+    QShortcut,
     QUndoStack,
 )
-from PySide6.QtWidgets import QWidget, QTreeView, QAbstractItemView, QMenu
+from PySide6.QtWidgets import QAbstractItemView, QMenu, QTreeView, QWidget
 
 from caqtus.gui.qtutil._temporary_widget import temporary_widget
 from caqtus.types.expression import Expression
 from caqtus.types.iteration import (
-    StepsConfiguration,
-    VariableDeclaration,
+    ArangeLoop,
     ExecuteShot,
     LinspaceLoop,
-    ArangeLoop,
+    StepsConfiguration,
+    VariableDeclaration,
 )
 from caqtus.types.variable_name import DottedVariableName
 from caqtus.utils import serialization
+
+from ..._icons import get_icon
+from ..sequence_iteration_editor import SequenceIterationEditor
 from .delegate import StepDelegate
 from .steps_model import StepsModel
-from ..sequence_iteration_editor import SequenceIterationEditor
-from ..._icons import get_icon
 
 
 def create_shot_step() -> ExecuteShot:
@@ -61,7 +62,7 @@ def create_arange_loop():
     )
 
 
-class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration]):
+class StepsIterationEditor(QWidget, SequenceIterationEditor[StepsConfiguration]):
     """Editor for the steps of a sequence.
 
     .. figure:: screenshot_StepsIterationEditor.png
@@ -80,40 +81,11 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
 
     def __init__(self, parent: Optional[QWidget] = None):
         super().__init__(parent)
-        self._model = StepsModel(StepsConfiguration([]))
-        self.setModel(self._model)
-        self.expandAll()
-        self.header().hide()
-        self._delegate = StepDelegate(self)
-        self.setItemDelegate(self._delegate)
 
-        self.setDragEnabled(True)
-        self.setAcceptDrops(True)
-        self.setDropIndicatorShown(True)
-        self.setDragDropMode(QAbstractItemView.DragDropMode.DragDrop)
-        self.setDefaultDropAction(Qt.DropAction.MoveAction)
-
-        self.delete_shortcut = QShortcut(QKeySequence("Delete"), self)
-        self.delete_shortcut.activated.connect(self.delete_selected)
-
-        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
-        self.customContextMenuRequested.connect(self.show_context_menu)
-
-        self.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
-        self.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
-
-        self.select_all_shortcut = QShortcut(QKeySequence("Ctrl+A"), self)
-        self.select_all_shortcut.activated.connect(self.selectAll)
-        self.copy_shortcut = QShortcut(QKeySequence("Ctrl+C"), self)
-        self.copy_shortcut.activated.connect(self.copy_selection)
-        self.cut_shortcut = QShortcut(QKeySequence("Ctrl+X"), self)
-        self.cut_shortcut.activated.connect(self.cut_selected)
-        self.paste_shortcut = QShortcut(QKeySequence("Ctrl+V"), self)
-        self.paste_shortcut.activated.connect(self.paste)
-
-        font = QFont("JetBrains Mono")
-        font.setPixelSize(13)
-        self.setFont(font)
+        layout = QtWidgets.QVBoxLayout(self)
+        self._steps_tree_editor = StepsTreeEditor(self)
+        layout.addWidget(self._steps_tree_editor)
+        self.setLayout(layout)
 
         self.toolbar = QtWidgets.QToolBar()
         self.add_button = QtWidgets.QToolButton(self)
@@ -124,6 +96,11 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         self.add_arange_action = self.add_menu.addAction("arange loop")
         self._setup_add_button()
         self.toolbar.addWidget(self.add_button)
+
+        self.add_tunable_parameter_action = self.toolbar.addAction(
+            get_icon("mdi6.tune-variant", self.palette().buttonText().color()),
+            "Add tunable parameter",
+        )
 
         self.toolbar.addSeparator()
         self.copy_to_clipboard_action = self.toolbar.addAction(
@@ -150,7 +127,7 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         editor.
         """
 
-        return self._model.undo_stack
+        return self._steps_tree_editor._model.undo_stack
 
     def _setup_add_button(self):
         self.add_button.setToolTip("Add step")
@@ -161,15 +138,15 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         self.add_button.setMenu(self.add_menu)
         self.add_shot_action.triggered.connect(
             functools.partial(
-                self._model.insert_steps,
+                self._steps_tree_editor._model.insert_steps,
                 [create_shot_step()],
-                self._model.rowCount(),
+                self._steps_tree_editor._model.rowCount(),
                 QModelIndex(),
             )
         )
         self.add_variable_action.triggered.connect(
             functools.partial(
-                self._model.insert_steps,
+                self._steps_tree_editor._model.insert_steps,
                 [create_variable_declaration()],
                 0,
                 QModelIndex(),
@@ -177,12 +154,18 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         )
         self.add_linspace_action.triggered.connect(
             functools.partial(
-                self._model.insert_steps, [create_linspace_loop()], 0, QModelIndex()
+                self._steps_tree_editor._model.insert_steps,
+                [create_linspace_loop()],
+                0,
+                QModelIndex(),
             )
         )
         self.add_arange_action.triggered.connect(
             functools.partial(
-                self._model.insert_steps, [create_arange_loop()], 0, QModelIndex()
+                self._steps_tree_editor._model.insert_steps,
+                [create_arange_loop()],
+                0,
+                QModelIndex(),
             )
         )
 
@@ -193,7 +176,7 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         without affecting the editor.
         """
 
-        return self._model.get_steps()
+        return self._steps_tree_editor._model.get_steps()
 
     def set_iteration(self, iteration: StepsConfiguration):
         """Updates the iteration displayed by the editor.
@@ -201,74 +184,108 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
         This method does not emit the signal iteration_edited.
         """
 
-        self._model.set_steps(iteration)
-        self.expandAll()
+        self._steps_tree_editor._model.set_steps(iteration)
+        self._steps_tree_editor.expandAll()
 
     def set_available_parameter_names(self, parameter_names: Set[DottedVariableName]):
         """Sets the names that can be used in the iteration.
 
         The names are used to populate the auto-completion of variable names when the
-        user try to edit the name of a variable or a loop.
+        user tries to edit the name of a variable or a loop.
         """
 
-        self._delegate.set_available_names(parameter_names)
+        self._steps_tree_editor._delegate.set_available_names(parameter_names)
 
     def set_read_only(self, read_only: bool) -> None:
-        """Sets the editor in read-only mode.
-
-        When the editor is in read-only mode, the user cannot edit the steps.
-
-        Even if the editor is in read-only mode, the iteration can still be set
-        programmatically with :meth:`set_iteration`.
-        """
-
-        self._model.set_read_only(read_only)
+        self._steps_tree_editor.set_read_only(read_only)
         self.paste_from_clipboard_action.setEnabled(not read_only)
         self.add_button.setEnabled(not read_only)
 
     def is_read_only(self) -> bool:
-        """Returns whether the editor is in read-only mode."""
+        return self._steps_tree_editor.is_read_only()
 
+    def copy_to_clipboard(self):
+        steps = self.get_iteration()
+
+        unstructured = serialization.converters["json"].unstructure(
+            steps, StepsConfiguration
+        )
+
+        text = yaml.safe_dump(unstructured)
+        clipboard = QGuiApplication.clipboard()
+        clipboard.setText(text)
+
+    def paste_from_clipboard(self) -> bool:
+        """Attempts to paste the content of the clipboard.
+
+        If the editor is in read-only mode, this method does nothing.
+        """
+
+        if self.is_read_only():
+            return False
+
+        clipboard = QGuiApplication.clipboard()
+        text = clipboard.text()
+        try:
+            data = yaml.safe_load(text)
+        except yaml.YAMLError as e:
+            QtWidgets.QMessageBox.warning(  # type: ignore[reportCallIssue]
+                self,
+                "Invalid YAML content",
+                f"Could not parse the clipboard content as YAML:\n {e}",
+            )
+            return False
+
+        # TODO: raise recoverable exception if data is not valid
+        config = serialization.structure(data, StepsConfiguration)
+        self._model.replace_steps(config.steps, "paste from clipboard")
+
+        return True
+
+
+class StepsTreeEditor(QTreeView):
+    def __init__(self, parent: QWidget | None = None):
+        super().__init__(parent)
+        self._model = StepsModel(StepsConfiguration([]))
+        self.setModel(self._model)
+        self.expandAll()
+        self.header().hide()
+        self._delegate = StepDelegate(self)
+        self.setItemDelegate(self._delegate)
+
+        self.setDragEnabled(True)
+        self.setAcceptDrops(True)
+        self.setDropIndicatorShown(True)
+        self.setDragDropMode(QAbstractItemView.DragDropMode.DragDrop)
+        self.setDefaultDropAction(Qt.DropAction.MoveAction)
+
+        self.setContextMenuPolicy(Qt.ContextMenuPolicy.CustomContextMenu)
+        self.customContextMenuRequested.connect(self.show_context_menu)
+
+        self.setSelectionBehavior(QAbstractItemView.SelectionBehavior.SelectRows)
+        self.setSelectionMode(QAbstractItemView.SelectionMode.ExtendedSelection)
+
+        self.delete_shortcut = QShortcut(QKeySequence("Delete"), self)
+        self.delete_shortcut.activated.connect(self.delete_selected)
+
+        self.select_all_shortcut = QShortcut(QKeySequence("Ctrl+A"), self)
+        self.select_all_shortcut.activated.connect(self.selectAll)
+        self.copy_shortcut = QShortcut(QKeySequence("Ctrl+C"), self)
+        self.copy_shortcut.activated.connect(self.copy_selection)
+        self.cut_shortcut = QShortcut(QKeySequence("Ctrl+X"), self)
+        self.cut_shortcut.activated.connect(self.cut_selected)
+        self.paste_shortcut = QShortcut(QKeySequence("Ctrl+V"), self)
+        self.paste_shortcut.activated.connect(self.paste)
+
+        font = QFont("JetBrains Mono")
+        font.setPixelSize(13)
+        self.setFont(font)
+
+    def set_read_only(self, read_only: bool) -> None:
+        self._model.set_read_only(read_only)
+
+    def is_read_only(self) -> bool:
         return self._model.is_read_only()
-
-    def delete_selected(self) -> bool:
-        if self.is_read_only():
-            return False
-        self._model.remove_indices(self.selectedIndexes())
-        return True
-
-    def copy_selection(self) -> None:
-        selected_indexes = self.selectedIndexes()
-        data = self._model.mimeData(selected_indexes)
-        clipboard = QGuiApplication.clipboard()
-        clipboard.setMimeData(data)
-
-    def cut_selected(self) -> bool:
-        if self.is_read_only():
-            return False
-
-        self.copy_selection()
-        self._model.remove_indices(self.selectedIndexes())
-        return True
-
-    def paste(self) -> bool:
-        if self.is_read_only():
-            return False
-
-        clipboard = QGuiApplication.clipboard()
-        data = clipboard.mimeData()
-
-        if self.selectedIndexes():
-            first = QPersistentModelIndex(self.selectedIndexes()[0])
-            parent = first.parent()
-            row = first.row()
-            if self._model.dropMimeData(data, Qt.DropAction.MoveAction, row, 0, parent):
-                self._model.removeRow(first.row(), first.parent())
-        else:
-            parent = QModelIndex()
-            row = self._model.rowCount()
-            self._model.dropMimeData(data, Qt.DropAction.MoveAction, row, 0, parent)
-        return True
 
     def show_context_menu(self, position):
         index = self.indexAt(position)
@@ -313,40 +330,41 @@ class StepsIterationEditor(QTreeView, SequenceIterationEditor[StepsConfiguration
             )
             menu.exec(self.mapToGlobal(position))
 
-    def copy_to_clipboard(self):
-        steps = self.get_iteration()
+    def delete_selected(self) -> bool:
+        if self.is_read_only():
+            return False
+        self._model.remove_indices(self.selectedIndexes())
+        return True
 
-        unstructured = serialization.converters["json"].unstructure(
-            steps, StepsConfiguration
-        )
-
-        text = yaml.safe_dump(unstructured)
+    def copy_selection(self) -> None:
+        selected_indexes = self.selectedIndexes()
+        data = self._model.mimeData(selected_indexes)
         clipboard = QGuiApplication.clipboard()
-        clipboard.setText(text)
+        clipboard.setMimeData(data)
 
-    def paste_from_clipboard(self) -> bool:
-        """Attempts to paste the content of the clipboard.
+    def cut_selected(self) -> bool:
+        if self.is_read_only():
+            return False
 
-        If the editor is in read-only mode, this method does nothing.
-        """
+        self.copy_selection()
+        self._model.remove_indices(self.selectedIndexes())
+        return True
 
-        if self._model.is_read_only():
+    def paste(self) -> bool:
+        if self.is_read_only():
             return False
 
         clipboard = QGuiApplication.clipboard()
-        text = clipboard.text()
-        try:
-            data = yaml.safe_load(text)
-        except yaml.YAMLError as e:
-            QtWidgets.QMessageBox.warning(  # type: ignore[reportCallIssue]
-                self,
-                "Invalid YAML content",
-                f"Could not parse the clipboard content as YAML:\n {e}",
-            )
-            return False
+        data = clipboard.mimeData()
 
-        # TODO: raise recoverable exception if data is not valid
-        config = serialization.structure(data, StepsConfiguration)
-        self._model.replace_steps(config.steps, "paste from clipboard")
-
+        if self.selectedIndexes():
+            first = QPersistentModelIndex(self.selectedIndexes()[0])
+            parent = first.parent()
+            row = first.row()
+            if self._model.dropMimeData(data, Qt.DropAction.MoveAction, row, 0, parent):
+                self._model.removeRow(first.row(), first.parent())
+        else:
+            parent = QModelIndex()
+            row = self._model.rowCount()
+            self._model.dropMimeData(data, Qt.DropAction.MoveAction, row, 0, parent)
         return True
